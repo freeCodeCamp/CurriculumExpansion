04b87e52d37cc18d0f1fe42b2f9edaa0a0e5defe
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-03-21 15:14:58 -0400

    Update algorithms-and-data-structures/basic-javascript/index030.js
    Co-Authored-By: beaucarnes <beaucarnes@gmail.com>

--- algorithms-and-data-structures/basic-javascript/index030.js
+++ algorithms-and-data-structures/basic-javascript/index030.js
@@ -2,4 +2,6 @@ var xp = 0;
 var health = 100;
 var gold = 50;
 
-// Create a variable to store the current weapon and set it to 0. When a name has two words, the convention is to use so-called "lower camel case". The variable name should look like this: currentWeapon.
+// Create a variable to store the current weapon and set it to 0.
+// When a name has two words, the convention is to use so-called "lowerCamelCase".
+// The variable name should look like this: currentWeapon.
