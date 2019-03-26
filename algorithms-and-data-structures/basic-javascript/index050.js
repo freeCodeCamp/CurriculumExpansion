f9a495720767be2e7d6454f074a2838440a72538
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-03-26 16:46:03 -0400

    Update algorithms-and-data-structures/basic-javascript/index050.js
    Co-Authored-By: beaucarnes <beaucarnes@gmail.com>

--- algorithms-and-data-structures/basic-javascript/index050.js
+++ algorithms-and-data-structures/basic-javascript/index050.js
@@ -7,3 +7,4 @@ let currentWeapon = 0;
 Now you will declare a variable without intializing it.
 Using the let keyword, declare a variable called `fighting` but don't set it equal to anything.
 Just end the line with a semicolon right after the variable name. 
+*/
