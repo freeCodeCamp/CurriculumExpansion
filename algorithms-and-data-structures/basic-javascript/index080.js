cdc266c70a6b820c9cb2afbbd5f4dd8c21992903
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-04-03 15:15:31 -0400

    Update algorithms-and-data-structures/basic-javascript/index080.js
    Co-Authored-By: beaucarnes <beaucarnes@gmail.com>

--- algorithms-and-data-structures/basic-javascript/index080.js
+++ algorithms-and-data-structures/basic-javascript/index080.js
@@ -6,4 +6,4 @@ let fighting;
 let monsterHealth;
 let inventory = "stick";
 
-// Since the inventory can store multiple items, change the value of inventory to an array with the items stick, dagger, and sword. Here is an example of a variable sandwich that equals a three-item array: let sandwich = ["peanut butter", "jelly", "bread"]
+// Since the inventory can store multiple items, change the value of inventory to an array with the items stick, dagger, and sword. Here is an example of a variable sandwich that equals a three-item array: `let sandwich = ["peanut butter", "jelly", "bread"]`
