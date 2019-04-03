858491bb6cd3c9c73ff3d06b0b7845a18f11709e
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-03 15:57:18 -0400

    feat: first draft of the JSON curriculum

---
+++ data-visualization/json/index05.js
@@ -0,0 +1,12 @@
+const myTeam = {
+  "Team": "Argentina",
+  "Sport": "Football",
+  "Year": 1986,
+  "Is World Cup Winner": true
+};
+
+/*
+So far, you've seen value types for strings, numbers, and Boolean values. Add a new key after `World Cup Winner` for `Head Coach` and set the value as an object.
+
+The object should have two key-value pairs in it. The first one is `Name`, set to `Carlos Bilardo`, and the second is for `Matches` (how many games he coached in the tournament), set to the number `7`.
+*/
