b2a1a52a9598ecbd2163a1807c27e74a5cc2ed05
picolloo <lucaspicolloo@gmail.com>
2019-04-13 00:25:14 -0300

    implementing new layout

--- issue98-websockets/src/routes/index.js
+++ issue98-websockets/src/routes/index.js
@@ -2,31 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.use((req, res, next) => {
-  //validate
-
-  next();
-});
-
 router.get("/", (req, res) => {
-  res.redirect("/login");
-  // res.sendFile(path.join(__dirname, "../views/chatroom.html"));
-});
-
-router.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/login.html"));
 });
 
-router.get("/rooms", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/rooms.html"));
-});
-
-router.post("/rooms", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/rooms.html"));
-});
-
-router.get("/rooms/:id", (req, res) => {
-  const roomId = req.params.id;
+router.use("/room", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/chatroom.html"));
 });
 
