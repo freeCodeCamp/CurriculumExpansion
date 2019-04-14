479954bb02293706fdf506e1df7175b3221b5a19
Lionel Rowe <lionel.rowe@gmail.com>
2019-04-14 17:42:50 +0800

    Initialize project with basic HTML/JS files

---
+++ learn-regular-expressions-by-building-a-spam-filter/spamFilter.js
@@ -0,0 +1,14 @@
+const wordCondenser = /(?:)/; // TODO
+const spaceCondenser = /(?:)/; // TODO
+
+const condenseSpaces = msg => {
+  return msg; // TODO
+};
+
+
+const isSpam = msg => {
+  const spacesCondensed = condenseSpaces(msg);
+
+  return false; // TODO
+};
+
