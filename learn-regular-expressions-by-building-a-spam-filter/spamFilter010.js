022c417e83f45405169773a21f055e920648abda
Lionel Rowe <lionel.rowe@gmail.com>
2019-04-14 19:06:35 +0800

    Steps 1 to 8 (up to and including mangled-string step)

---
+++ learn-regular-expressions-by-building-a-spam-filter/spamFilter010.js
@@ -0,0 +1,16 @@
+const isSpam = msg => {
+  /*
+
+  To start with, declare a variable called `re` using `const` and set its value to `/./`.
+
+  `/./` breaks down as follows:
+  * The starting and ending forward slashes (`/`) signal that it is a regex.
+  * The dot (`.`) means "match any character (other than new lines)".
+
+  Our regex will match any string that contains a character other than a new line. For example, "A", "嗡", and "asdf" all match, but "" doesn't.
+
+  Note that the match can be anywhere in the string.
+  */
+
+  return false;
+};
