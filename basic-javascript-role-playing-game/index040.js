d1aab22ca049955cd592a5998039429992eb6c45
Beau Carnes <beaucarnes@gmail.com>
2019-04-19 13:09:11 -0400

    Update index040.js

--- basic-javascript-role-playing-game/index040.js
+++ basic-javascript-role-playing-game/index040.js
@@ -4,8 +4,5 @@ var gold = 50;
 var currentWeapon = 0;
 
 /*
-We've been declaring variables with the 'var' keyword.
-However, in modern JavaScript, it's better to use 'const' or 'let' instead, because they fix a number of unusual behaviors with 'var' that make it difficult to reason about.
-'const' signals that a variable can only be assigned once, and 'let' signals that we want to reassign it.
-Because we will be changing these values as the game progresses, let's use 'let' for all of them.
+We've been declaring variables with the `var` keyword. However, in modern JavaScript, it's better to use `let` instead of `var` because it fixes a number of unusual behaviors with 'var' that make it difficult to reason about. Change every `var` to `let`.
 */
