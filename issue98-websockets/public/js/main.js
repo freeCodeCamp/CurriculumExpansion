c54e3237d0b5a99d11668c13326503646ea07d0a
picolloo <lucaspicolloo@gmail.com>
2019-04-22 23:03:40 -0300

    common css applied and messages signals implemented

--- issue98-websockets/public/js/main.js
+++ issue98-websockets/public/js/main.js
@@ -19,20 +19,31 @@ const app = {
     document.querySelector("#chat-form").addEventListener("submit", e => {
       e.preventDefault();
 
-      const msg = document.querySelector("#msg-input").nodeValue;
+      const msg = document.querySelector("#msg-input").value;
 
       if (msg) {
         socket.emit("newMessage", msg);
+        app.helpers.addMessage(msg);
       }
     });
   },
 
   login: function() {},
+
   helpers: {
     updateUsersList: function(users) {},
 
     addMessage: function(message) {
-      console.log(message);
+      const template = document.querySelector(".msg-component");
+      const clone = document.importNode(template.content, true);
+
+      clone.querySelector(".msg-content").textContent = message;
+
+      const chat_history = document.querySelector("#chat-history");
+
+      chat_history.appendChild(clone);
+
+      document.querySelector("#msg-input").value = "";
     }
   }
 };
