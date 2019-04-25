0b129d87dba0a0b2ec3ddd3c15304730523ff05c
Phillip Shim <shimphillip@gmail.com>
2019-04-25 11:20:10 -0500

    Update index05.js

--- learn-data-structures-by-building-a-shopping-cart/index05.js
+++ learn-data-structures-by-building-a-shopping-cart/index05.js
@@ -32,15 +32,15 @@ Inside the constructor is where we can initialize our state.
 
 What state do we need for our shopping cart?
 
-We need some sort of storage place that can save the products we buy.
+First, we need some sort of storage place that can save the products we buy.
 
-To declare variables inside our class, we need to use this keyword.
+To declare variables inside our constructor, we need to use this keyword.
 
-In simple terms, this refers the current object or in this case the ShoppingCart class because class is afterall an object.
-
-The variables inside the constructor need to be bound to this. Because you are binding a property to an object you need to use dot notation. Example syntax goes like this.
+In simple terms, this refers to the current object. The variables inside the constructor need to be bound to this. For ex.
 
 this.name = "Phillip";
+
+This value will then be readily and easily accessible in other methods inside the class. 
 
 Create an empty array and assign it to a variable called 'items'. This items array will hold the state of products in the cart.
 */
