1a22d52e675813e639a0c706bd066e1ed73c3764
Phillip Shim <shimphillip@gmail.com>
2019-04-25 11:22:47 -0500

    Update index06.js

--- learn-data-structures-by-building-a-shopping-cart/index06.js
+++ learn-data-structures-by-building-a-shopping-cart/index06.js
@@ -28,9 +28,11 @@ class ShoppingCart {
 }
 
 /*
-Good job! Nowe we can start adding some products to our cart.
+Good job! Now we have a place to store any items we buy.
 
-To manipulate our current state, we need another method to help us. We will call it addItem.
+To manipulate states in class, we need to start creating other methods.
 
-Declare a method addItem under the constructor function.
+Let's create a method called addItem that will help us start adding items to the cart.
+
+Declare a method addItem under the constructor function. Use the same syntax like constructor method.
 */
