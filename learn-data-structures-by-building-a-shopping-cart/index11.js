33c7a6fc2dc1ff4e86bcef5718350c6eed0ed1f8
Phillip Shim <shimphillip@gmail.com>
2019-04-25 13:22:52 -0500

    Create index11.js

---
+++ learn-data-structures-by-building-a-shopping-cart/index11.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: 0,
+    name: "Vanilla buttercream cupcake",
+    price: 2.99
+  },
+  {
+    id: 1,
+    name: "French Macaroon",
+    price: 3.99
+  },
+  {
+    id: 2,
+    name: "Fruit sprinkles cupcake",
+    price: 3.99
+  },
+  {
+    id: 3,
+    name: "Pink flower cupcake",
+    price: 5.99
+  }
+];
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(id, products) {
+    const item = products.find(item => item.id === id);
+    this.items.push(item);
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
+
+const shoppingCart = new ShoppingCart();
+
+/*
+Great.
+
+Let's try adding an item and print the items array after.
+
+addItems method on shoppingCart. Pass in 2 as its id and products array as its products.
+
+Then call getItems method to see what's returned from the array. For ex.
+
+ourInstance.ourMethod();
+*/
