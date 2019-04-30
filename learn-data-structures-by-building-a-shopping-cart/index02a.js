0a6629c6f2cd1fb3feef637c836b42cb2ac054c8
Phillip Shim <shimphillip@gmail.com>
2019-04-30 10:42:10 -0500

    Update index02a.js

--- learn-data-structures-by-building-a-shopping-cart/index02a.js
+++ learn-data-structures-by-building-a-shopping-cart/index02a.js
@@ -5,16 +5,15 @@ const products = [
 ];
 
 /*
-Good job. Now we have an cupcake in our store!
+Our delicious Vanilla buttercream cupcake needs more information for us to sell it.
 
-But we need more information about our delicious cupcake, like its price if we are going to sell it.
+Let's add additional keys and values. Give our cupcake properties of id and price. Match them with values 0 and 2.99 respectively.
 
-When you add more key value pairs to an object, you separate them with commas. For example:
+When you add more key value pairs to an object, you separate them with commas. Here is an example:
 
 {
   name: "Phillip",
-  age: 29
+  age: 29,
+  gender: male
 }
-
-Give our first product additional properties of id and price. Match them with values 0 and 2.99 respectively.
 */
