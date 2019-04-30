28c9e510ae501ab5a8a1fb9d474a857e297cd27b
Phillip Shim <shimphillip@gmail.com>
2019-04-30 10:18:50 -0500

    Update index02.js

--- learn-data-structures-by-building-a-shopping-cart/index02.js
+++ learn-data-structures-by-building-a-shopping-cart/index02.js
@@ -1,25 +1,15 @@
 const products = [];
 
 /*
-  Good job. 
+Because the products in our online pastry shop have a lot of information, we will store each one as an object. 
 
-  We will be building an online pastry shop and each food has special attributes that the store assigns to organize their inventories.
+Inside the products array, add an object with the property name and the value "Vanilla buttercream cupcake". 
 
-  Because a product has much information, more than a name, we will store it as an object. 
+For example, here is an array that contains a similar object:
 
-  There are few ways to create a JavaScript Object, the easiest way is to use object literal.
-
-  To create object literal, we use curly brackets {}. 
-
-  Objects use key value pairs separated by colons :. Key in a object is often called a property. For example, 
-  
-  const obj = {
+const users = [
+  {
     name: "Phillip"
   }
-
-  This object has a key of name and string value of Phillip and is assigned to a variable called obj.
-
-  Let's start making our first product. Insert an object as the first item inside products array. You don't have to assign the object to variables.
-
-  Give the object a key called name and corresponding string of Vanilla buttercream cupcake
+];
 */
