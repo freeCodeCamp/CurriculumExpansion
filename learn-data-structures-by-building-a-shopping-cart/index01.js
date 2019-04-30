cd3adc5733cc21a53a70e21b54d7eda5c87a2f13
Phillip Shim <shimphillip@gmail.com>
2019-04-30 10:17:25 -0500

    Update index01.js

--- learn-data-structures-by-building-a-shopping-cart/index01.js
+++ learn-data-structures-by-building-a-shopping-cart/index01.js
@@ -1,19 +1,13 @@
 
 
 /* 
-A data structure is a way to store and organize data in a program.
+A data structure is a way to store and organize data in a program. 
 
-Arrays and objects are both common data structures you'll use in JavaScript.
+Arrays and objects are both common data structures you'll use in JavaScript. 
 
-We will use an object as our primary data sturcture to mimic functionalities of a shopping cart.
+Declare a new variable using the 'const' keyword and name it 'products', then set it equal to an empty array. 
 
-Before building our shopping cart though, we will first need some products that we can work with.
+Here is an example of a variable 'users' that equals an empty array: 
 
-Since there will be multiple products, we will use an array as our datasturcture to hold them. 
-
-To declare an array, we need a set of brackets. For example:
-
-const array = [];
-
-Declare a new variable using 'const' keyword and name it 'products', then set it equal to an empty array;
+const users = [];
 */
