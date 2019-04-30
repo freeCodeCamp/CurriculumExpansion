22e7f1561665de93d04772bda05c3bee5865a524
Phillip Shim <shimphillip@gmail.com>
2019-04-30 10:44:58 -0500

    Update index03.js

--- learn-data-structures-by-building-a-shopping-cart/index03.js
+++ learn-data-structures-by-building-a-shopping-cart/index03.js
@@ -23,17 +23,13 @@ const products = [
 
 /* 
 
-OK, We have actually filled up the rest of the products array with rest of the items to speed up the process.
+We have actually filled up the rest of the products array with rest of the items to speed up the process.
 
 Now that our shelves are full, let's build our shopping cart with a JavaScript class.
 
 What are classes? It's container that makes it easy to define a number of methods to work with data and hold its current state.
 
-In our case, think of it as a factory that can produce many number of shopping carts. 
-
-They will look the same and do the same things. (unless given different parameters, which we won't cover in this lesson.)
-
-To create a class, use a keyword called 'class' followed by a name then give opening and closing curly brackets. For ex.
+To create a class, use a keyword called 'class'. For example:
 
 class MyClass {
 
