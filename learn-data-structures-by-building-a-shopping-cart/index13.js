3625b61d5ac1138dfc123b8be2ca848cf0b9769e
Phillip Shim <shimphillip@gmail.com>
2019-05-06 15:06:10 -0500

    Create index13.js

---
+++ learn-data-structures-by-building-a-shopping-cart/index13.js
@@ -0,0 +1,54 @@
+const products = [
+  {
+    id: 0,
+    name: "Vanilla buttercream cupcake",
+    price: 2.99
+  },
+  {
+    id: 1,
+    name: "French Macaroon",
+    price: 3.99
+  },
+  {
+    id: 2,
+    name: "Fruit sprinkles cupcake",
+    price: 3.99
+  },
+  {
+    id: 3,
+    name: "Pink flower cupcake",
+    price: 5.99
+  }
+];
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(id, products) {
+    const item = products.find(item => item.id === id);
+    this.items.push(item);
+  }
+
+  getItems() {
+    return this.items;
+  }
+  
+  getCount() {
+    return this.items.length;
+  }
+}
+
+/*
+Let's create removeItem method that will take id its unique parameter.
+
+We will go through our items array to see if we have any matching ids first.
+
+We can use built in indexOf method method to find index number of the match, then use the splice built in method to remove the matched product.
+
+Here is an example:
+
+const index = arr.indexOf(id);
+myArr.splice(index, 1);
+*/
