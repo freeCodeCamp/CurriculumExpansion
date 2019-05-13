604081a2770e558f60da96029bbe337d30b6faeb
Phillip Shim <shimphillip@gmail.com>
2019-05-13 14:32:02 -0500

    Update index17.js

--- learn-data-structures-by-building-a-shopping-cart/index17.js
+++ learn-data-structures-by-building-a-shopping-cart/index17.js
@@ -64,4 +64,6 @@ You can use toFixed() method to round your number to the given decimal place. Th
 Use parseFloat() method to convert the string back to number. For ex:
 
 parseFloat(2.3999999999.toFixed(2))
+
+Ensure the returned value of applyDiscount's decimal number if fixed to 2 decimal numbers.
 */
