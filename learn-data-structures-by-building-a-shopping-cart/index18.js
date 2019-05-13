373c1ccb1c231091a86779fa158e24a5b159c462
Phillip Shim <shimphillip@gmail.com>
2019-05-13 15:18:50 -0500

    Create index18.js

---
+++ learn-data-structures-by-building-a-shopping-cart/index18.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 0,
+    name: "Vanilla buttercream cupcake",
+    price: 2.99
+  },
+  {
+    id: 1,
+    name: "French Macaroon",
+    price: 3.99
+  },
+  {
+    id: 2,
+    name: "Fruit sprinkles cupcake",
+    price: 3.99
+  },
+  {
+    id: 3,
+    name: "Pink flower cupcake",
+    price: 5.99
+  }
+];
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.discountPercentage = 30;
+  }
+
+  addItem(id, products) {
+    const item = products.find(item => item.id === id);
+    this.items.push(item);
+  }
+
+  getItems() {
+    return this.items;
+  }
+  
+  getCount() {
+    return this.items.length;
+  }
+  
+  removeItem(id) {
+    const index = this.items.indexOf(id);
+    this.items.splice(index, 1);
+  }
+  
+  clearCart() {
+    this.items = [];
+  }
+  
+  applyDiscount(amount) {
+    return parseFloat((this.discountPercentage / 100 * amount).toFixed(2));
+  }
+}
+
+/*
+
+We should also handle tax.
+
+Tax rates vary by each state so we will use texas rate of 8.25%. 
+
+Create a new state called taxRate in our constructor function and assign 8.25 to it.
+
+*/
