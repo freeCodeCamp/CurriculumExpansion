bb84d8b54c4cee7bbb210b982b4e4303ce49a6f0
Beau Carnes <beaucarnes@gmail.com>
2019-05-15 14:55:00 -0400

    update node server

---
+++ node-js-web-server/server.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = './public' + request.url;
+    if (filePath == './public/') {
+        filePath = './public/index.html';
+    }
+
+    console.log(filePath)
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'image/png'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            fs.readFile('./public/404.html', function(error, content) {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            }); 
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
