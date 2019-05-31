973c4ef070bc89ac200b728115cf8a7d48415606
Phillip Shim <shimphillip@gmail.com>
2019-05-31 10:05:26 -0500

    Update index02.js

--- learn-data-structures-by-building-a-shopping-cart/index02.js
+++ learn-data-structures-by-building-a-shopping-cart/index02.js
@@ -1,12 +1,8 @@
 const products = [];
 
 /*
-We have products array to hold our inventories.
-
-Because each product in our online pastry shop contain a lot of information, we will store it as an object which is another type of data structure. 
-
-Inside the products array, add an object with the property "name" and the value "Vanilla buttercream cupcake". 
-
+Because each product in our online pastry shop contains a lot of information, we will store it as an object which is another simple data structure.
+Inside the products array, add an object with the property "name" and the value "Vanilla buttercream cupcake".
 For example, here is an array that contains a similar object:
 
 const users = [
