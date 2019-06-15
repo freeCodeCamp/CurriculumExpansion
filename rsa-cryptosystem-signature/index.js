550c49d9c101a7d22a39d6f96c9c6dc37672d734
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2019-06-16 01:34:10 +0530

    feat: RSA cryptosystem signature project

---
+++ rsa-cryptosystem-signature/index.js
@@ -0,0 +1,105 @@
+"use strict";
+
+const signBtn = document.querySelector("#sign-btn");
+const transmitBtn = document.querySelector("#transmit-btn");
+const verifyBtn = document.querySelector("#verify-btn");
+const messageBox = document.querySelector("#message-to-be-sent");
+const receivedMessageBox = document.querySelector("#received-message");
+const generatedSignature = document.querySelector("#generated-signature");
+const receivedSignature = document.querySelector("#received-signature");
+const verificationStatus = document.querySelector("#verification-status");
+
+const firstPrime = 3;
+const secondPrime = 7;
+const N = firstPrime * secondPrime;
+const phiOfN = (firstPrime - 1) * (secondPrime - 1);
+let publicKey = 0;
+
+function hashTheMessage(message) {
+  let hashValue = 0;
+  for (let i = 0, msgLength = message.length; i < msgLength; ++i) {
+    hashValue += message.charCodeAt(i);
+  }
+  return hashValue % N === 0 ? 1 : hashValue % N;
+}
+
+function isCoPrime(smallerNum, largerNum) {
+  for (let i = 2; i <= smallerNum; ++i) {
+    if (smallerNum % i === 0 && largerNum % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function generatePrivateKey() {
+  for (let i = 2; i < phiOfN; ++i) {
+    if (isCoPrime(i, N) && isCoPrime(i, phiOfN)) {
+      return i;
+    }
+  }
+
+  console.log("Private key can't be generated.");
+  return 0;
+}
+
+function generatePublicKey(privateKey) {
+  publicKey = 1;
+  while (privateKey) {
+    if ((publicKey * privateKey) % phiOfN === 1 && privateKey !== publicKey) {
+      return;
+    }
+    ++publicKey;
+  }
+
+  console.log("Public key can't be generated.");
+  publicKey = 0;
+}
+
+function generateSignature(hashValue, privateKey) {
+  return Math.pow(hashValue, privateKey) % N;
+}
+
+function decryptSignature(digitalSignature) {
+  return Math.pow(digitalSignature, publicKey) % N;
+}
+
+signBtn.addEventListener("click", function() {
+  let hashValue = hashTheMessage(messageBox.value);
+  let privateKey = generatePrivateKey();
+  generatePublicKey(privateKey);
+
+  generatedSignature.innerText = generateSignature(
+    hashValue,
+    privateKey
+  ).toString();
+
+  transmitBtn.disabled = false;
+});
+
+messageBox.addEventListener("input", function() {
+  generatedSignature.innerText = "none";
+  transmitBtn.disabled = true;
+});
+
+transmitBtn.addEventListener("click", function() {
+  receivedMessageBox.value = messageBox.value;
+  receivedSignature.innerText = generatedSignature.textContent;
+  verificationStatus.innerText = "";
+  verifyBtn.disabled = false;
+});
+
+verifyBtn.addEventListener("click", function() {
+  let hashValue = hashTheMessage(receivedMessageBox.value);
+  let decryptedSignature = decryptSignature(
+    parseInt(receivedSignature.textContent)
+  );
+
+  if (hashValue === decryptedSignature) {
+    verificationStatus.innerText =
+      "Success! Signature is verified and Data is intact.";
+  } else {
+    verificationStatus.innerText =
+      "Failure! Looks like data has been modified.";
+  }
+});
