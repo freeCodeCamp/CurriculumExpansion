22bf7eec44d7b1ff5a4f72971108580f1e9a3f62
Beau Carnes <beaucarnes@gmail.com>
2019-06-27 13:54:11 -0400

    Restful API project

---
+++ restful-api-grocery-list/server000.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+/*
+We'll learn about RESTful APIs by building a grocery list app. It will be a backend server created with Express. RESTful APIs consist of a series of URLs that are used to create, read, update, and delete data. Each URL is called an API endpoint or request. The data sent back is called a response.
+
+Usually the data is stored in a database, but we'll be using an array. Use `let` to create a new empty array called "list".
+*/
---
+++ restful-api-grocery-list/server035.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let list = [];
+
+app.get('/', (req, res) => {
+  res.json(list);
+});
+
+app.post('/add/:item', (req, res) => {
+  list.push(req.params.item);
+  res.json("Added " + req.params.item);
+});
+
+/*
+Create a new API endpoint that responds to GET requests at the URL: `/:index`. 
+*/
