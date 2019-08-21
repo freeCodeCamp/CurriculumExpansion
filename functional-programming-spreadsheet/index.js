18369782f655ac57cbf23f42a008186b980d7b7b
SomeDer <48731521+SomeDer@users.noreply.github.com>
2019-08-21 07:04:15 +0100

    Update index.js to index990.js

--- functional-programming-spreadsheet/index.js
+++ functional-programming-spreadsheet/index.js
@@ -17,30 +17,33 @@ const highPrecedence = str => {
 };
 
 const isEven = num => num % 2 === 0;
-const average = nums => sum(nums) / nums.length;
 const sum = nums => nums.reduce((a, x) => a + x);
+const average = nums => sum(nums) / nums.length;
 
 const median = nums => {
-  const arr = [...nums];
-  const sorted = arr.sort((x, y) => x - y);
+  const sorted = nums.slice().sort((x, y) => x - y);
   const length = sorted.length;
   const middle = sorted.length / 2 - 1;
   return isEven(length)
     ? average([sorted[middle], sorted[middle + 1]])
     : sorted[middle + 0.5];
-};
+}; 
 
 const spreadsheetFunctions = {
   "": x => x,
-  range: ([x, y]) => range(x, y),
-  median,
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  increment: nums => nums.map(x => x + 1),
+  firsttwo = arr => arr.slice(0, 2),
+  lasttwo = arr => arr.slice(-2),
+  even: nums => nums.filter(isEven),
   sum,
   average,
-  increment: nums => nums.map(x => x + 1),
-  even: nums => nums.filter(isEven),
-  dropone: arr => arr.slice(1),
-  random: ([x, y]) => Math.floor(Math.random() * y + x),
-  firsttwo: arr => arr.slice(0, 2)
+  median,
+  has2: arr => arr.includes(2),
+  nodups: arr => arr.reduce((a, x) => a.includes(x) ? a : a.concat(x), []),
+  range: arr => range(...arr),
+  someeven: arr => arr.some(isEven),
+  everyeven: arr => arr.every(isEven)
 };
 
 const applyFn = str => {
@@ -58,18 +61,24 @@ const applyFn = str => {
   );
 };
 
-const range = (start, end) =>
-  start > end ? [] : [start].concat(range(start + 1, end));
+const range = (start, end) => Array(end - start + 1).fill(start).map((x, i) => x + i);
+
+/*
+Congragulations!
+You've finished this spreadsheet. Try inputing something like `=sum(range(1, 10))` into the spreadsheetFunctions.
+The function names are not case sensitive.
+*/
 
 const charRange = (start, end) =>
   range(start.charCodeAt(0), end.charCodeAt(0)).map(x =>
     String.fromCharCode(x)
   );
 
-const evalFormula = x => {
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
   const rangeFromString = (n1, n2) => range(parseInt(n1), parseInt(n2));
-  const elemValue = n => c => document.getElementById(c + n).value;
+  const elemValue = n => c => idToText(c + n);
   const addChars = c1 => c2 => n => charRange(c1, c2).map(elemValue(n));
   const varRangeExpanded = x.replace(rangeRegex, (_, c1, n1, c2, n2) =>
     rangeFromString(n1, n2).map(addChars(c1)(c2))
@@ -77,12 +86,12 @@ const evalFormula = x => {
   const varRegex = /[A-J][1-9][0-9]?/gi;
   const varExpanded = varRangeExpanded.replace(
     varRegex,
-    match => document.getElementById(match.toUpperCase()).value
+    match => idToText(match.toUpperCase())
   );
   const functionExpanded = applyFn(varExpanded);
   return functionExpanded === x
     ? functionExpanded
-    : evalFormula(functionExpanded);
+    : evalFormula(functionExpanded, cells);
 };
 
 window.onload = () => {
@@ -111,6 +120,9 @@ const update = event => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
   if (!value.includes(element.id) && value[0] === "=") {
-    element.value = evalFormula(value.substring(1), element.id);
+    element.value = evalFormula(
+      value.substring(1),
+      Array.from(document.getElementById("container").children)
+    );
   }
 };
