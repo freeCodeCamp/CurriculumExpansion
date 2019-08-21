a5808cbf254574fe896a2130e265185e4da3d641
SomeDer <48731521+SomeDer@users.noreply.github.com>
2019-08-21 17:09:22 +0100

    Break down index070.js

--- functional-programming-spreadsheet/index070.js
+++ functional-programming-spreadsheet/index070.js
@@ -5,6 +5,5 @@ const infixToFunction = {
 const addVar = (x, y) => x + y;
 
 /*
-Replace `addVar` with its value in `infixToFunction` and remove its definition.
-Because `addVar` is immutable we can always replace it with its definition (whether in reasoning or in code) - we don't have to keep track of its value.
+In `infixToFunction`, replace `addVar` with `(x, y) => x + y`.
 */
---
+++ functional-programming-spreadsheet/index075.js
@@ -0,0 +1,9 @@
+const infixToFunction = {
+  "+": (x, y) => x + y
+};
+
+const addVar = (x, y) => x + y;
+
+/*
+Remove the now redundant `addVar` definition. 
+*/
