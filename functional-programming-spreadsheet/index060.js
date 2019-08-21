b2475a87ed11695f5f12afff2f09614f211139fa
SomeDer <48731521+SomeDer@users.noreply.github.com>
2019-08-21 15:27:14 +0100

    Rephrase index060.js

--- functional-programming-spreadsheet/index060.js
+++ functional-programming-spreadsheet/index060.js
@@ -3,5 +3,5 @@ const infixToFunction = {};
 const addVar = (x, y) => x + y;
 
 /*
-Add a key `+` to `infixToFunction` and assign `addVar` to it.
+Add a key `+` to `infixToFunction` and assign it the value `addVar`.
 */
