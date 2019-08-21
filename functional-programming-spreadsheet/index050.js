57fca774ab89947fed306b2db319e6b543bf78a7
SomeDer <48731521+SomeDer@users.noreply.github.com>
2019-08-21 15:26:29 +0100

    Change index050.js

--- functional-programming-spreadsheet/index050.js
+++ functional-programming-spreadsheet/index050.js
@@ -8,6 +8,7 @@ const addVar = function(x, y) {
 This is possible because the anonymous function has been immediately assigned to a value - this is effectively the same as using a named function.
 Rewrite the above using ES6's arrow syntax:
 ```
-const fn = (x, y) => x; // value is returned implicitly
+const fn = (x, y) => x;
 ```
+Note that the value is returned implicitly.
 */
