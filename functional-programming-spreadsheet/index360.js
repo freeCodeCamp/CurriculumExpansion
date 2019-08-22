a26072f62bbd494c689c94b00dce94c198089ec6
SomeDer <48731521+SomeDer@users.noreply.github.com>
2019-08-22 08:36:16 +0100

    rephrase index360.js

--- functional-programming-spreadsheet/index360.js
+++ functional-programming-spreadsheet/index360.js
@@ -44,6 +44,6 @@ const charRange = (start, end) =>
   );
 
 /*
-Create a new function `evalFormula` which takes a single argument.
-Set `/([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi` to `rangeRegex`
+Create a new function `evalFormula` which takes a single argument, `x`.
+Set `/([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi` to a variable named `rangeRegex`.
 */
