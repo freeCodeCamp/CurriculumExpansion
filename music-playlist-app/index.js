5b8aa6395ec566e0450eca98bfa9f9c56a6fa964
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-17 16:52:10 -0700

    feat: adding comments for concepts covered

--- music-playlist-app/index.js
+++ music-playlist-app/index.js
@@ -1,12 +1,15 @@
+const displayCurrentSong = document.querySelector(".text-container");
+const playlistContainer = document.querySelector(".playlist-container");
+
+// This is the first introduction to getElementById
 const playPauseBtn = document.getElementById("play-btn");
 const previousBtn = document.getElementById("previous-btn");
 const nextBtn = document.getElementById("next-btn");
-const displayCurrentSong = document.querySelector(".text-container");
-const playlistContainer = document.querySelector(".playlist-container");
 const shuffleBtn = document.getElementById("shuffle-btn");
 let isPlaying = false;
 let currentSong = 0;
 
+// This could be a nice review for objects nested inside arrays and how to access those properties
 let playlist = [
   {
     id: "We-Are-Going-to-Make-it",
@@ -40,8 +43,19 @@ let playlist = [
   },
 ];
 
+// First mention of the Audio constructor
 let song = new Audio(playlist[currentSong].audio);
 
+/**
+ *
+ * This is the first introduction to map.
+ * We could stress how map is being used here to create a new array to render this HTML.
+ * I believe this is also the first time innerHTML is being used. Maybe we could point out the differences between textContent and innerHTML.
+ * Lastly, this is the first time template literals are being taught.
+ * This would be the time to teach string interpolation
+ *
+ *
+ */
 function createPlaylist() {
   playlist.map(
     (song, id) =>
@@ -60,6 +74,7 @@ function createPlaylist() {
 }
 createPlaylist();
 
+// This is the first time teaching default parameters.
 function displaySong(song = "", artist = "") {
   displayCurrentSong.innerHTML = `
   <p class="song">${song}</p>
@@ -69,6 +84,8 @@ function displaySong(song = "", artist = "") {
 
 function playPauseSong() {
   isPlaying ? song.pause() : song.play();
+
+  // This might be the first time teaching the logical NOT operator.
   isPlaying = !isPlaying;
   displaySong(playlist[currentSong].title, playlist[currentSong].artist);
 }
@@ -96,6 +113,7 @@ function shufflePlaylist() {
     let randomIndex = Math.floor(Math.random() * playlistLength);
     playlistLength--;
 
+    // First time teaching array destructing.
     [playlist[playlistLength], playlist[randomIndex]] = [
       playlist[randomIndex],
       playlist[playlistLength],
@@ -113,6 +131,7 @@ function shufflePlaylist() {
 }
 
 function deleteSong() {
+  // This is the first time where forEach and addEventListener is being introduced
   document.querySelectorAll(".delete-btn").forEach(function (btn, songId) {
     btn.addEventListener("click", function () {
       let deleteSong = confirm("Are you sure you want to delete this song?");
@@ -121,6 +140,7 @@ function deleteSong() {
         isPlaying = false;
         displaySong();
         document.getElementById(`song${songId}`).remove();
+        // This might be the first time where splice is introduced
         playlist.splice(songId, 1);
       }
     });
@@ -133,6 +153,7 @@ function chooseSong() {
     songDiv.addEventListener("click", function () {
       song.pause();
       isPlaying = false;
+      // This is the first mention of findIndex
       currentSong = playlist.findIndex((obj) => obj.id === songDiv.id);
       song = new Audio(playlist[currentSong].audio);
       playPauseSong();
