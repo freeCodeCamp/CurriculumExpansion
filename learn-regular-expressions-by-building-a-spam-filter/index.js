57af3b0e2e35acf8d9873dde3b7ab898e346c692
jdwilkin4 <jwilkin4@hotmail.com>
2022-09-06 00:25:18 -0700

    refactor: removing spam phrase array

--- learn-regular-expressions-by-building-a-spam-filter/index.js
+++ learn-regular-expressions-by-building-a-spam-filter/index.js
@@ -2,20 +2,20 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const spamPhrases = [
-  "free money",
-  "work from home",
-  "stock alert",
-  "dear friend",
-  "1 million dollars",
-  "PLEASE ASSIST ME",
-];
+/**
+ * I read through the previous discussions for this project
+ * and we can start with matches for literal phrases and then have the
+ * campers create more complex regex which seemed to be the original intent for this project.
+ *
+ * Ex. fr33 m0ney would result in true for the spam filter
+ *
+ *
+ */
 
 const blacklistRegexps = [
   /[0-9] hundred|thousand|million dollars/i,
   /PLEASE HELP|ASSIST ME/i,
   /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i,
-  /(?:^|\s)w[o0]rk fr[o0]m h[o0]m[e3](?:$|\s)/i,
   /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i,
   /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i,
 ];
