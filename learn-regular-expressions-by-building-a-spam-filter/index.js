b4cb6ef3c9ae58ead5fce4d1abf1cf5e7ce274f7
jdwilkin4 <jwilkin4@hotmail.com>
2022-09-06 00:04:48 -0700

    fix: removing unused functions

--- learn-regular-expressions-by-building-a-spam-filter/index.js
+++ learn-regular-expressions-by-building-a-spam-filter/index.js
@@ -2,33 +2,25 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const wordCondenser = /(?:^|\s)\S(?:(\s+)\S)(?:\1\S)*(?:$|\s)/g;
-const spaceCondenser = /\s{2,}/g;
-
 const spamPhrases = [
   "free money",
   "work from home",
   "stock alert",
   "dear friend",
+  "1 million dollars",
+  "PLEASE ASSIST ME",
 ];
 
-const condenseSpaces = (msg) => {
-  return msg
-    .replace(wordCondenser, (m) => m.replace(/\s+/g, ""))
-    .replace(spaceCondenser, " ");
-};
-
 const blacklistRegexps = [
+  /[0-9] hundred|thousand|million dollars/i,
+  /PLEASE HELP|ASSIST ME/i,
   /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i,
   /(?:^|\s)w[o0]rk fr[o0]m h[o0]m[e3](?:$|\s)/i,
   /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i,
   /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i,
 ];
 
-const isSpam = (msg) => {
-  const spacesCondensed = condenseSpaces(msg);
-  return blacklistRegexps.some((re) => re.test(spacesCondensed));
-};
+const isSpam = (msg) => blacklistRegexps.some((re) => re.test(msg));
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
