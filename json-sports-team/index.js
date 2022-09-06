7ba17ed0cb1f8f8264b6c68f71d83c029e510168
jdwilkin4 <jwilkin4@hotmail.com>
2022-09-05 19:22:05 -0700

    fix: applying code review changes

--- json-sports-team/index.js
+++ json-sports-team/index.js
@@ -1,213 +1,204 @@
 const teamName = document.getElementById("team");
-const sport = document.getElementById("sport");
-const year = document.getElementById("year");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const matches = document.getElementById("matches");
 const playerCards = document.getElementById("player-cards");
 const selectElement = document.getElementById("players");
 
-/**
- * I don't know if we want them to build out this JSON data or just supply it to them in the first step.
- * If we supply them with the myTeam JSON then we can have the first few steps be about reviewing how to access property values.
- * Ex. console.log(myTeam.Sport);
- * We could have them review dot and bracket notation.
- * This will also be good review for nested objects.
- * Ex. console.log(myTeam["Head Coach"].Name);
- */
-
-const myTeam = {
-  Team: "Argentina",
-  Sport: "Football",
-  Year: 1986,
-  "Is World Cup Winner": true,
-  "Head Coach": {
-    Name: "Carlos Bilardo",
-    Matches: 7,
+const myFavoriteFootballTeam = {
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
   },
-  Players: [
+  players: [
     {
-      Name: "Sergio Almirón",
-      Position: "FW",
-      Number: 1,
+      name: "Sergio Almirón",
+      position: "forward",
+      number: 1,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Sergio Batista",
-      Position: "MF",
-      Number: 2,
+      name: "Sergio Batista",
+      position: "midfielder",
+      number: 2,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Ricardo Bochini",
-      Position: "MF",
-      Number: 3,
+      name: "Ricardo Bochini",
+      position: "midfielder",
+      number: 3,
       isCaptain: false,
-      Nickname: "El Bocha",
+      nickname: "El Bocha",
     },
     {
-      Name: "Claudio Borghi",
-      Position: "MF",
-      Number: 4,
+      name: "Claudio Borghi",
+      position: "midfielder",
+      number: 4,
       isCaptain: false,
-      Nickname: "Bichi",
+      nickname: "Bichi",
     },
     {
-      Name: "José Luis Brown",
-      Position: "DF",
-      Number: 5,
+      name: "José Luis Brown",
+      position: "defender",
+      number: 5,
       isCaptain: false,
-      Nickname: "Tata",
+      nickname: "Tata",
     },
     {
-      Name: "Daniel Passarella",
-      Position: "DF",
-      Number: 6,
+      name: "Daniel Passarella",
+      position: "defender",
+      number: 6,
       isCaptain: false,
-      Nickname: "El Gran Capitán",
+      nickname: "El Gran Capitán",
     },
     {
-      Name: "Jorge Burruchaga",
-      Position: "FW",
-      Number: 7,
+      name: "Jorge Burruchaga",
+      position: "forward",
+      number: 7,
       isCaptain: false,
-      Nickname: "Burru",
+      nickname: "Burru",
     },
     {
-      Name: "Néstor Clausen",
-      Position: "DF",
-      Number: 8,
+      name: "Néstor Clausen",
+      position: "defender",
+      number: 8,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "José Luis Cuciuffo",
-      Position: "DF",
-      Number: 9,
+      name: "José Luis Cuciuffo",
+      position: "defender",
+      number: 9,
       isCaptain: false,
-      Nickname: "El Cuchu",
+      nickname: "El Cuchu",
     },
     {
-      Name: "Diego Maradona",
-      Position: "MF",
-      Number: 10,
+      name: "Diego Maradona",
+      position: "midfielder",
+      number: 10,
       isCaptain: true,
-      Nickname: "El Pibe de Oro",
+      nickname: "El Pibe de Oro",
     },
     {
-      Name: "Jorge Valdano",
-      Position: "FW",
-      Number: 11,
+      name: "Jorge Valdano",
+      position: "forward",
+      number: 11,
       isCaptain: false,
-      Nickname: "The Philosopher of Football",
+      nickname: "The Philosopher of Football",
     },
     {
-      Name: "Héctor Enrique",
-      Position: "MF",
-      Number: 12,
+      name: "Héctor Enrique",
+      position: "midfielder",
+      number: 12,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Oscar Garré",
-      Position: "DF",
-      Number: 13,
+      name: "Oscar Garré",
+      position: "defender",
+      number: 13,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Ricardo Giusti",
-      Position: "MF",
-      Number: 14,
+      name: "Ricardo Giusti",
+      position: "midfielder",
+      number: 14,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Luis Islas",
-      Position: "GK",
-      Number: 15,
+      name: "Luis Islas",
+      position: "goalkeeper",
+      number: 15,
       isCaptain: false,
-      Nickname: "El loco",
+      nickname: "El loco",
     },
     {
-      Name: "Julio Olarticoechea",
-      Position: "DF",
-      Number: 16,
+      name: "Julio Olarticoechea",
+      position: "defender",
+      number: 16,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Pedro Pasculli",
-      Position: "FW",
-      Number: 17,
+      name: "Pedro Pasculli",
+      position: "forward",
+      number: 17,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Nery Pumpido",
-      Position: "GK",
-      Number: 18,
+      name: "Nery Pumpido",
+      position: "goalkeeper",
+      number: 18,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Oscar Ruggeri",
-      Position: "DF",
-      Number: 19,
+      name: "Oscar Ruggeri",
+      position: "defender",
+      number: 19,
       isCaptain: false,
-      Nickname: "El Cabezón",
+      nickname: "El Cabezón",
     },
     {
-      Name: "Carlos Tapia",
-      Position: "MF",
-      Number: 20,
+      name: "Carlos Tapia",
+      position: "midfielder",
+      number: 20,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
     {
-      Name: "Marcelo Trobbiani",
-      Position: "MF",
-      Number: 21,
+      name: "Marcelo Trobbiani",
+      position: "midfielder",
+      number: 21,
       isCaptain: false,
-      Nickname: "Calesita",
+      nickname: "Calesita",
     },
     {
-      Name: "Héctor Zelada",
-      Position: "GK",
-      Number: 22,
+      name: "Héctor Zelada",
+      position: "goalkeeper",
+      number: 22,
       isCaptain: false,
-      Nickname: null,
+      nickname: null,
     },
   ],
 };
 
 // this could be a good review of destructuring
-const { Sport, Team, Year, Players } = myTeam;
-const { Name } = myTeam["Head Coach"];
+const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
 
-sport.innerHTML = Sport;
-teamName.innerHTML = Team;
-year.innerHTML = Year;
-headCoach.innerHTML = Name;
+typeOfSport.innerHTML = sport;
+teamName.innerHTML = team;
+worldCupYear.innerHTML = year;
+headCoach.innerHTML = coachName;
 
 const setPlayerCards = (arr) => {
   playerCards.innerHTML += arr
     .map(
-      ({ Name, Position, Number, isCaptain, Nickname }) =>
+      ({ name, position, number, isCaptain, nickname }) =>
         `
         <div class="player-card">
-          <h2>${Name} ${isCaptain ? "(Captain)" : ""}</h2>
-          <p>Position: ${Position}</p>
-          <p>Number: ${Number}</p>
-          <p>Nickname: ${Nickname !== null ? Nickname : "N/A"}</p>
+          <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
       `
     )
     .join("");
 };
 
-setPlayerCards(Players);
+setPlayerCards(players);
 
 selectElement.addEventListener("change", (e) => {
   // maybe we could have users print the results of the values for the select options
@@ -218,27 +209,27 @@ selectElement.addEventListener("change", (e) => {
   //this might be the first time a switch statement is used
   switch (e.target.value) {
     case "nickname":
-      newArr = Players.filter((player) => player.Nickname !== null);
+      newArr = players.filter((player) => player.nickname !== null);
       setPlayerCards(newArr);
       break;
-    case "fw":
-      newArr = Players.filter((player) => player.Position === "FW");
+    case "forward":
+      newArr = players.filter((player) => player.position === "forward");
       setPlayerCards(newArr);
       break;
-    case "mf":
-      newArr = Players.filter((player) => player.Position === "MF");
+    case "midfielder":
+      newArr = players.filter((player) => player.position === "midfielder");
       setPlayerCards(newArr);
       break;
-    case "df":
-      newArr = Players.filter((player) => player.Position === "DF");
+    case "defender":
+      newArr = players.filter((player) => player.position === "defender");
       setPlayerCards(newArr);
       break;
-    case "gk":
-      newArr = Players.filter((player) => player.Position === "GK");
+    case "goalkeeper":
+      newArr = players.filter((player) => player.position === "goalkeeper");
       setPlayerCards(newArr);
       break;
     default:
-      setPlayerCards(Players);
+      setPlayerCards(players);
   }
 });
 
@@ -249,8 +240,8 @@ selectElement.addEventListener("change", (e) => {
  * This will help users compare the outputs and understand how these methods work.
  *
  *
- *  console.log(myTeam);
- *  const stringifiedJSON = JSON.stringify(myTeam);
+ *  console.log(myFavoriteFootballTeam);
+ *  const stringifiedJSON = JSON.stringify(myFavoriteFootballTeam);
  *  console.log(stringifiedJSON);
  *  console.log(JSON.parse(stringifiedJSON));
  *
