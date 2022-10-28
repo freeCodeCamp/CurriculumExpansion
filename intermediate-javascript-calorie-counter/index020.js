741668f0bd7a7f9cc965e1655776890d4fab770f
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-10-28 16:01:20 +0900

    feat: update calorie counter project (#321)
    * feat: update calorie counter project
    
    * fix: change error to isError based on review
    
    * fix: add comment on first time teaching preventDefault()
    
    * fix: add comment on first time teaching alert()
    
    * fix: change zeroed index to entryNumber starting from 1
    
    * fix: simplify HTML and CSS
    
    * fix: rename directory to javascript-calorie-counter
    
    * fix: add note about entry numbers for later steps
    
    * feat: improve output with colors and more details
    
    * fix: input values lost when appending inputs and labels to an input container
    
    Co-authored-by: Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>

--- intermediate-javascript-calorie-counter/index020.js
+++
@@ -1,9 +0,0 @@
-// console.log(document);
-
-/*
-In our HTML document, we have a form element with an `id` attribute: `<form id="calorie-form">`
-
-To reference and access this particular form in JavaScript, we can use the getElementById() method on the document and provide the ID.
-
-The code `document.getElementById('my-form')` gets a reference to an HTML element with an `id` of `my-form`. Get a reference to the HTML element with the `id` of `calorie-form`.
-*/
