8e75b967abbb9f588a84324392055e3fefb651db
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2023-04-26 12:52:34 -0700

    feat: adding Learn Intermediate OOP by Building a Platformer Game (#336)
    * feat: adding OOP platformer project
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Estefania Cassingena Navone <85124911+estefaniacn@users.noreply.github.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/styles.css
    
    Co-authored-by: Estefania Cassingena Navone <85124911+estefaniacn@users.noreply.github.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Estefania Cassingena Navone <85124911+estefaniacn@users.noreply.github.com>
    
    * fix: applying code review changes
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/index.html
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * refactor: applying rest of code review changes
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Update javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    ---------
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    Co-authored-by: Estefania Cassingena Navone <85124911+estefaniacn@users.noreply.github.com>

---
+++ javascript-algorithms-and-data-structures/Learn-Intermediate-OOP-by-Building-a-Platformer-Game/script.js
@@ -0,0 +1,269 @@
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let activeCheckpointCollisionDetection = true;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 10,
+      y: 400,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 40;
+    this.height = 40;
+  }
+  draw() {
+    ctx.fillStyle = "#99c9ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    //this condition stops the player from falling past the canvas screen's height
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      // this condition stops the player from jumping above the screen
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+    if (this.position.x < this.width) {
+      this.position.x = this.width;
+    }
+  }
+}
+
+class Platform {
+  constructor(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 200;
+    this.height = 40;
+  }
+  draw() {
+    ctx.fillStyle = "#acd157";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+class CheckPoint {
+  constructor(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 40;
+    this.height = 70;
+  }
+  draw() {
+    ctx.fillStyle = "#f1be32";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  claim() {
+    this.width = 0;
+    this.height = 0;
+    this.position.y = Infinity;
+  }
+}
+
+const player = new Player();
+const platformPositions = [
+  { x: 500, y: 450 },
+  { x: 700, y: 400 },
+  { x: 850, y: 350 },
+  { x: 900, y: 350 },
+  { x: 1050, y: 150 },
+  { x: 2500, y: 450 },
+  { x: 2900, y: 400 },
+  { x: 3150, y: 350 },
+  { x: 3900, y: 450 },
+  { x: 4200, y: 400 },
+  { x: 4400, y: 200 },
+  { x: 4700, y: 150 },
+];
+
+const platforms = platformPositions.map(
+  (platform) => new Platform(platform.x, platform.y)
+);
+
+const checkpointPositions = [
+  { x: 1170, y: 80 },
+  { x: 2900, y: 330 },
+  { x: 4800, y: 80 },
+];
+
+const checkpoints = checkpointPositions.map(
+  (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y)
+);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
+
+  checkpoints.forEach((checkpoint) => {
+    checkpoint.draw();
+  });
+
+  player.update();
+
+  if (keys.rightKey.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.leftKey.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+
+    if (keys.rightKey.pressed && activeCheckpointCollisionDetection) {
+      platforms.forEach((platform) => {
+        platform.position.x -= 5;
+      });
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x -= 5;
+      });
+    } else if (keys.leftKey.pressed && activeCheckpointCollisionDetection) {
+      platforms.forEach((platform) => {
+        platform.position.x += 5;
+      });
+      checkpoints.forEach((checkpoint) => {
+        checkpoint.position.x += 5;
+      });
+    }
+  }
+
+  platforms.forEach((platform) => {
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y,
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+    ];
+
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+
+    if (platformDetectionRules.every((rule) => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
+    }
+  });
+
+  checkpoints.forEach((checkpoint, index) => {
+    if (
+      player.position.x >= checkpoint.position.x &&
+      player.position.y >= checkpoint.position.y &&
+      player.position.y + player.height <=
+        checkpoint.position.y + checkpoint.height &&
+      activeCheckpointCollisionDetection
+    ) {
+      checkpoint.claim();
+      if (index === checkpoints.length - 1) {
+        activeCheckpointCollisionDetection = false;
+        showCheckpointScreen("You reached the final checkpoint!");
+        movePlayer("ArrowRight", 0, false);
+      } else if (
+        player.position.x >= checkpoint.position.x &&
+        player.position.x <= checkpoint.position.x + 40
+      ) {
+        showCheckpointScreen("You reached a checkpoint!");
+      }
+    }
+  });
+};
+
+const keys = {
+  rightKey: {
+    pressed: false,
+  },
+  leftKey: {
+    pressed: false,
+  },
+};
+
+const movePlayer = (key, xVelocity, isPressed) => {
+  if (!activeCheckpointCollisionDetection) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+    return;
+  }
+  switch (key) {
+    case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity;
+      break;
+
+    case "ArrowUp":
+    // older browsers might use spacebar instead of an empty string
+    case " ":
+    case "Spacebar":
+      player.velocity.y -= 8;
+      break;
+
+    case "ArrowRight":
+      keys.rightKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x += xVelocity;
+  }
+};
+
+const startGame = () => {
+  // show the game and hide instructions screen
+  canvas.style.display = "block";
+  startScreen.style.display = "none";
+  animate();
+};
+
+const showCheckpointScreen = (msg) => {
+  checkpointScreen.style.display = "block";
+  checkpointMessage.textContent = msg;
+  if (activeCheckpointCollisionDetection) {
+    setTimeout(() => (checkpointScreen.style.display = "none"), 2000);
+  }
+};
+
+startBtn.addEventListener("click", startGame);
+
+window.addEventListener("keydown", ({ key }) => {
+  movePlayer(key, 8, true);
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  movePlayer(key, 0, false);
+});
