8db9b668a116d824cc947e42e8e308e2ff0fd415
Joy Shaheb <istiaque.joy@northsouth.edu>
2023-09-06 20:48:36 +0600

    fix/learn-to-build-music-player-app (#344)
    * started working on the mucis player
    
    * fix: comment spacing
    
    * songs display and shuffle button simplified
    
    * resetButton added
    
    * work in progress
    
    * play pause button functions added
    
    * play pause remember time of play
    
    * previous and next song logic added
    
    * fix: simplify and adjust existing styling, adjust classes and ids in HTML
    
    * fix: add ids to remaining player buttons
    
    * improvement on next prev songs
    
    * fixed next and prev song buttons
    
    * player display and song highlighter added
    
    * fix: remove unused div, adjust spacing for parallel lines
    
    * fix: remove notes from styling, add width and height to delete SVG
    
    * fix: restore previous functionality, use ids to play and delete songs
    
    * feat: incorporate Bruce's changes
    
    * fix: deleting songs, move button component into the song li, increase
    height of playlist song button
    
    * chore: add latest code from main to script.js for easy comparison, comment out unused code
    
    * fix: rename btn to button, reset button aria-label text
    
    * fix: mirror Bruce's change to add accessible name to delete button
    
    * fix: simplify next and previous song functions, rename variables, and prevent functions from looping through playlist
    
    * feat: add function to add/update accessible text for the play button
    
    * fix: remove playing class from play button whenever audio is paused
    
    * feat: rename playerDisplay function to setPlayerDisplay, make it more flexible
    
    * fix: rename updatePlayButtonAccessibleText to setPlayButtonAccessibleText
    
    * chore: rename playlist element HTML and JS for clarity, remove
    aria-labeledby attribute
    
    * chore: cleanup, add logic to setPlayButtonAccessibleText function to fix reset button
    
    * fix: remove extra files, move newScript code into script file, remove comment in HTML
    
    * fix: move script tag back to before closing body tag
    
    * fix: remove cover art from JS code
    
    ---------
    
    Co-authored-by: scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>

--- javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
+++ javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
@@ -1,384 +1,253 @@
- //  Songs array creation
- const songs = [
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
+
+const allSongs = [
   {
-    id:"0",
+    id: "0",
     title: "Scratching The Surface",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "4:25",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
   },
   {
-    id:"1",
+    id: "1",
     title: "Can't Stay Down",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "4:15",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
   },
   {
-    id:"2",
+    id: "2",
     title: "Still Learning",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:51",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
   },
   {
-    id:"3",
+    id: "3",
     title: "Cruising for a Musing",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:34",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
   },
   {
-    id:"4",
+    id: "4",
     title: "Never Not Favored",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:35",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
   },
   {
-    id:"5",
+    id: "5",
     title: "From the Ground Up",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:12",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
   },
   {
-    id:"6",
+    id: "6",
     title: "Walking on Air",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:25",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
   },
   {
-    id:"7",
+    id: "7",
     title: "Can't Stop Me. Can't Even Slow Me Down.",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:52",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
   },
   {
-    id:"8",
+    id: "8",
     title: "The Surest Way Out is Through",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "3:10",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
   },
   {
-    id:"9",
+    id: "9",
     title: "Chasing That Feeling",
-    cover:
-      "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/quincy-larson-album-art.jpg",
     artist: "Quincy Larson",
     duration: "2:43",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
   },
 ];
 
-// State Management: useState Hook
-function useState(array) {
-  let state = array;
-  function getState() {
-    return state;
-  }
-  function setState(newState) {
-    state = newState;
-  }
-  return [getState, setState];
-}
-
-function renderSongs(array) {
-  // map Array Method
-  const songList = array.map((song, index) => {
-    return `
-        <li id=${song.id} class="playlist-song" ${index === currentIndex() && `aria-current="true"`}>
-              <button class="playlist-song-info">
-                  <span class="playlist-song-title">${song.title}</span>
-                  <span class="playlist-song-artist">${song.artist}</span>
-                  <span class="playlist-song-duration">${song.duration}</span>
-              </button>
-              <button class="playlist-song-delete" aria-label="delete ${song.title}">
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle id="delete-Btn" cx="8" cy="8" r="8" fill="#4d4d62"/>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/>
-                </svg>
-              </button>
-        </li>
-      `;
-  }).join('');
-
-  songListDiv.innerHTML = songList;
-
-}
-
-
-// song playlist
-const songListDiv = document.getElementById("playlist-songs");
-const playPath = document.getElementById('play');
-const closeBtn = document.querySelector(".playlist-close");
-const arrow = document.querySelector(".arrow");
-
-// audio API
 const audio = new Audio();
+let userData = {
+  songs: allSongs,
+  currentSong: null,
+  songCurrentTime: 0,
+};
 
-const [playlist,setPlaylist] = useState([...songs]);
+const deleteSong = (id) => {
+  if (userData?.currentSong?.id === id.toString()) {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
 
-const [currentIndex,setCurrentIndex] = useState(0);
+    pauseSong();
+    setPlayerDisplay();
+  }
 
-const [currentTime,setCurrentTime] = useState(0);
+  userData.songs = userData?.songs.filter((song) => song.id !== id.toString());
+  renderSongs(userData?.songs);
+  setPlayButtonAccessibleText();
 
-const [currentSong,setCurrentSong] = useState(playlist()[currentIndex()]);
+  if (userData.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
 
-// first time the matchMedia is being taught. Make sure to introduce with example in the steps
-let view = window.matchMedia("(max-width: 700px)");
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
 
-function playSong(song) {
-  // using indesOf method
-  setCurrentIndex(playlist().indexOf(song));
+    resetButton.addEventListener("click", () => {
+      userData.songs = allSongs;
 
-  playPath.classList.add('playing');
+      renderSongs(userData?.songs);
+      resetButton.remove();
+    });
+  }
+};
 
+const setPlayerDisplay = (title, artist) => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+
+  playingSong.textContent = title ? title : "";
+  songArtist.textContent = artist ? artist : "";
+};
+
+const songHighlighter = (id) => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(`song-${id}`);
+
+  playlistSongElements.forEach((song) => {
+    song.removeAttribute("aria-current");
+  });
+
+  songToHighlight.setAttribute("aria-current", "true");
+};
+
+const renderSongs = (array) => {
+  const songsHTML = array
+    .map((song) => {
+      return `
+      <li id="song-${song.id}" class="playlist-song">
+        <button class="playlist-song-info" onclick="playSong(${song.id})">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+        </button>
+        <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/>
+          </svg>
+        </button>
+      </li>
+    `;
+    })
+    .join("");
+
+  playlistSongs.innerHTML = songsHTML;
+};
+
+const playSong = (songId) => {
+  const song = userData?.songs.find((song) => song.id === songId.toString());
   audio.src = song.src;
   audio.title = song.title;
-  audio.currentTime = currentTime();
 
-  audio.play();
-  setCurrentSong(song.title);
-
-  updatePlayingSongBackground(currentIndex());
-  renderSongDisplay(currentIndex());
-}
-
-function pauseSong() {
-  audio.pause();
-  setCurrentTime(audio.currentTime);
-  playPath.classList.remove('playing');
-}
-
-function togglePlay() {
-  playSong(playlist()[currentIndex()]);
-  updatePlayingSongBackground(currentIndex());
-}
-
-function toggleClosePlaylist() {
-    arrow.classList.toggle('active');
-    if (songListDiv.style.visibility === "hidden") {
-        songListDiv.style.visibility = "visible";
-        songListDiv.style.display = "flex";
-        closeBtn.setAttribute('aria-expanded', true);
-        return;
-
-    } else {
-        songListDiv.style.visibility = "hidden";
-        songListDiv.style.display = "none";
-        closeBtn.setAttribute('aria-expanded', false);
-    }
-}
-
-// player controls
-function nextSong() {
-  const nextIndex = (currentIndex() + 1) % playlist().length;
-  setCurrentTime(0);
-  clearSongBgs();
-  playSong(playlist()[nextIndex]);
-  setCurrentIndex(nextIndex);
-
-//   keeping keyboard indicator in sync with array keyls/selected indicator.
-  if(document.activeElement.classList.contains('playlist-song-info')) {
-    const nextSongDivs = document.querySelectorAll(".playlist-song-info");
-    nextSongDivs[currentIndex()].focus();
-  }
-}
-
-function previousSong() {
-  const prevIndex = (currentIndex() - 1 + playlist().length) % playlist().length;
-  setCurrentTime(0);
-  clearSongBgs();
-  playSong(playlist()[prevIndex]);
-  setCurrentIndex(prevIndex);
-
-//   keeping keyboard indicator in sync with array keyls/selected indicator.
-  if(document.activeElement.classList.contains('playlist-song-info')) {
-      const previousSongDivs = document.querySelectorAll(".playlist-song-info");
-      previousSongDivs[currentIndex()].focus();
-    }
-}
-
-function renderSongDisplay(currentIndex) {
-  document.getElementById("player-song-title").innerText = playlist()[currentIndex].title;
-  document.getElementById("player-song-artist").innerText = playlist()[currentIndex].artist;
-  document.getElementById("player-album-art").innerHTML = `<img src="${playlist()[currentIndex].cover}" alt="song cover art" />`;
-}
-
-
-// playlist display
-function clearSongBgs() {
-    document.querySelectorAll(".playlist-song").forEach(song => {
-        song.removeAttribute('aria-current');
-    });
-}
-
-function updatePlayingSongBackground(newIndex) {
-    const updateSongDivs = document.querySelectorAll(".playlist-song");
-    updateSongDivs[newIndex].setAttribute('aria-current', true);
-}
-
-function playSelectedSong(song, songBtn) {
-  playSong(song);
-  clearSongBgs();
-  songBtn.setAttribute('aria-current', true);
-}
-
-
-function shuffle() {
-  // first time the spread operator is being taught. Make sure to introduce with example in the steps
-  let songsCopy = [...playlist()];
-  // using array sort method and Math.random method
-  songsCopy.sort(() => 0.5 - Math.random());
-  renderSongs(songsCopy);
-  songPlayingInList()
-  setPlaylist(songsCopy);
-  clearSongBgs();
-  updatePlayingSongBackground(currentIndex());
-  setDeleteEventListener();
-  setSongEventListener();
-}
-
-function songPlayingInList() {
-  document.querySelectorAll('.playlist-song').forEach((songDiv,index) => {
-    let songPlayingInList = songDiv.querySelector('.playlist-song-info').firstElementChild.innerText;
-    if (songPlayingInList === currentSong()) {
-      setCurrentIndex(index);
-    }
-  });
-}
-
-function resetButton() {
-  let resetBtn = document.createElement('button');
-  let resetText = document.createTextNode('Reset Playlist');
-  resetBtn.appendChild(resetText);
-  resetBtn.ariaLabel = 'reset playlist';
-  resetBtn.classList.add('player-reset-btn');
-  resetBtn.onclick = function () {
-  renderSongs(songs);
-  songPlayingInList()
-  setPlaylist([...songs]);
-  clearSongBgs();
-  updatePlayingSongBackground(currentIndex());
-  setDeleteEventListener();
-  setSongEventListener();
-  };
-
-  return resetBtn;
-}
-
-
-function deleteSong(song){
-
-  if(currentSong() === playlist()[song].title){
-    audio.pause();
-  }
-
-  if(song === playlist().length - 1){
-    // first time splice is introduced. make sure to provide explanation and example in the steps.
-    playlist().splice(song);
-  }
-
-  playlist().splice(song,1);
-  renderSongs(playlist());
-  songPlayingInList()
-  setPlaylist(playlist());
-  clearSongBgs();
-
-  if(playlist().length > 0){
-    updatePlayingSongBackground(currentIndex());
-  }
-
-  setDeleteEventListener();
-  setSongEventListener();
-
-  if(playlist().length < 2) {
-    songListDiv.append(resetButton());
-  }
-}
-
-function setSongEventListener(){
-  document.querySelectorAll('.playlist-song').forEach((songDiv,index) => {
-    songDiv.querySelector('.playlist-song-info').addEventListener('click', () => playSelectedSong(playlist()[index], songDiv));
-  });
-};
-
-function setDeleteEventListener(){
-  document.querySelectorAll('.playlist-song').forEach((songDiv,index) => {
-    songDiv.querySelector('.playlist-song-delete').addEventListener('click', () => deleteSong(index));
-  });
-};
-
-// Set Media Query with JavaScript to close the playlist container when viewport is at 700px or smaller
-function closePlaylist(view){
-  // first time the matchMedia's property 'matches' is being taught. Make sure to introduce with example in the steps
-  if (view.matches) {
-    return toggleClosePlaylist();
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
   } else {
-    arrow.classList.toggle('active');
-    songListDiv.style.visibility = "visible";
-    songListDiv.style.display = "flex";
-    closeBtn.setAttribute('aria-expanded', true);
+    audio.currentTime = userData.songCurrentTime;
   }
-}
+  userData.currentSong = song;
+  playButton.classList.add("playing");
 
-function setEventListeners(){
-  // Event listeners
-  document.querySelector(".play").addEventListener("click", togglePlay);
-  document.querySelector(".pause").addEventListener("click", pauseSong);
-  document.querySelector(".previous").addEventListener("click", previousSong);
-  document.querySelector(".next").addEventListener("click", nextSong);
-  document.querySelector(".shuffle").addEventListener("click", shuffle);
-  closeBtn.addEventListener("click", toggleClosePlaylist);
+  songHighlighter(song.id);
+  setPlayerDisplay(song.title, song.artist);
+  audio.play();
+};
 
-  // continue playing next song
-  audio.addEventListener("ended", nextSong);
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
 
-  view.addEventListener("change", closePlaylist);
+  playButton.classList.remove("playing");
+  audio.pause();
+};
 
-  // keyboardEvents
-  window.addEventListener("keydown", (event) => {
-      switch (true) {
-          case (event.key === "ArrowUp"):
-              previousSong();
-              break
-          case (event.key === "ArrowDown"):
-              nextSong();
-              break
-          default:
-      }
-  })
-}
+const nextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = userData?.songs.indexOf(userData.currentSong);
+    const nextSong = userData?.songs[currentSongIndex + 1];
 
+    playSong(nextSong.id);
+    setPlayButtonAccessibleText();
+  }
+};
 
-function app() {
-  renderSongs(playlist());
-  updatePlayingSongBackground(currentIndex());
-  renderSongDisplay(currentIndex());
-  setEventListeners();
-  setSongEventListener();
-  setDeleteEventListener();
-  closePlaylist(view);
-}
+const previousSong = () => {
+  if (userData?.currentSong === null) return;
+  else {
+    const currentSongIndex = userData?.songs.indexOf(userData.currentSong);
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+    setPlayButtonAccessibleText();
+  }
+};
+
+const shuffle = () => {
+  userData.songs = userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
+const setPlayButtonAccessibleText = () => {
+  const song = userData?.currentSong || userData?.songs[0];
+
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play",
+  );
+};
+
+playButton.addEventListener("click", () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
+
+pauseButton.addEventListener("click", () => {
+  pauseSong();
+});
+
+nextButton.addEventListener("click", () => {
+  nextSong();
+});
+
+previousButton.addEventListener("click", () => {
+  previousSong();
+});
+
+shuffleButton.addEventListener("click", shuffle);
+
+renderSongs(userData?.songs);
+setPlayButtonAccessibleText();
