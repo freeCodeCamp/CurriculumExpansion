e150cb3e29ddef21aabf71371334c426fe71edcc
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-09-13 17:14:26 +0900

    fix: convert ids to integers

--- javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
+++ javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
@@ -7,70 +7,70 @@ const shuffleButton = document.getElementById("shuffle");
 
 const allSongs = [
   {
-    id: "0",
+    id: 0,
     title: "Scratching The Surface",
     artist: "Quincy Larson",
     duration: "4:25",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
   },
   {
-    id: "1",
+    id: 1,
     title: "Can't Stay Down",
     artist: "Quincy Larson",
     duration: "4:15",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
   },
   {
-    id: "2",
+    id: 2,
     title: "Still Learning",
     artist: "Quincy Larson",
     duration: "3:51",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
   },
   {
-    id: "3",
+    id: 3,
     title: "Cruising for a Musing",
     artist: "Quincy Larson",
     duration: "3:34",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
   },
   {
-    id: "4",
+    id: 4,
     title: "Never Not Favored",
     artist: "Quincy Larson",
     duration: "3:35",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
   },
   {
-    id: "5",
+    id: 5,
     title: "From the Ground Up",
     artist: "Quincy Larson",
     duration: "3:12",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
   },
   {
-    id: "6",
+    id: 6,
     title: "Walking on Air",
     artist: "Quincy Larson",
     duration: "3:25",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
   },
   {
-    id: "7",
+    id: 7,
     title: "Can't Stop Me. Can't Even Slow Me Down.",
     artist: "Quincy Larson",
     duration: "3:52",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
   },
   {
-    id: "8",
+    id: 8,
     title: "The Surest Way Out is Through",
     artist: "Quincy Larson",
     duration: "3:10",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
   },
   {
-    id: "9",
+    id: 9,
     title: "Chasing That Feeling",
     artist: "Quincy Larson",
     duration: "2:43",
@@ -87,7 +87,7 @@ let userData = {
 
 const playSong = (id) => {
   // Introduce the .find() method
-  const song = userData?.songs.find((song) => song.id === id.toString());
+  const song = userData?.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
 
@@ -145,7 +145,7 @@ const shuffle = () => {
 };
 
 const deleteSong = (id) => {
-  if (userData?.currentSong?.id === id.toString()) {
+  if (userData?.currentSong?.id === id) {
     userData.currentSong = null;
     userData.songCurrentTime = 0;
 
@@ -153,7 +153,7 @@ const deleteSong = (id) => {
     setPlayerDisplay();
   }
 
-  userData.songs = userData?.songs.filter((song) => song.id !== id.toString());
+  userData.songs = userData?.songs.filter((song) => song.id !== id);
   renderSongs(userData?.songs);
   songHighlighter();
   setPlayButtonAccessibleText();
