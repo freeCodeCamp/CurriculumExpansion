f146ca3c65643d84b5e3e51a8c9d61bde8a2c39e
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-09-29 18:15:01 +0900

    fix: comment out test mp3s

--- javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
+++ javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
@@ -5,103 +5,103 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
-// Short mp3s for testing
-const allSongs = [
-  {
-    id: 0,
-    title: "9 Second Sample",
-    artist: "Quincy Larson",
-    duration: "0:09",
-    src: "https://download.samplelib.com/mp3/sample-9s.mp3",
-  },
-  {
-    id: 1,
-    title: "12 Second Sample",
-    artist: "Quincy Larson",
-    duration: "0:12",
-    src: "https://download.samplelib.com/mp3/sample-12s.mp3",
-  },
-  {
-    id: 2,
-    title: "15 Second Sample",
-    artist: "Quincy Larson",
-    duration: "0:15",
-    src: "https://download.samplelib.com/mp3/sample-15s.mp3",
-  }
-];
-
+// // Short mp3s for testing
 // const allSongs = [
 //   {
 //     id: 0,
-//     title: "Scratching The Surface",
+//     title: "9 Second Sample",
 //     artist: "Quincy Larson",
-//     duration: "4:25",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+//     duration: "0:09",
+//     src: "https://download.samplelib.com/mp3/sample-9s.mp3",
 //   },
 //   {
 //     id: 1,
-//     title: "Can't Stay Down",
+//     title: "12 Second Sample",
 //     artist: "Quincy Larson",
-//     duration: "4:15",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+//     duration: "0:12",
+//     src: "https://download.samplelib.com/mp3/sample-12s.mp3",
 //   },
 //   {
 //     id: 2,
-//     title: "Still Learning",
+//     title: "15 Second Sample",
 //     artist: "Quincy Larson",
-//     duration: "3:51",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
-//   },
-//   {
-//     id: 3,
-//     title: "Cruising for a Musing",
-//     artist: "Quincy Larson",
-//     duration: "3:34",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
-//   },
-//   {
-//     id: 4,
-//     title: "Never Not Favored",
-//     artist: "Quincy Larson",
-//     duration: "3:35",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
-//   },
-//   {
-//     id: 5,
-//     title: "From the Ground Up",
-//     artist: "Quincy Larson",
-//     duration: "3:12",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
-//   },
-//   {
-//     id: 6,
-//     title: "Walking on Air",
-//     artist: "Quincy Larson",
-//     duration: "3:25",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
-//   },
-//   {
-//     id: 7,
-//     title: "Can't Stop Me. Can't Even Slow Me Down.",
-//     artist: "Quincy Larson",
-//     duration: "3:52",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
-//   },
-//   {
-//     id: 8,
-//     title: "The Surest Way Out is Through",
-//     artist: "Quincy Larson",
-//     duration: "3:10",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
-//   },
-//   {
-//     id: 9,
-//     title: "Chasing That Feeling",
-//     artist: "Quincy Larson",
-//     duration: "2:43",
-//     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
-//   },
+//     duration: "0:15",
+//     src: "https://download.samplelib.com/mp3/sample-15s.mp3",
+//   }
 // ];
+
+const allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+  },
+  {
+    id: 2,
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
+  },
+  {
+    id: 3,
+    title: "Cruising for a Musing",
+    artist: "Quincy Larson",
+    duration: "3:34",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
+  },
+  {
+    id: 4,
+    title: "Never Not Favored",
+    artist: "Quincy Larson",
+    duration: "3:35",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
+  },
+  {
+    id: 5,
+    title: "From the Ground Up",
+    artist: "Quincy Larson",
+    duration: "3:12",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
+  },
+  {
+    id: 6,
+    title: "Walking on Air",
+    artist: "Quincy Larson",
+    duration: "3:25",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
+  },
+  {
+    id: 7,
+    title: "Can't Stop Me. Can't Even Slow Me Down.",
+    artist: "Quincy Larson",
+    duration: "3:52",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
+  },
+  {
+    id: 8,
+    title: "The Surest Way Out is Through",
+    artist: "Quincy Larson",
+    duration: "3:10",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
+  },
+  {
+    id: 9,
+    title: "Chasing That Feeling",
+    artist: "Quincy Larson",
+    duration: "2:43",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
+  },
+];
 
 const audio = new Audio();
 let userData = {
