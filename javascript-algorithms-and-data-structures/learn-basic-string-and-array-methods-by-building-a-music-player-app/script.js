c905f540d5a01114a2b5fe0774cbf3c90966004a
Rafael Hernandez <rafaeldavish443@gmail.com>
2023-10-12 23:16:20 -0400

    fix: add focus to current song for dashed lines

--- javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
+++ javascript-algorithms-and-data-structures/learn-basic-string-and-array-methods-by-building-a-music-player-app/script.js
@@ -221,7 +221,12 @@ const highlightCurrentSong = () => {
     songEl.removeAttribute("aria-current");
   });
 
-  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
+  if (songToHighlight) {
+    // Teach how to query specific inner elements within a node
+    const songButton = songToHighlight.querySelector("button.playlist-song-info");
+    songToHighlight.setAttribute("aria-current", "true")
+    songButton.focus();
+  }
 };
 
 const renderSongs = (array) => {
