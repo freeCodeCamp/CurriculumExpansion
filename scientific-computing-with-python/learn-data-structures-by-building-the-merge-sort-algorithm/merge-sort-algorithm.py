7c145f9d71a68e99ff79b83bf0d76f46ef084565
ihechikara <ihechikara.dev@gmail.com>
2023-10-24 19:38:23 +0100

    update PR

--- scientific-computing-with-python/learn-data-structures-by-building-the-merge-sort-algorithm/merge-sort-algorithm.py
+++ scientific-computing-with-python/learn-data-structures-by-building-the-merge-sort-algorithm/merge-sort-algorithm.py
@@ -1,43 +1,45 @@
-def mergeSort(array):
-  if len(array) > 1:
+def merge_sort(array):
+    if not array:
+        return
+    
+    if len(array) > 1:
+        middle_point = len(array) // 2
+        left_part = array[:middle_point]
+        right_part = array[middle_point:]
 
-      midPoint = len(array)//2
-      leftArray = array[:midPoint]
-      rightArray = array[midPoint:]
+        merge_sort(left_part)
+        merge_sort(right_part)
 
-      mergeSort(leftArray)
-      mergeSort(rightArray)
+        left_array_index = 0
+        right_array_index = 0
+        sorted_index = 0
 
-      x = 0
-      y = 0
-      z = 0
+        while left_array_index < len(left_part) and right_array_index < len(right_part):
+            if left_part[left_array_index] < right_part[right_array_index]:
+                array[sorted_index] = left_part[left_array_index]
+                left_array_index += 1
+            else:
+                array[sorted_index] = right_part[right_array_index]
+                right_array_index += 1
+            sorted_index += 1
 
-      while x < len(leftArray) and y < len(rightArray):
-          if leftArray[x] < rightArray[y]:
-              array[z] = leftArray[x]
-              x += 1
-          else:
-              array[z] = rightArray[y]
-              y += 1
-          z += 1
+        while left_array_index < len(left_part):
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+            sorted_index += 1
 
-      while x < len(leftArray):
-          array[z] = leftArray[x]
-          x += 1
-          z += 1
-
-      while y < len(rightArray):
-          array[z] = rightArray[y]
-          y += 1
-          z += 1
+        while right_array_index < len(right_part):
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+            sorted_index += 1
 
 if __name__ == '__main__':
-  numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
 
-  print("Unsorted array: ")
-  print(numbers)
+    print("Unsorted array: ")
+    print(numbers)
 
-  mergeSort(numbers)
+    merge_sort(numbers)
 
-  print("Sorted array: ")
-  print(numbers)
+    print("Sorted array: ")
+    print(numbers)
