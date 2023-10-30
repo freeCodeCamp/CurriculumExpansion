51846a64af2804260624218c210761a5355c7a51
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2023-10-30 21:30:18 +0100

    implemented gikf's suggestions

--- scientific-computing-with-python/learn-regular-expressions-by-building-a-password-generator/password_generator.py
+++ scientific-computing-with-python/learn-regular-expressions-by-building-a-password-generator/password_generator.py
@@ -4,7 +4,7 @@ import string
 
 def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
     # Define the possible characters for the password
-    letters = string.ascii_letters   
+    letters = string.ascii_letters
     digits = string.digits
     symbols = string.punctuation
 
@@ -12,24 +12,27 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
     all_characters = letters + digits + symbols
 
     while True:
-        password = ''    
+        password = ''
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
 
-        # Check constraints        
+        # Check constraints
         constraints = [
-            (nums, re.findall('[\d]', password)),
-            (special_chars, re.findall('[\W]', password)),
-            (uppercase, re.findall('[A-Z]', password)),
-            (lowercase, re.findall('[a-z]', password))
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
         ]
 
-        if all(limit <= len(matched_regex) for limit, matched_regex in constraints):
-            break    
+        if all(
+            limit <= len(re.findall(pattern, password))
+            for limit, pattern in constraints
+        ):
+            break
     
     return password
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     new_password = generate_password()
     print("Generated Password:", new_password)
