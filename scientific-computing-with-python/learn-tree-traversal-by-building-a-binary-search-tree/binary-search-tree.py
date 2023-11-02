8d13bc1ac5388afe284b4de422321c1c077ab912
Zaira <33151350+zairahira@users.noreply.github.com>
2023-11-02 19:06:42 +0500

    revamp the code

--- scientific-computing-with-python/learn-tree-traversal-by-building-a-binary-search-tree/binary-search-tree.py
+++ scientific-computing-with-python/learn-tree-traversal-by-building-a-binary-search-tree/binary-search-tree.py
@@ -11,69 +11,72 @@ class BinarySearchTree:
     def insert(self, key):
         self.root = self._insert(self.root, key)
 
-    def _insert(self, root, key):
-        if root is None:
+    def _insert(self, node, key):
+        if node is None:
             return TreeNode(key)
-        if key < root.key:
-            root.left = self._insert(root.left, key)
-        else:
-            root.right = self._insert(root.right, key)
-        return root
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
 
     def search(self, key):
         return self._search(self.root, key)
 
-    def _search(self, root, key):
-        if root is None or root.key == key:
-            return root
-        if key < root.key:
-            return self._search(root.left, key)
-        return self._search(root.right, key)
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
 
     def delete(self, key):
         self.root = self._delete(self.root, key)
 
-    def _delete(self, root, key):
-        if root is None:
-            return root
-        if key < root.key:
-            root.left = self._delete(root.left, key)
-        elif key > root.key:
-            root.right = self._delete(root.right, key)
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
         else:
-            # Node with one child or no child
-            if root.left is None:
-                return root.right
-            elif root.right is None:
-                return root.left
-            # Node with two children, get the in-order successor (smallest in the right subtree)
-            root.key = self._min_value_node(root.right).key
-            root.right = self._delete(root.right, root.key)
-        return root
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            node.key = self._min_value(node.right)
+            node.right = self._delete(node.right, node.key)
+        return node
 
-    def _min_value_node(self, node):
+    def _min_value(self, node):
         while node.left is not None:
             node = node.left
-        return node
+        return node.key
 
     def inorder_traversal(self):
         result = []
         self._inorder_traversal(self.root, result)
         return result
 
-    def _inorder_traversal(self, root, result):
-        if root:
-            self._inorder_traversal(root.left, result)
-            result.append(root.key)
-            self._inorder_traversal(root.right, result)
+    def _inorder_traversal(self, node, result):
+        if node:
+            self._inorder_traversal(node.left, result)
+            result.append(node.key)
+            self._inorder_traversal(node.right, result)
 
 # Example usage:
+
 bst = BinarySearchTree()
+
 nodes = [50, 30, 20, 40, 70, 60, 80]
 for node in nodes:
     bst.insert(node)
 
-print("Inorder traversal:", bst.inorder_traversal())  # Should print [20, 30, 40, 50, 60, 70, 80]
+print("Inorder traversal:", bst.inorder_traversal())
 
-bst.delete(20)
-print("Inorder traversal after deleting 20:", bst.inorder_traversal())  # Should print [30, 40, 50, 60, 70, 80]
+print("Search for 40:", bst.search(40))
+print("Search for 30:", bst.search(30))
+
+bst.delete(40)
+print("Inorder traversal after deleting 40:", bst.inorder_traversal())
