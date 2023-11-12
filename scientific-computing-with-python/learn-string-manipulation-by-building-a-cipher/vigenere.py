24e2074c610bfed4cd6a422e6dfcdd53344584ce
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2023-11-12 11:34:25 +0100

    Apply gikf's suggestions

--- scientific-computing-with-python/learn-string-manipulation-by-building-a-cipher/vigenere.py
+++ scientific-computing-with-python/learn-string-manipulation-by-building-a-cipher/vigenere.py
@@ -1,25 +1,23 @@
 from string import ascii_lowercase as letters
 
-def vigenere(message, key, direction):
-    spaced_key = ''
+def vigenere(message, key, direction):    
     key_index = 0
     final_message = ''
-    for char in message.lower():        
-        if char == ' ':            
-            # Keep the same spacing in message and key
-            final_message += ' '            
-            spaced_key += ' '
+    for char in message.lower():
+        if not char.isalpha():
+            # Append any non-letter character to the message
+            final_message += char
             continue
+        
+        # Find the right key character to encode/decode
+        key_char = key[key_index % len(key)]
+        key_index += 1
+        
+        # Define the shift and the encrypted/decrypted letter
+        shift = letters.index(key_char)
         index = letters.find(char)
-        if index >= 0:
-            # Generate the key repeating it up to the messege length
-            spaced_key += key[key_index % len(key)]
-            key_index += 1
-
-            # Define the shift and the encrypted/decrypted letter
-            shift = letters.find(spaced_key[-1])
-            new_index = (index + shift *direction) % 26
-            final_message += letters[new_index]    
+        new_index = (index + shift*direction) % 26
+        final_message += letters[new_index]
 
     return final_message
 
@@ -27,7 +25,7 @@ def encrypt(message, key):
     return vigenere(message, key, 1)
 
 def decrypt(message, key):
-    return vigenere(message, key, - 1)
+    return vigenere(message, key, -1)
 
 text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'happycoding'
