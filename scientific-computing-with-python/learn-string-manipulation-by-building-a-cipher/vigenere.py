38f265e4245ff7b0aece98815d6d41039e2723d5
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2023-11-17 08:58:15 +0100

    Update vigenere.py

--- scientific-computing-with-python/learn-string-manipulation-by-building-a-cipher/vigenere.py
+++ scientific-computing-with-python/learn-string-manipulation-by-building-a-cipher/vigenere.py
@@ -1,35 +1,37 @@
-from string import ascii_lowercase as letters
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'happycoding'
 
-def vigenere(message, key, direction):    
+def vigenere(message, key, direction=1):
     key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
     final_message = ''
+
     for char in message.lower():
+
+        # Append any non-letter character to the message
         if not char.isalpha():
-            # Append any non-letter character to the message
             final_message += char
-            continue
-        
-        # Find the right key character to encode/decode
-        key_char = key[key_index % len(key)]
-        key_index += 1
-        
-        # Define the shift and the encrypted/decrypted letter
-        shift = letters.index(key_char)
-        index = letters.find(char)
-        new_index = (index + shift*direction) % 26
-        final_message += letters[new_index]
+
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)    
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
 
     return final_message
 
 def encrypt(message, key):
-    return vigenere(message, key, 1)
+    return vigenere(message, key)
 
 def decrypt(message, key):
     return vigenere(message, key, -1)
 
-text = 'mrttaqrhknsw ih puggrur'
-custom_key = 'happycoding'
-
 print(f'\nEncrypted text: {text}')
-print(f'Key: {custom_key}')    
-print(f'\nDecrypted text: {decrypt(text, custom_key)}', '\n')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
