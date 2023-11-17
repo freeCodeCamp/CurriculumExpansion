6df380552ee2498d4d66f98dc0bb31c81d621537
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2023-11-17 09:02:56 +0100

    remove empty space

--- scientific-computing-with-python/learn-string-manipulation-by-building-a-cipher/vigenere.py
+++ scientific-computing-with-python/learn-string-manipulation-by-building-a-cipher/vigenere.py
@@ -12,14 +12,14 @@ def vigenere(message, key, direction=1):
         if not char.isalpha():
             final_message += char
 
-        else:        
+        else:
             # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
             # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
-            index = alphabet.find(char)    
+            index = alphabet.find(char)
             new_index = (index + offset*direction) % len(alphabet)
             final_message += alphabet[new_index]
 
@@ -35,3 +35,4 @@ print(f'\nEncrypted text: {text}')
 print(f'Key: {custom_key}')
 decryption = decrypt(text, custom_key)
 print(f'\nDecrypted text: {decryption}\n')
+
