fde4af00e8e10df0b909cf07567cf79cc0396fa7
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2023-11-19 11:17:23 +0100

    Update shortest-path-algorithm.py

--- scientific-computing-with-python/learn-data-structures-by-building-the-shortest-path-algorithm/shortest-path-algorithm.py
+++ scientific-computing-with-python/learn-data-structures-by-building-the-shortest-path-algorithm/shortest-path-algorithm.py
@@ -1,17 +1,16 @@
 my_graph = {
-    'A': [('B', 1), ('C', 6)],
-    'B': [('A', 1), ('D', 3), ('E', 1), ('F', 5)],
-    'C': [('A', 6), ('D', 4)],
-    'D': [('B', 3), ('C', 4), ('E', 2), ('G', 3)],
-    'E': [('B', 1), ('D', 2), ('F', 9)],
-    'F': [('B', 5), ('E', 9), ('G', 8)],
-    'G': [('D', 3), ('F', 8)]
+    'A': [('B', 5), ('C', 3), ('E', 11)],
+    'B': [('A', 5), ('C', 1), ('F', 2)],
+    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
+    'D': [('C',1 ), ('E', 9), ('F', 3)],
+    'E': [('A', 11), ('C', 5), ('D', 9)],
+    'F': [('B', 2), ('D', 3)]
 }
 
 def shortest_path(graph, start, target = ''):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
-    paths = {node: [] for node in graph}    
+    paths = {node: [] for node in graph}
     paths[start].append(start)
     
     while unvisited:
@@ -25,15 +24,16 @@ def shortest_path(graph, start, target = ''):
                     paths[node].extend(paths[current])
                 paths[node].append(node)
         unvisited.remove(current)
-        
-    if target:
-        print(f'\n{start}-{target} distance: {distances[target]}\nPath: {" -> ".join(paths[target])}\n')
-    else:
-        for node in graph:
-            if node == start:
-                continue
-            print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
-        print()
+    
+    targets_to_print = [target] if target else graph
+    for node in targets_to_print:
+        if node == start:
+            continue
+        print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
+    print()
     return distances, paths
+    
+shortest_path(my_graph, 'A', 'F
+')
 
-shortest_path(my_graph, 'A', 'G')
+              
