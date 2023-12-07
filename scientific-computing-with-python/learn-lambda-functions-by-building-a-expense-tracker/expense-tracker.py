636f6c93218bd04d4cd1384e5ab39e1f703203fc
Tomi Tokko <56736462+tomitokko@users.noreply.github.com>
2023-12-07 08:13:01 +0000

    Add expense tracker project (#365)
    * Added expense tracker project
    
    * Update scientific-computing-with-python/learn-lambda-functions-by-building-a-expense-tracker/expense-tracker.py
    
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>
    
    * Update scientific-computing-with-python/learn-lambda-functions-by-building-a-expense-tracker/expense-tracker.py
    
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>
    
    * Update scientific-computing-with-python/learn-lambda-functions-by-building-a-expense-tracker/expense-tracker.py
    
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>
    
    * Update scientific-computing-with-python/learn-lambda-functions-by-building-a-expense-tracker/expense-tracker.py
    
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>
    
    * Updated code
    
    ---------
    
    Co-authored-by: Oluwatomi Tokko <tomi@Oluwatomis-MacBook-Air.local>
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>

---
+++ scientific-computing-with-python/learn-lambda-functions-by-building-a-expense-tracker/expense-tracker.py
@@ -0,0 +1,52 @@
+def add_expense(expenses, amount, category):
+    expenses.append({"amount": amount, "category": category})
+
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f"Amount: {expense['amount']}, Category: {expense['category']}")
+
+def total_expenses(expenses):
+    return sum(map(lambda expense: expense['amount'], expenses))
+
+def filter_expenses_by_category(expenses, category):
+    return filter(lambda expense: expense['category'] == category, expenses)
+
+def main():
+    expenses = []  # Create a new expenses list for each instance
+    while True:
+        print("\nExpense Tracker")
+        print("1. Add an expense")
+        print("2. List all expenses")
+        print("3. Show total expenses")
+        print("4. Filter expenses by category")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            amount = float(input("Enter amount: "))
+            category = input("Enter category: ")
+            add_expense(expenses, amount, category)
+
+        elif choice == '2':
+            print("\nAll Expenses:")
+            print_expenses(expenses)
+
+        elif choice == '3':
+            print("\nTotal Expenses: ", total_expenses(expenses))
+
+        elif choice == '4':
+            category = input("Enter category to filter: ")
+            print(f"\nExpenses for {category}:")
+            expenses_from_category = filter_expenses_by_category(expenses, category)
+            print_expenses(list(expenses_from_category))
+
+        elif choice == '5':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
