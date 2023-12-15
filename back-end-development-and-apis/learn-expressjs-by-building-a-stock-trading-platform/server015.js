47fd2afd03dacc7bc4772654575dd7c1cfe0e93c
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-15 14:12:33 +0530

    added middleware function

--- back-end-development-and-apis/learn-expressjs-by-building-a-stock-trading-platform/server015.js
+++ back-end-development-and-apis/learn-expressjs-by-building-a-stock-trading-platform/server015.js
@@ -3,6 +3,8 @@ const app = express();
 
 // Create a new instance of Express named 'app' with the following code: `const app = express();`
 
+app.use(express.json());
+
 app.listen(5500, () => {
     console.log('Server is listening on 5500');
 });
