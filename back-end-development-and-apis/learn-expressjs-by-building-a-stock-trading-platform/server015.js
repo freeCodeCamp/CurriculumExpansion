219c31a64938f3127b567be835168a2e42e48229
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-15 14:10:50 +0530

    Changes in express.js

--- back-end-development-and-apis/learn-expressjs-by-building-a-stock-trading-platform/server015.js
+++ back-end-development-and-apis/learn-expressjs-by-building-a-stock-trading-platform/server015.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const app = express();
 
 // Create a new instance of Express named 'app' with the following code: `const app = express();`
+
+app.listen(5500, () => {
+    console.log('Server is listening on 5500');
+});
