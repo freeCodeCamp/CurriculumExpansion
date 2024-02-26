9cbdae2e7f4b95b06b9fcb555fea6f83186f45f1
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-02-26 09:40:08 +0100

    refactor code to include inheritance

--- scientific-computing-with-python/learn-special-methods-by-building-a-vector-space/vectors.py
+++ scientific-computing-with-python/learn-special-methods-by-building-a-vector-space/vectors.py
@@ -1,66 +1,49 @@
-class Vector:
-    space = 'R3'
-
-    def __init__(self, x, y, z):
+class R2Vector:
+    def __init__(self, *, x, y):
         self.x = x
         self.y = y
-        self.z = z
-        self.norm = (x**2 + y**2 + z**2)**0.5
+    
+    def norm(self):
+        return sum(val**2 for val in self.__dict__.values())**0.5
 
     def __str__(self):
-        return str((self.x, self.y, self.z))
-    
-    def __repr__(self):
-        return f'Vector({self.x}, {self.y}, {self.z})'
+        return str(tuple(self.__getattribute__(i) for i in self.__dict__))
 
+    def __repr__(self):
+        arg_list = [f'{key}={val}' for key, val in self.__dict__.items()]
+        args = ", ".join(arg_list)
+        return f'{self.__class__.__name__}({args})'
+    
     def __add__(self, other):
-        if not isinstance(other, Vector):
+        if not isinstance(other, self.__class__):
             return NotImplemented
-        x = self.x + other.x
-        y = self.y + other.y
-        z = self.z + other.z        
-        return self.__class__(x, y, z)
+        kwargs = {i: self.__getattribute__(i) + other.__getattribute__(i) for i in self.__dict__}
+        return self.__class__(**kwargs)
     
     def __sub__(self, other):
-        if not isinstance(other, Vector):
+        if not isinstance(other, self.__class__):
             return NotImplemented
-        x = self.x - other.x
-        y = self.y - other.y
-        z = self.z - other.z
-        return Vector(x, y, z)
+        kwargs = {i: self.__getattribute__(i) - other.__getattribute__(i) for i in self.__dict__}
+        return self.__class__(**kwargs)
     
     def __mul__(self, other):
         # Scalar multiplication
         if isinstance(other, (float, int)):
-            x = self.x * other
-            y = self.y * other
-            z = self.z * other            
-            return Vector(x, y, z)
-        
+            kwargs = {i: self.__getattribute__(i) * other for i in self.__dict__}
+            return self.__class__(**kwargs)
+    
         # Scalar product
-        elif isinstance(other, Vector):
-            x = self.x * other.x
-            y = self.y * other.y
-            z = self.z * other.z
-            scalar = (x**2 + y**2 + z**2)**0.5
-            return scalar
-        else:
-            other_type = other.__class__.__name__
-            raise TypeError(f"Multiplication is not defined between 'Vector' and '{other_type}'")
+        elif isinstance(other, self.__class__):
+            args = [self.__getattribute__(i) * other.__getattribute__(i) for i in self.__dict__]
+            return (sum(arg**2 for arg in args))**0.5
         
-    def cross(self, other):
-        # Cross product
-        if not isinstance(other, Vector):
-            raise TypeError("Argument must be a 'Vector' instance")
-        x = self.y * other.z - self.z * other.y
-        y = self.z * other.x - self.x * other.z
-        z = self.x * other.y - self.y * other.x
-        return Vector(x, y, z)
+        raise TypeError(f"Multiplication is not defined between '{self.__class__.__name__}' and '{other.__class__.__name__}'")
     
     def __eq__(self, other):
-        if not isinstance(other, Vector):
+        if not isinstance(other, self.__class__):
             return NotImplemented
-        if self.x == other.x and self.y == other.y and self.z == other.z:
+        
+        if all(self.__getattribute__(i) == other.__getattribute__(i) for i in self.__dict__):
             return True
         return False
     
@@ -68,30 +51,44 @@ class Vector:
         return not self == other
     
     def __lt__(self, other):
-        if not isinstance(other, Vector):
+        if not isinstance(other, self.__class__):
             return NotImplemented
-        return self.norm < other.norm
+        return self.norm() < other.norm()
     
     def __gt__(self, other):
         return not self < other
-    
+
     def __le__(self, other):
-        if not isinstance(other, Vector):
+        if not isinstance(other, self.__class__):
             return NotImplemented
-        return self.norm <= other.norm
+        return self.norm() <= other.norm()
     
     def __ge__(self, other):
         return not self <= other
 
 
-# Example usage
-v1 = Vector(3, 2, 2)
-v2 = Vector(4.7, 1, 1.5)
-print('v1 =', v1)
-print('v2 =', v2)
+class R3Vector(R2Vector):
+    def __init__(self, *, x, y, z):
+        super().__init__(x=x, y=y)
+        self.z = z
+
+    def cross(self, other):
+        # Cross product
+        if not isinstance(other, self.__class__):
+            raise TypeError(f"Argument must be a '{self.__class__.__name__}' instance")
+        kwargs = {}
+        kwargs['x'] = self.y * other.z - self.z * other.y
+        kwargs['y'] = self.z * other.x - self.x * other.z
+        kwargs['z'] = self.x * other.y - self.y * other.x
+        
+        return self.__class__(**kwargs)
+
+
+v1 = R3Vector(x=1, y=2, z=0.2)
+v2 = R3Vector(x=2, y=3, z=1.5)
+print(f"v1 = {v1}")
+print(f"v2 = {v2}")
 v3 = v1 + v2
-print('v1 + v2 =', v3)
-n = v1 * v2
-print('v1 * v2 =', n)
+print(f"v1 + v2 = {v3}")
 v4 = v1.cross(v2)
-print('v1 x v2 =', v4)
+print(f"v1 x v2 = {v4}")
