d528ce60f882865cebf1f92da25d17c4d6216422
Zaira <33151350+zairahira@users.noreply.github.com>
2024-03-06 16:50:51 +0500

    feat: add bisection method project Python curriculum (#373)
    * feat: add bisection method
    
    * fix: apply suggestions from code review
    
    * fix: apply Dario's suggestion
    
    Co-authored-by: Dario-DC <105294544+Dario-DC@users.noreply.github.com>
    
    * fix: add special case for 0
    
    * fix: update code for case 0 and 1
    
    * fix: apply Dario's suggestions from code review
    
    Co-authored-by: Dario-DC <105294544+Dario-DC@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dario-DC <105294544+Dario-DC@users.noreply.github.com>

---
+++ scientific-computing-with-python/learn-numerical-analysis-by-finding-the-square-root-of-a-number-using-the-bisection-method/bisection-method.py
@@ -0,0 +1,40 @@
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    if square_target < 0:
+        # Square root of negative number is not defined in real numbers
+        raise ValueError('Square root of negative number is not defined in real numbers')
+    if square_target == 1:
+        root = 1
+        print(f'The square root of {square_target} is 1')
+
+    elif square_target == 0:
+        root = 0
+        print(f'The square root of {square_target} is 0')
+    else:
+
+        low = 0
+        # Ensures the interval covers the root for N < 1 and N > 1
+        high = max(1, square_target)
+        root = None
+
+        for _ in range(max_iterations):
+            mid = (low + high) / 2
+            square_mid = mid**2
+
+            if abs(square_mid - square_target) < tolerance:
+                root = mid
+                break
+            elif square_mid < square_target:
+                low = mid
+            else:
+                high = mid
+
+        if root is None:
+            print(f"Failed to converge within {max_iterations} iterations.")
+        else:
+            print(f'The square root of {square_target} is approximately {root}')
+    return root
+
+
+# Example usage
+N = 81
+root = square_root_bisection(N)
