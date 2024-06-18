9a6f75cb3080f7ee190a037c8f3a3d2c4d67310b
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-06-18 11:15:45 +0200

    delete abstract properties, move result strings creation outside the class

--- scientific-computing-with-python/learn-inheritance-by-building-an-equation-solver/quadratic_equation_solver.py
+++ scientific-computing-with-python/learn-inheritance-by-building-an-equation-solver/quadratic_equation_solver.py
@@ -3,6 +3,9 @@ import re
 
 
 class Equation(ABC):
+    degree: int
+    type: str
+
     def __init__(self, *args):
         if (self.degree + 1) != len(args):
             raise TypeError(
@@ -13,128 +16,115 @@ class Equation(ABC):
                 raise TypeError("Coefficients must be of type 'int' or 'float'")
         if args[0] == 0:
             raise ValueError("Highest degree coefficient must be different from zero")
-        self.coefficients = {n: arg for n, arg in enumerate(reversed(args))}
-        self.results = []
-        self.details = []
+        self.coefficients = {(len(args) - n - 1): arg for n, arg in enumerate(args)}
+
+    def __init_subclass__(cls):
+        if not (hasattr(cls, "type") and hasattr(cls, "degree")):
+            raise AttributeError()
 
     def __str__(self):
         terms = []
         for n, coefficient in self.coefficients.items():
-            if coefficient:
-                if n == 0:
-                    terms.append(f"{coefficient:+}")
-                elif n == 1:
-                    terms.insert(0, f"{coefficient:+}x")
-                else:
-                    terms.insert(0, f"{coefficient:+}x**{n}")
-        equation_string = " ".join(terms)
-        equation_string += " = 0"
+            if not coefficient:
+                continue
+            if n == 0:
+                terms.append(f"{coefficient:+}")
+            elif n == 1:
+                terms.append(f"{coefficient:+}x")
+            else:
+                terms.append(f"{coefficient:+}x**{n}")
+        equation_string = " ".join(terms) + " = 0"
         return re.sub(r"(?<!\d)1(?=x)", "", equation_string.strip("+"))
 
-    @property
     @abstractmethod
-    def degree(self): ...
-
-    @property
-    @abstractmethod
-    def type(self): ...
+    def solve(self):
+        pass
 
     @abstractmethod
-    def solve(self): ...
-
-    @abstractmethod
-    def analyze(self): ...
+    def analyze(self):
+        pass
 
 
 class LinearEquation(Equation):
-    @property
-    def degree(self):
-        return 1
-
-    @property
-    def type(self):
-        return "Linear Equation"
+    degree = 1
+    type = "Linear Equation"
 
     def solve(self):
-        x = -self.coefficients[0] / self.coefficients[1]
-        self.results.append(f"x = {x:.3f}")
+        a, b = self.coefficients.values()
+        x = -b / a
         return [x]
 
     def analyze(self):
-        self.details.append(
-            f"slope = {self.coefficients[1]:>16}\ny-intercept = {self.coefficients[0]:>10}"
-        )
-        return {"slope": self.coefficients[1], "intercept": self.coefficients[0]}
+        slope, intercept = self.coefficients.values()
+        return {"slope": slope, "intercept": intercept}
 
 
 class QuadraticEquation(Equation):
+    degree = 2
+    type = "Quadratic Equation"
+
     def __init__(self, *args):
         super().__init__(*args)
-        self.delta = (
-            self.coefficients[1] ** 2 - 4 * self.coefficients[2] * self.coefficients[0]
-        )
-
-    @property
-    def degree(self):
-        return 2
-
-    @property
-    def type(self):
-        return "Quadratic Equation"
+        a, b, c = self.coefficients.values()
+        self.delta = b**2 - 4 * a * c
 
     def solve(self):
         if self.delta < 0:
-            self.results.append("No real roots")
             return []
 
-        x1 = (-self.coefficients[1] + (self.delta) ** 0.5) / (2 * self.coefficients[2])
-        x2 = (-self.coefficients[1] - (self.delta) ** 0.5) / (2 * self.coefficients[2])
+        a, b, _ = self.coefficients.values()
+        x1 = (-b + (self.delta) ** 0.5) / (2 * a)
+        x2 = (-b - (self.delta) ** 0.5) / (2 * a)
         if self.delta == 0:
-            self.results.append(f"x = {x1:.3f}")
             return [x1]
 
-        self.results.extend([f"x1 = {x1:.3f}", f"x2 = {x2:.3f}"])
         return [x1, x2]
 
     def analyze(self):
-        x = -self.coefficients[1] / (2 * self.coefficients[2])
-        y = (
-            self.coefficients[2] * x**2
-            + self.coefficients[1] * x
-            + self.coefficients[0]
-        )
-        vertex = {"x": x, "y": y}
-        if self.coefficients[2] > 0:
+        a, b, c = self.coefficients.values()
+        x = -b / (2 * a)
+        y = a * x**2 + b * x + c
+        if a > 0:
             concavity = "upwards"
-            vertex["m"] = "min"
+            min_max = "min"
         else:
             concavity = "downwards"
-            vertex["m"] = "max"
-        coord = f"({x:.3f}, {y:.3f})"
-        self.details.append(f'concavity = {concavity:>12}\n{vertex["m"]} = {coord:>18}')
-        return {"vertex": vertex, "concavity": concavity}
+            min_max = "max"
+        return {"x": x, "y": y, "min_max": min_max, "concavity": concavity}
 
 
 def solver(equation):
+    width: int
     if not isinstance(equation, Equation):
         raise TypeError("Argument must be an Equation object")
-    if not equation.results:
-        equation.solve()
-    if not equation.details:
-        equation.analyze()
-    output_string = "\n{:-^24}".format(equation.type)
-    output_string += f"\n\n{str(equation):^24}\n\n"
-    output_string += "{:-^24}".format("Solutions") + "\n\n"
-    for result in equation.results:
+    output_string = f"\n{equation.type:-^24}"
+    output_string += f"\n\n{equation!s:^24}\n\n"
+    output_string += f'{"Solutions":-^24}\n\n'
+    results = equation.solve()
+    match len(results):
+        case 0:
+            result_list = ["No real roots"]
+        case 1:
+            result_list = [f"x = {results[0]:.3f}"]
+        case 2:
+            result_list = [f"x1 = {results[0]:+.3f}", f"x2 = {results[1]:+.3f}"]
+    for result in result_list:
         output_string += f"{result:^24}\n"
-    output_string += "\n"
-    output_string += "{:-^24}".format("Details") + "\n\n"
-    for detail in equation.details:
-        output_string += detail + "\n"
-    print(output_string)
+    output_string += f'\n{"Details":-^24}\n\n'
+    details = equation.analyze()
+    if isinstance(equation, LinearEquation):
+        slope, intercept = details.values()
+        details_list = [f"slope = {slope:>16.3f}", f"y-intercept = {intercept:>10.3f}"]
+    elif isinstance(equation, QuadraticEquation):
+        x, y, min_max, concavity = details.values()
+        coord = f"({x:.3f}, {y:.3f})"
+        details_list = [f"concavity = {concavity:>12}", f"{min_max} = {coord:>18}"]
+    for detail in details_list:
+        output_string += f"{detail}\n"
+    return output_string
 
 
 lin_eq = LinearEquation(2, 3)
-solver(lin_eq)
+print(solver(lin_eq))
 quadr_eq = QuadraticEquation(2, -3, -2)
-solver(quadr_eq)
+print(solver(quadr_eq))
