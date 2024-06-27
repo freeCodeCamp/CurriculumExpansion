874fe508068c8735f948c83700676415fb833a79
Ilenia <26656284+ilenia-magoni@users.noreply.github.com>
2024-06-27 15:34:23 +0200

    Update projectile-trajectory.py

--- scientific-computing-with-python/learn-encapsulation-by-drawing-a-projectile-trajectory/projectile-trajectory.py
+++ scientific-computing-with-python/learn-encapsulation-by-drawing-a-projectile-trajectory/projectile-trajectory.py
@@ -1,139 +1,125 @@
 import math
 
-# constants
 GRAVITATIONAL_ACCELERATION = 9.81
-PROJECTILE = '∙'
-
+PROJECTILE = "∙"
+x_axis_tick = "T"
+y_axis_tick = "⊣"
 
 class Projectile:
-    __slots__ = ('__height', '__speed', '__angle')
+    __slots__ = ('__speed', '__height', '__angle')
 
-    def __init__(self, height, speed, angle):
-        self.__height = height
+    def __init__(self, speed, height, angle):
         self.__speed = speed
+        self.__height = height
         self.__angle = math.radians(angle)
+        
+    def __str__(self):
+        return f'''
+Projectile details:
+speed: {self.speed} m/s
+height: {self.height} m
+angle: {self.angle}°
+displacement: {round(self.__calculate_displacement(), 1)} m
+'''
 
     def __calculate_displacement(self):
         horizontal_component = self.__speed * math.cos(self.__angle)
         vertical_component = self.__speed * math.sin(self.__angle)
-        sqrt_component = math.sqrt(self.__speed ** 2 * math.sin(self.__angle) ** 2 +
-                                   2 * GRAVITATIONAL_ACCELERATION * self.__height)
-        displacement = horizontal_component * (vertical_component + sqrt_component) / GRAVITATIONAL_ACCELERATION
-        return displacement
-
+        squared_component = vertical_component**2
+        gh_component = 2 * GRAVITATIONAL_ACCELERATION * self.__height
+        sqrt_component = math.sqrt(squared_component + gh_component)
+        
+        return horizontal_component * (vertical_component + sqrt_component) / GRAVITATIONAL_ACCELERATION
+        
     def __calculate_y_coordinate(self, x):
         height_component = self.__height
         angle_component = math.tan(self.__angle) * x
         acceleration_component = GRAVITATIONAL_ACCELERATION * x ** 2 / (
                 2 * self.__speed ** 2 * math.cos(self.__angle) ** 2)
         y_coordinate = height_component + angle_component - acceleration_component
-        return y_coordinate
 
+        return y_coordinate
+    
     def calculate_all_coordinates(self):
         return [
             (x, self.__calculate_y_coordinate(x))
             for x in range(math.ceil(self.__calculate_displacement()))
         ]
 
-    def details(self):
-        return f'\nangle: {round(math.degrees(self.__angle))}°\nspeed: {self.__speed} m/s\nstarting height: {self.__height} m\ndisplacement: {round(self.__calculate_displacement(), 1)} m'
-
-    def __str__(self):
-        return self.details()
-    
     @property
     def height(self):
         return self.__height
-    
-    @height.setter
-    def height(self, new_height):
-        self.__height = new_height
 
     @property
     def angle(self):
-        return self.__angle
-    
-    @angle.setter
-    def angle(self, new_angle):
-        self.__angle = math.radians(new_angle)
+        return round(math.degrees(self.__angle))
 
     @property
     def speed(self):
         return self.__speed
 
+    @height.setter
+    def height(self, n):
+        self.__height = n
+
+    @angle.setter
+    def angle(self, n):
+        self.__angle = math.radians(n)
+
     @speed.setter
-    def speed(self, new_speed):
-        self.__speed = new_speed
+    def speed(self, s):
+       self.__speed = s
+    
+    def __repr__(self):
+        return f'{self.__class__}({self.speed}, {self.height}, {self.angle})'
 
 class Graph:
-    def __init__(self, coordinates):
-        self.__coordinates = coordinates
+    __slots__ = ('__coordinates')
 
-    def __create_coordinates_table(self):
+    def __init__(self, coord):
+        self.__coordinates = coord
+
+    def __repr__(self):
+        return f"Graph({self.__coordinates})"
+        
+    def __str__(self):
+    	return self.create_trajectory()
+
+    def create_coordinates_table(self):
         table = '\n  x      y\n'
         for x, y in self.__coordinates:
-            table += f'{x:>3}{round(y, 2):>7.2f}\n'
+            table += f'{x:>3}{y:>7.2f}\n'
 
         return table
 
-    def __create_trajectory(self):
-        rounded_coord = [(x, round(y)) for x, y in self.__coordinates]
-        rows = max([y for x, y in rounded_coord]) + 1
-        columns = max([x for x, y in rounded_coord]) + 1
-        graph_matrix = [[PROJECTILE if (x, y) in rounded_coord else ' ' for x in range(columns)] for y in range(rows, -1, -1)]
-        graph_rows = ['⊣' + ''.join(row) for row in graph_matrix] + [' ' + '⊤'*columns]
-        graph = '\n' + '\n'.join(graph_rows)
+    def create_trajectory(self):
+
+        rounded_coords = [(round(x), round(y)) for x, y in self.__coordinates]
+
+        x_max = max(rounded_coords, key=lambda i: i[0])[0]
+        y_max = max(rounded_coords, key=lambda j: j[1])[1]
+
+        matrix_list = [[" " for _ in range(x_max + 1)] for _ in range(y_max + 1)]
+
+        for x, y in rounded_coords:
+            matrix_list[-1 - y][x] = PROJECTILE
+
+        matrix = ["".join(line) for line in matrix_list]
+
+        matrix_axes = [y_axis_tick + row for row in matrix]
+        matrix_axes.append(" " + x_axis_tick * (len(matrix[0])))
+
+        graph = "\n" + "\n".join(matrix_axes) + "\n"
+
         return graph
+
+def projectile_helper(speed, height, angle):
+    p = Projectile(speed, height, angle)
+    print(p)
+    coord = p.calculate_all_coordinates()
+    g = Graph(coord)
+    print(g.create_coordinates_table())
+    print(g.create_trajectory())
     
-    def __str__(self):
-        return self.__create_trajectory()
-
-    def table(self):
-        return self.__create_coordinates_table()
-
-def terminal_menu():
-    page = 0
-    while True:
-        if page == 0:
-            value_from_user = input(
-                "Please provide starting height, speed and angle for the PROJECTILE separated by a space: "
-            )
-            numbers = [int(number) for number in value_from_user.split()]
-            
-            bullet = Projectile(*numbers)
-            page = 1
-        elif page == 1:
-            print()
-            print(bullet.details())
-            try:
-                page = int(
-                    input(
-                        "\nChoose what to do:\n1. Draw trajectory\n2. List coordinates\n3. Change a value\n4. Exit\n"
-                    )) + 1
-            except:
-                print('Invalid input, please try again')
-        elif page == 2:
-            graph = Graph(bullet.calculate_all_coordinates())
-            print(graph)
-            page = 1
-        elif page == 3:
-            graph = Graph(bullet.calculate_all_coordinates())
-            print(graph.table())
-            page = 1
-        elif page == 4:
-            key = input(
-                'Write "speed", "angle", or "height" to choose which value: ')
-            if f'__{key}' in bullet.__slots__:
-                try:
-                    value = float(input('What value do you want to give? '))
-                    bullet.__setattr__(key, value)
-                except ValueError:
-                    print('Invalid value has been submitted')
-            else:
-                print('Invalid value has been submitted')
-            
-            page = 1
-        elif page == 5:
-            break
-
-terminal_menu()
+projectile_helper(12, 43, 1)
+    
