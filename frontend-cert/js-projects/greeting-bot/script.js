6a3b970f31b76c6688ea7ca6dd551e5556e28ec7
jdwilkin4 <jwilkin4@hotmail.com>
2024-07-02 19:33:34 -0700

    chore: adding 1st JS only workshop

---
+++ frontend-cert/js-projects/greeting-bot/script.js
@@ -0,0 +1,50 @@
+// This will be the first workshop in new JS section
+
+// create steps for console.log and basic string usage
+console.log("Hi there!");
+console.log("It is a beautiful day today");
+console.log("I am excited to talk to you");
+
+// create steps just for declaring variables
+let bot;
+let occupation;
+// introduce camel case here
+let botLocation;
+
+// add steps to teach assignment
+bot = "teacherBot";
+occupation = "teacher";
+botLocation = "the universe";
+
+// add review step for adding console statement
+console.log("Allow me to introduce myself");
+
+// teach multiple arguments for console statement
+// We will teach other types of string concatenation in the strings module which follows this one
+console.log("My name is", bot);
+console.log("I am a", occupation);
+console.log("I live in", botLocation);
+
+// teach reassignment
+bot = "professorBot";
+
+// review multiple arguments for console statement
+console.log("My nickname is", bot);
+
+// review reassignment
+bot = "awesomeTeacherBot";
+
+// review multiple arguments for console statement
+console.log("I love my nickname but I wish people would call me", bot);
+
+// teach const and initialization
+const favoriteSubject = "Computer Science";
+const favoriteHobby = "playing video games";
+
+// review multiple arguments for console statement
+console.log("My favorite subject is", favoriteSubject);
+console.log("I love to teach others about", favoriteSubject);
+console.log("When I am not teaching I love", favoriteHobby);
+console.log("I know some do not like it but I love", favoriteHobby);
+
+console.log("Well, it was nice to talk to you. Have a nice day!");
