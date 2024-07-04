6d2a2189cb38153fb9227b924cd5cb5d49caefb7
A Cottrill <a.rcottrill521@gmail.com>
2024-07-04 12:30:32 -0400

    feat: use assert and explain that campers write tests on own

--- javascript-algorithms-and-data-structures/learn-callbacks-by-building-a-testing-lib/script.js
+++ javascript-algorithms-and-data-structures/learn-callbacks-by-building-a-testing-lib/script.js
@@ -1,21 +1,10 @@
-
 function test(title, callback) {
   try {
-    callback()
-    console.log(`✓ ${title}`)
+    callback();
+    console.log(`✓ ${title}`);
   } catch (error) {
-    console.error(`✕ ${title}`)
-    console.error(error)
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
-    }
+    console.error(`✕ ${title}`);
+    console.error(error);
   }
 }
 
@@ -23,52 +12,41 @@ function assert(actual) {
   return {
     deepEqual(expected) {
       for (let i = 0; i < actual.length; i++) {
-        expect(actual[i]).toBe(expected[i]);
+        if (actual[i] !== expected[i]) {
+          throw new Error(
+            `Element at index ${i} is different: ${actual[i]} is not equal to ${expected[i]}`
+          );
+        }
       }
-    }
-  }
+    },
+  };
 }
 
-
-function sum(num1, num2) {
-  return num1 + num2;
+function multipleBy2(num) {
+  return num * 2;
 }
 
-const numToString = (num) => num.toString();
-
-const isEven = (num) => num % 2 === 0;
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('map fails to convert array to a different array', () => {
+test("[1, 2, 3].map(multipleBy2) returns new array of [2,4,6]", () => {
   const input = [1, 2, 3];
-  const expectedOutput = ['1', '2', '3'];
-  expect(input.map(numToString)).toBe(expectedOutput);
-});
-
-test('converts numbers to strings', () => {
-  const input = [1, 2, 3];
-  const output = input.map(numToString);
-  const expectedOutput = ['1', '2', '3'];
-  for (let i = 0; i < input.length; i++) {
-    expect(output[i]).toBe(expectedOutput[i]);
-  }
-});
-
-test('map fails to convert array to a different array', () => {
-  const input = [1, 2, 3];
-  const expectedOutput = ['1', '2', '3'];
-  expect(input.map(numToString)).toBe(expectedOutput);
-});
-
-
-test("keeps only event numbers", () => {
-  const input = [1, 2, 3, 4, 5, 6];
-  const output = input.filter(isEven);
   const expectedOutput = [2, 4, 6];
-  assert(output).deepEqual(expectedOutput);
+  assert(input.map(multipleBy2)).deepEqual(expectedOutput);
+});
+
+// have campers write tests on their own
+// and have two steps that deal with trying to call 
+// the callback directly to demonstrate why it doesn't work.  
+
+
+test("[12, 6, 8, 24].map(multipleBy2) returns new array of [ 24, 12, 16, 48 ]", () => {
+  const input = [12, 6, 8, 24];
+  const expectedOutput = [24, 12, 16, 48];
+  assert(input.map(multipleBy2)).deepEqual(expectedOutput);
+});
+
+test("[12, 6].map(multipleBy2) should not equal [1, 2, 3, 4]", () => {
+  const input = [12, 6];
+  const expectedOutput = [1, 2, 3, 4];
+  assert(input.map(multipleBy2)).deepEqual(expectedOutput);
 });
 
 
