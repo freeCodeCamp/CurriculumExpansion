59fd385ac4d485f7578fe2c1999f089314f614d6
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2024-07-05 11:18:33 -0700

    chore: adding 1st JS only workshop (#411)
    * chore: adding 1st JS only workshop
    
    * chore: applying code review suggestions

---
+++ frontend-cert/js-projects/greeting-bot/script.js
@@ -0,0 +1,44 @@
+// This will be the first workshop in new JS section
+
+// create steps for console.log and basic string usage
+console.log("Hi there!");
+console.log("I am excited to talk to you");
+
+// create steps just for declaring variables
+let bot;
+// introduce camel case here
+let botLocation;
+
+// add steps to teach assignment
+bot = "teacherBot";
+botLocation = "the universe";
+
+// add review step for adding console statement
+console.log("Allow me to introduce myself");
+
+// teach multiple arguments for console statement
+// We will teach other types of string concatenation in the strings module which follows this one
+console.log("My name is", bot);
+console.log("I live in", botLocation);
+
+// teach reassignment
+bot = "professorBot";
+
+// review multiple arguments for console statement
+console.log("My nickname is", bot);
+
+// review reassignment
+bot = "awesomeTeacherBot";
+
+// review multiple arguments for console statement
+console.log("I love my nickname but I wish people would call me", bot);
+
+// teach const and initialization
+const favoriteSubject = "Computer Science";
+
+// review multiple arguments for console statement
+console.log("My favorite subject is", favoriteSubject);
+console.log("I love to teach others about", favoriteSubject);
+
+// review step for adding console statement
+console.log("Well, it was nice to talk to you. Have a nice day!");
