50e7b994ff613e6808c074edc01dbc2b858e8105
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-07-09 14:28:39 +0200

    add user stories

--- frontend-cert/js-projects/random-bg-changer-app/script.js
+++ frontend-cert/js-projects/random-bg-changer-app/script.js
@@ -1,4 +1,36 @@
 const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+
+function getRandomIndex() {
+  const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+  return randomIndex;
+}
+
+const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+
+function changeBackgroundColor() {
+  const color = darkColorsArr[getRandomIndex()];
+
+  bgHexCodeSpanElement.innerText = color;
+  body.style.backgroundColor = color;
+}
+const btn = document.querySelector("#btn");
+
+btn.onclick = changeBackgroundColor;
+
+/* seed code
+const darkColorsArr = [
     "#2C3E50",
     "#34495E",
     "#2C2C2C"
@@ -29,5 +61,7 @@ const darkColorsArr = [
     body.style.backgroundColor = color;
   }
   const btn = document.querySelector("#click-btn");
+  console.log(btn)
   
-  btn.onclick = changeBackgroundColor;
+  btn.onclick = changeBackgroundColor();
+*/
