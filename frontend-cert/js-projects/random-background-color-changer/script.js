ee4119782f36f30f0273609498f16801b0210a31
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-07-09 12:04:23 +0200

    add seed code

---
+++ frontend-cert/js-projects/random-background-color-changer/script.js
@@ -0,0 +1,31 @@
+const darkColorsArr = [
+    "#2C3E50",
+    "#34495E",
+    "#2C2C2C"
+    "#616A6B"
+    "#4A235A"
+    "#2F4F4F",
+    "#0E4B5A",
+    "#36454F",
+    "#2C3E50",
+    "#800020",
+  ];
+  
+  function getRandomIndex() {
+    const randomIndex = darkColorsArr.length * math.random();
+    return randomIndex;
+  }
+  
+  const body = document.queryselector("body");
+  const bgHexCodeSpanElement = document.querySelector("bg-hex-code");
+  console.log(bgHexCodeSpanElement)
+  
+  function changeBackgroundColor() {
+    const color = darkColorsArr[getRandomIndex];
+  
+    bgHexCodeSpanElement.innerText = color;
+    body.style.backgroundColor = color;
+  }
+  const btn = document.querySelector("#click-btn");
+  
+  btn.onclick = changeBackgroundColor;
