498ae71356fe62810799293af41fcd9f3f987efa
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-07-24 13:46:29 +0200

    refactor code and add user stories

--- frontend-cert/js-projects/inventory-management-app/script.js
+++ frontend-cert/js-projects/inventory-management-app/script.js
@@ -1,68 +1,67 @@
 let inventory = [];
 
-const findProduct = (productName) => {
-    let product = null;
+const findProductIndex = (productName) => {
+    let productIndex = -1;
     inventory.forEach((element, index) => {
         if (element.name === productName) {
-            product = element;
-            console.log(product);
-            return index;
+            productIndex = index;
         }
     })
-    if (!product) {
-        console.log(`${productName} not found`)
-        return null;
-    }
+    return productIndex;
 }
 
 const addProduct = (product) => {
-    for (let i of inventory) {
-        if (i.name === product.name) {
-            i.quantity += product.quantity;
-            console.log(`${product.name} quantity updated`)
-            return;
-        }
+    const productIndex = findProductIndex(product.name);
+    if (productIndex < 0) {
+        inventory.push(product);
+        console.log(`${product.name} added to inventory`)
+        return;
     }
-    inventory.push(product);
-    console.log(`${product.name} added to inventory`)
+    inventory[productIndex].quantity += product.quantity;
+    console.log(`${product.name} quantity updated`)
 }
 
-const removeProduct = (productName, quantity) => {
-    for (let i = 0; i < inventory.length; i++) {
-        if (inventory[i].name === productName) {
-            if (inventory[i].quantity >= quantity) {
-                inventory[i].quantity -= quantity;
-                console.log(`Remaining ${productName} pieces: ${inventory[i].quantity}`)
-                if (!inventory[i].quantity) {
-                    inventory.splice(i, 1);
-                }
-                return;
-            } else {
-                console.log(`Not enough ${productName} available`)
-                return;
-            }
-        }
+const removeProduct = (productName, productQuantity) => {
+    const productIndex = findProductIndex(productName);
+    if (productIndex < 0) {
+        console.log(`${productName} not found`);
+        return;
     }
-    console.log(`${productName} not found`)
+    const foundProduct = inventory[productIndex];
+    if (foundProduct.quantity >= productQuantity) {
+        foundProduct.quantity -= productQuantity;
+        console.log(`Remaining ${productName} pieces: ${foundProduct.quantity}`)
+        if (!foundProduct.quantity) {
+            inventory.splice(productIndex, 1);
+        }
+        return;
+    }
+    console.log(`Not enough ${productName} available, remaining pieces: ${foundProduct.quantity}`);
 }
-/*
+
+// Example usage
+
 const product1 = {
     name: 'flour',
-    quantity: 5
+    quantity: 15
 }
-
 const product2 = {
     name: 'flour',
     quantity: 10
 }
-
 const product3 = {
     name: 'sugar',
     quantity: 20
 }
-
 const product4 = {
     name: 'salt',
     quantity: 12
 }
-*/
+
+addProduct(product1);
+addProduct(product2);
+addProduct(product3);
+addProduct(product4);
+removeProduct('flour', 30);
+removeProduct('flour', 20);
+removeProduct('flour', 5);
