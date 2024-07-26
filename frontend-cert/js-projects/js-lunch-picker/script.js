0161c70dafbba0362a7d049fe9182f2ea97fc7ca
zairahira <33151350+zairahira@users.noreply.github.com>
2024-07-26 13:26:15 +0500

    fix: enhance prototype

--- frontend-cert/js-projects/js-lunch-picker/script.js
+++ frontend-cert/js-projects/js-lunch-picker/script.js
@@ -1,13 +1,19 @@
-let meals = [
-  "Biryani",
-  "Spicy Noodles",
-  "Chicken Salad",
-  "Pizza",
-  "Taco Salad",
-  "Beef Tacos",
-  "Sushi"
-];
 
-let randomIndex = Math.floor(Math.random() * meals.length);
+// task 1: declare these arrays:
 
-console.log("Today you should eat:", meals[randomIndex]);
+let kidsMeal = ["Chicken Nuggets", "Pizza", "Burgers", "Tacos", "Fries", "Prawns"];
+let adultsMeal = ["Salad", "Wraps", "Hummus", "Rice"];
+
+// task 2: remove "prawns" from kidsMeal using pop()
+
+kidsMeal.pop();
+
+// task 3: add "beef steak" to adultMeal using push()
+
+adultsMeal.push("Beef Steak");
+
+let randomIndexKids = Math.floor(Math.random() * kidsMeal.length);
+let randomIndexAdults = Math.floor(Math.random() * adultsMeal.length);
+
+console.log("Today kids should eat:", kidsMeal[randomIndexKids]);
+console.log("Today adults should eat:", adultsMeal[randomIndexAdults]);
