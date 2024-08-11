a95c167e986fd7fd9d9c296870e7a8cc0fd0d8b7
zairahira <33151350+zairahira@users.noreply.github.com>
2024-08-11 17:15:02 +0500

    fix: update user stores and script

--- frontend-cert/js-projects/lab-madlibs/script.js
+++ frontend-cert/js-projects/lab-madlibs/script.js
@@ -1,3 +1,6 @@
+//Objective: create 2 different stories using the same template
+// Reassign the variables to create a new story
+
 // Words to fill in the placeholders
 let adjective = "funny";
 let noun = "dragon";
@@ -6,7 +9,49 @@ let place = "garden";
 let adjective2 = "sparkling";
 let noun2 = "cakes";
 
-let story = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat "  + noun2 + ". The "+ noun +  " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+let story =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+// output the story
+console.log("First story:\n" + story);
 
-// Print the final story
-console.log(story);
+// reassign the variables
+
+adjective = "cute";
+noun = "puppy";
+verb = "barking";
+place = "park";
+adjective2 = "colorful";
+noun2 = "flower";
+
+// create the second story
+story =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story:\n" + story);
--- frontend-cert/js-projects/lab-madlibs/user-stories.md
+++ frontend-cert/js-projects/lab-madlibs/user-stories.md
@@ -1,5 +1,3 @@
-Madlibs is a word game where one player prompts others for a list of words to substitute for blanks in a story before reading the story aloud. In this lab, you will create a Madlibs story using variables and string concatenation.
-
 1. You should declare the following variables using `let`:
 - `adjective`
 - `noun`
@@ -12,8 +10,14 @@ Madlibs is a word game where one player prompts others for a list of words to su
 
 1. You should declare a `story` variable using `let` to store the complete story.
    
-1. You should use the following story template to create a story and assign it to the `story` variable:
+1. You should use the following story template to create the first story and assign it to the `story` variable:
 
 `"Once upon a time, there was a(n) [adjective] [noun] who loved to eat [noun2]. The [noun] lived in a [place] and had [adjective2] nostrils that blew fire when it was [verb]."`;
 
-1. You should output the final story to the console.
+1. You should output your first story to the console.
+
+1. You should assign new values to your `adjective`, `noun`, `verb`, `place`, `adjective2`, and `noun2` variables.
+
+1. Create another story using the same template and assign it to the `story` variable.
+
+1. You should output your second story to the console.
