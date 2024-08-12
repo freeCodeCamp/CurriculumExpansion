7f0bcce5beb1feef7160be6407a7e0afa4cd97c5
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2024-08-12 19:35:33 +0600

    Made minor adjustments to the code

--- frontend-cert/js-projects/weather-app/script.js
+++ frontend-cert/js-projects/weather-app/script.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  getLocation()
-});
-
-function getLocation() {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(getWeather);
   } else {
-    alert('Geolocation is NOT Available');
+    alert('Geolocation is not available');
   }
-}
+});
 
 async function getWeather(location) {
   const url = `https://weather-proxy.freecodecamp.rocks/api/current?lat=${location.coords.latitude}&lon=${location.coords.longitude}`
-  let unit = "c"
+  
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -25,12 +21,16 @@ async function getWeather(location) {
     
     document.getElementById("weather-icon").src = json.weather[0].icon
 
-    document.getElementById("temperature").textContent = document.getElementById("temperature").textContent.replace("{{template}}", json.main.temp)
+    document.getElementById("main-temperature").textContent = document.getElementById("main-temperature").textContent.replace("{{template}}", json.main.temp)
     document.getElementById("feels-like").textContent = document.getElementById("feels-like").textContent.replace("{{template}}", json.main.feels_like)
+
     document.getElementById("humidity").textContent += json.main.humidity
+
     document.getElementById("wind").textContent += json.wind.speed
     document.getElementById("wind-gust").textContent += json.wind.gust
+    
     document.getElementById("weather-main").textContent += json.weather[0].main
+    
     document.getElementById("location").textContent = `${json.weather[0].description.toUpperCase()} in ${json.name.toUpperCase()}`
   } catch (error) {
     console.error(error.message);
