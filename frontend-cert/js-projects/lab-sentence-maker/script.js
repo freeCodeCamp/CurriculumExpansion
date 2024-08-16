17906aa8126bd39ff1d4d4c4444e30274c4f08fd
Zaira <33151350+zairahira@users.noreply.github.com>
2024-08-16 19:06:44 +0500

    Fix/modify madlibs (#527)
    * fix: update user stores and script
    
    * fix: update project name
    
    * fix: update prototype

---
+++ frontend-cert/js-projects/lab-sentence-maker/script.js
@@ -0,0 +1,33 @@
+// Build a sentence maker
+
+//Objective: create 2 different stories using the same template
+// Reassign the variables to create a new story
+
+// Words to fill in the placeholders
+let adjective = "funny";
+let noun = "dragon";
+let verb = "jumping";
+let place = "garden";
+let adjective2 = "sparkling";
+let noun2 = "cakes";
+
+const firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. 
+The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// output the story
+console.log("First story:\n" + firstStory);
+
+// reassign the variables
+
+adjective = "cute";
+noun = "puppy";
+verb = "barking";
+place = "park";
+adjective2 = "colorful";
+noun2 = "flower";
+
+// create the second story
+const secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. 
+The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story:\n" + secondStory);
