3014e378f1e2e8441467230a8a18d317e3cece93
zairahira <33151350+zairahira@users.noreply.github.com>
2024-08-16 18:55:06 +0500

    fix: update prototype

--- frontend-cert/js-projects/lab-sentence-maker/script.js
+++ frontend-cert/js-projects/lab-sentence-maker/script.js
@@ -11,24 +11,11 @@ let place = "garden";
 let adjective2 = "sparkling";
 let noun2 = "cakes";
 
-let story =
-  "Once upon a time, there was a(n) " +
-  adjective +
-  " " +
-  noun +
-  " who loved to eat " +
-  noun2 +
-  ". The " +
-  noun +
-  " lived in a " +
-  place +
-  " and had " +
-  adjective2 +
-  " nostrils that blew fire when it was " +
-  verb +
-  ".";
+const firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. 
+The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
 // output the story
-console.log("First story:\n" + story);
+console.log("First story:\n" + firstStory);
 
 // reassign the variables
 
@@ -40,20 +27,7 @@ adjective2 = "colorful";
 noun2 = "flower";
 
 // create the second story
-story =
-  "Once upon a time, there was a(n) " +
-  adjective +
-  " " +
-  noun +
-  " who loved to eat " +
-  noun2 +
-  ". The " +
-  noun +
-  " lived in a " +
-  place +
-  " and had " +
-  adjective2 +
-  " nostrils that blew fire when it was " +
-  verb +
-  ".";
-console.log("Second story:\n" + story);
+const secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. 
+The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story:\n" + secondStory);
--- frontend-cert/js-projects/lab-sentence-maker/user-stories.md
+++ frontend-cert/js-projects/lab-sentence-maker/user-stories.md
@@ -8,16 +8,16 @@
 
 1. You should assign the above variables some string values of your choice.
 
-1. You should declare a `story` variable using `let` to store the complete story.
+1. You should declare a `firstStory` variable using `const` to store the first story.
    
-1. You should use the following story template to create the first story and assign it to the `story` variable:
+1. You should use the following story template to create the first story and assign it to the `firstStory` variable:
 
 `"Once upon a time, there was a(n) [adjective] [noun] who loved to eat [noun2]. The [noun] lived in a [place] and had [adjective2] nostrils that blew fire when it was [verb]."`;
 
-1. You should output your first story to the console.
+1. You should output your first story to the console using the message `"First story:\n" + firstStory`.
 
 1. You should assign new values to your `adjective`, `noun`, `verb`, `place`, `adjective2`, and `noun2` variables.
 
-1. Create another story using the same template and assign it to the `story` variable.
+1. Create another story using the same template and assign it to the `secondStory` variable.
 
-1. You should output your second story to the console.
+1. You should output your second story to the console using the message `"Second story:\n" + secondStory`.
