131eb3c40215238b1af51aa3aa4db919b1945da7
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-09-12 17:30:22 +0200

    refactor: object lab (JS only) (#565)
    * refactor quiz game
    
    * remove user input

--- frontend-cert/js-projects/quiz-game/script.js
+++ frontend-cert/js-projects/quiz-game/script.js
@@ -1,49 +1,60 @@
-const questions = {
-    science: [],
-    geography: [],
-    literature: []
-};
+const questions = [
+    {
+        category: "science",
+        question: "What is the chemical symbol for potassium?",
+        choices: ["P", "K", "Pt"],
+        answer: "K"
+    },
+    {
+        category: "science",
+        question: "What is the unit of electrical resistance?",
+        choices: ["Ohm", "Coulomb", "Sievert"],
+        answer: "Ohm"
+    },
+    {
+        category: "geography",
+        question: "What is the capital city of Australia?",
+        choices: ["Sidney", "Canberra", "Wellington"],
+        answer: "Canberra"
+    },
+    {
+        category: "literature",
+        question: 'Who wrote "1984"?',
+        choices: ["Ray Bradbury", "Aldous Huxley", "George Orwell"],
+        answer: "George Orwell"
+    },
+    {
+        category: "sport",
+        question: "How many players are on a standard volleyball team?",
+        choices: ["6", "7", "12"],
+        answer: "6"
+    }
+]
 
-const question1 = {
-    question: "What is the chemical symbol for potassium?",
-    choices: ["P", "K", "Pt"],
-    answer: "K"
-};
-
-questions.science.push(question1);
-
-const question2 = {
-    question: "What is the unit of electrical resistance?",
-    choices: ["Ohm", "Coulomb", "Sievert"],
-    answer: "Ohm"
-};
-
-questions.science.push(question2);
-
-const question3 = {
-    question: "What is the capital city of Australia?",
-    choices: ["Sidney", "Canberra", "Wellington"],
-    answer: "Canberra"
-};
-questions.geography.push(question3);
-
-const question4 = {
-    question: 'Who wrote "1984"?',
-    choices: ["Ray Bradbury", "Aldous Huxley", "George Orwell"],
-    answer: "George Orwell"
-};
-questions.literature.push(question4);
-
-const category = "science";
-let chosenQuestion = questions[category].shift();
-
-console.log(chosenQuestion.question);
-console.log(chosenQuestion.choices);
-
-const answer = "K";
-if (answer === chosenQuestion.answer) {
-    console.log("The answer is correct!");
-} else {
-    console.log(`Wrong answer, the correct answer is: ${chosenQuestion.answer}`);    
+function getRandomQuestion() {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    return questions[randomIndex];
 }
 
+function getRandomComputerChoice(choices) {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function getResults(obj) {
+    return computerChoice === obj.answer
+        ? "The computer's choice is correct!"
+        : `The computer's choice is wrong. The correct answer is: ${obj.answer}`;
+}
+
+const questionObj = getRandomQuestion();
+const { question, choices } = questionObj;
+
+console.log(question);
+console.log(`Choices: ${choices}`);
+
+const computerChoice = getRandomComputerChoice(choices);
+console.log(`Computer chooses: ${computerChoice}`);
+
+const results = getResults(questionObj);
+console.log(results);
