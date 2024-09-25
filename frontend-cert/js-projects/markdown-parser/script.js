cbd790c21fd581e1d0993a04a538f5de1b959a4c
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-09-25 18:31:25 +0200

    refactor script.js to show raw html

--- frontend-cert/js-projects/markdown-parser/script.js
+++ frontend-cert/js-projects/markdown-parser/script.js
@@ -1,68 +1,62 @@
-const h3Regex = /^###\s(.*$)/gm;
-const h3Replacement = "<h3>$1</h3>";
-
-const h2Regex = /^##\s(.*$)/gm;
-const h2Replacement = "<h2>$1</h2>";
-
-const h1Regex = /^#\s(.*$)/gm;
-const h1Replacement = "<h1>$1</h1>";
-
-const boldRegex = /((?:\*|_){2})(.*)\1/g;
-const boldReplacement = "<strong>$2</strong>";
-
-const italicRegex = /(\*|_)(.*)\1/g;
-const italicReplacement = "<em>$2</em>";
-
-const imageRegex = /!\[\s*(.*?)\s*\]\(\s*(.*?)\s*\)/g;
-const imageReplacement = '<img alt="$1" src="$2">';
-
-const linkRegex = /\[(.*?)\]\(\s*(.*?)\s*\)/g;
-const linkReplacement = '<a href="$2">$1</a>';
-
-const blockquoteRegex = /^> (.*?)$/gm;
-const blockquoteReplacement = '<blockquote>$1</blockquote>';
-
-const unorderedListRegex1 = /((?:^-\s+.*?$\n)+\s*?)\n/gm
-const unorderedListReplacement1 = "<ul>\n$1</ul>";
-
-const unorderedListRegex2 = /^-\s+(.*?$)\n/gm
-const ListReplacement2 = "<li>$1</li>\n";
-
-const orderedListRegex1 = /((?:^\d*\.\s+.*?$\n)+\s*?)\n/gm;
-const orderedListReplacement1 = "<ol>\n$1</ol>";
-
-const orderedListRegex2 = /^\d*\.\s+(.*?$)\n/gm;
-
-const replacementArray = [
-    [h3Regex, h3Replacement],
-    [h2Regex, h2Replacement],
-    [h1Regex, h1Replacement],
-    [boldRegex, boldReplacement],
-    [italicRegex, italicReplacement],
-    [imageRegex, imageReplacement],
-    [linkRegex, linkReplacement],
-    [blockquoteRegex, blockquoteReplacement],
-    [unorderedListRegex1, unorderedListReplacement1],
-    [unorderedListRegex2, ListReplacement2],
-    [orderedListRegex1, orderedListReplacement1],
-    [orderedListRegex2, ListReplacement2],
+const patternReplacementArray = [
+    { h6: [/^######[ \t](.+$)/gm, "<h6>$1</h6>\n"] },
+    { h5: [/^#####[ \t](.+$)/gm, "<h5>$1</h5>\n"] },
+    { h4: [/^####[ \t](.+$)/gm, "<h4>$1</h4>\n"] },
+    { h3: [/^###[ \t](.+$)/gm, "<h3>$1</h3>\n"] },
+    { h2: [/^##[ \t](.+$)/gm, "<h2>$1</h2>\n"] },
+    { h1: [/^#[ \t](.+$)/gm, "<h1>$1</h1>\n"] },
+    { hr: [/(?<=\n|^)---(?=\n|$)/, "<hr>\n"] },
+    { strong: [/((?:\*|_){2})(.*?)\1/g, "<strong>$2</strong>"] },
+    { em: [/(\*|_)(.*?)\1/g, "<em>$2</em>"] },
+    { img: [/!\[\s*(.*?)\s*\]\(\s*(.*?)\s*\)/g, '<img alt="$1" src="$2">'] },
+    { a: [/\[(.*?)\]\(\s*(.*?)\s*\)/g, '<a href="$2">$1</a>'] },
+    { blockquote1: [/^> ([^\n]+?)$/gm, "<blockquote>$1</blockquote>\n"] },
+    { blockquote2: [/(?<=<blockquote>)(.+?)<\/blockquote>\n\n^([^<\n]+?)$/gm, "$1 $2</blockquote>\n"] },
+    { ul: [/(?:^[-*+]\s+.*?$\n)+\s*?/gm, "<ul>\n$&</ul>\n\n"] },
+    { ol: [/(?:^\d*\.\s+?\w.*?$\n)+\s*?/gm, "<ol>\n$&</ol>\n\n"] },
+    { uli: [/^[-*+]\s+(.*?$)\n/gm, "<li>$1</li>\n"] },
+    { oli: [/^\d*\.\s+?(.*?$)\n/gm, "<li>$1</li>\n"] },
+    { p1: [/^([^<>\s].*?)(?=\n\s*$)/gsm, "<p>$1</p>\n"] },
+    { p2: [/^\w*?(?:<strong>|<em>).+(?:<\/strong>|<\/em>)\s*\w*?[ \t]*$/gm, "<p>$&</p>\n"] }
 ]
+
 const markdownInput = document.getElementById('markdown-input');
 const convertMarkdown = () => {
     let markdown = markdownInput.value;
-    replacementArray.forEach((item) => {
-        markdown = markdown.replace(item[0], item[1]);
+    let matchUList = false;
+    let matchOList = false;
+    patternReplacementArray.forEach((item) => {
+        const [key] = Object.keys(item);
+        const [pattern, replacement] = Object.values(item)[0];
+        if (key === "blockquote2") {
+            while (markdown.match(pattern)) {
+                markdown = markdown.replace(pattern, replacement);
+            }
+        } else if (key === "ul") {
+            if (markdown.match(pattern)) {
+                matchUList = true;
+                markdown = markdown.replace(pattern, replacement);
+            }
+
+        } else if (key === "ol") {
+            if (markdown.match(pattern)) {
+                matchOList = true;
+                markdown = markdown.replace(pattern, replacement);
+            }
+
+        } else if (key === "uli" && !matchUList) {
+            return;
+        } else if (key === "oli" && !matchOList) {
+            return;
+        } else {
+            markdown = markdown.replace(pattern, replacement);
+        }
     })
-    return markdown.trim();
+    return markdown;
 }
 
 markdownInput.addEventListener('input', () => {
     const html = convertMarkdown();
     const out = document.getElementById('html-output');
-    out.innerHTML = html;
-    if (out.innerHTML !== "") {
-        out.style.display = "block";
-    } else {
-        out.style.display = "none";
-    }
+    out.textContent = html;
 });
