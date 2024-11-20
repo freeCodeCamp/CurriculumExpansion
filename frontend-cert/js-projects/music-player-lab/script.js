85d353765cf34105a1a2bec0ffac55b0c26946c3
Dario-DC <105294544+Dario-DC@users.noreply.github.com>
2024-11-20 22:59:15 +0100

    move playlist to html

--- frontend-cert/js-projects/music-player-lab/script.js
+++ frontend-cert/js-projects/music-player-lab/script.js
@@ -170,24 +170,6 @@ const highlightCurrentSong = () => {
   if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
-const renderSongs = (array) => {
-  const songsHTML = array
-    .map((song) => {
-      return `
-      <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info" onclick="playSong(${song.id})">
-          <span class="playlist-song-title">${song.title}</span>
-          <span class="playlist-song-artist">${song.artist}</span>
-          <span class="playlist-song-duration">${song.duration}</span>
-      </button>
-      </li>
-      `;
-    })
-    .join("");
-
-  playlistSongs.innerHTML = songsHTML;
-};
-
 const setPlayButtonAccessibleText = () => {
   const song = userData.currentSong || userData.songs[0];
 
@@ -213,5 +195,4 @@ previousButton.addEventListener("click", playPreviousSong);
 
 audio.addEventListener("ended", playNextSong);
 
-renderSongs(userData.songs);
 setPlayButtonAccessibleText();
