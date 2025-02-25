1862d09945c235ae799645496e000502e7ad4184
zaira <zairahira@gmail.com>
2025-02-25 15:38:51 +0500

    update code

--- fullstack-cert/react-projects/tic-tac-toe/index.jsx
+++ fullstack-cert/react-projects/tic-tac-toe/index.jsx
@@ -1,4 +1,4 @@
-export const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick }) => {
   return (
     <button className="square" onClick={onClick}>
       {value}
@@ -6,16 +6,11 @@ export const Square = ({ value, onClick }) => {
   );
 };
 
-export const calculateWinner = (squares) => {
+const calculateWinner = (squares) => {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
   ];
   for (let line of lines) {
     const [a, b, c] = line;
@@ -26,7 +21,7 @@ export const calculateWinner = (squares) => {
   return null;
 };
 
-export const Board = () => {
+const Board = () => {
   const [squares, setSquares] = React.useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = React.useState(true);
   const winner = calculateWinner(squares);
@@ -75,10 +70,4 @@ export const Board = () => {
   );
 };
 
-export const App = () => {
-  return (
-    <div className="App">
-      <Board />
-    </div>
-  );
-};
+export { Board };
