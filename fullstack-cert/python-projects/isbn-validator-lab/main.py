225bbe48aa73e20356352dd33ee87d597470baff
sidemt <25644062+sidemt@users.noreply.github.com>
2025-03-24 22:39:28 +0900

    update seed code and user stories

--- fullstack-cert/python-projects/isbn-validator-lab/main.py
+++ fullstack-cert/python-projects/isbn-validator-lab/main.py
@@ -99,66 +99,72 @@ main()
 ''' seed code
 
 def validate_isbn(isbn, length):
-    if length == 10:
-        return validate_isbn_10(isbn, length)
-    elif length == 13:
-        return validate_isbn_13(isbn, length)
-    else:
-        print('Length should be 10 or 13.')
-        return False
+    if len(isbn, length) != length:
+        print(f'ISBN-{length} code should be {length} digits long.')
+        return
 
+    main_digits = isbn[0:length]
+    given_check_digit = isbn[length]
 
-def validate_isbn_10(isbn):
-    if len(isbn) != 10:
-        print('ISBN-10 code should be 10 digits long.')
-        return False
-
-    main_digits = isbn[0:9]
-    given_check_digit = isbn[10]
-    digits_sum = 0
+    main_digits_list = [int(digit) for digit in main_digits]
 
     # Calculate the check digit from other digits
-    # Multiply each of the first 9 digits by its corresponding weight (10 to 2) and sum up the results
-    for index, digit in enumerate(main_digits):
-        digits_sum += int(digit) * (10 - index)
+    if length == 10:
+        expected_check_digit = calculate_check_digit_10(main_digits_list)
+    else:
+        expected_check_digit = calculate_check_digit_13(main_digits_list)
 
+    # Check if the given check digit matches with the calculated check digit
+    if given_check_digit == expected_check_digit:
+        print('Valid ISBN Code.')
+    else:
+        print('Invalid ISBN Code.')
+
+
+def calculate_check_digit_10(main_digits_list):
+    # Note: You don't have to fully understand the logic in this function.
+
+    digits_sum = 0
+    # Multiply each of the first 9 digits by its corresponding weight (10 to 2) and sum up the results
+    for index, digit in enumerate(main_digits_list):
+        digits_sum += digit * (10 - index)
     # Find the remainder of dividing the sum by 11, then subtract it from 11
-    calculated_check_digit = 11 - digits_sum % 11
-    # If the result is 11, use 0 instead
-    # If the result is 10, use upper case X instead
-    if calculated_check_digit == 11:
+    result = 11 - digits_sum % 11
+
+    # The calculation result can range from 1 to 11.
+    # If the result is 11, use 0.
+    # If the result is 10, use upper case X.
+    # Use the value as it is for other numbers.
+    if result == 11:
         expected_check_digit = '0'
-    elif calculated_check_digit == 10:
+    elif result == 10:
         expected_check_digit = 'X'
     else:
-        expected_check_digit = str(calculated_check_digit)
+        expected_check_digit = str(result)
+    return expected_check_digit
 
-    return given_check_digit == expected_check_digit
 
+def calculate_check_digit_13(main_digits_list):
+    # Note: You don't have to fully understand the logic in this function.
 
-def validate_isbn_13(isbn):
-    if len(isbn) != 13:
-        print('ISBN-13 code should be 13 digits long.')
-        return False
-
-    main_digits = isbn[0:12]
-    given_check_digit = isbn[12]
     digits_sum = 0
-
-    # Calculate the check digit from other digits
     # Multiply each of the first 12 digits by 1 and 3 alternately (starting with 1), and sum up the results
-    for index, digit in enumerate(main_digits):
+    for index, digit in enumerate(main_digits_list):
         if index % 2 == 0:
-            digits_sum += int(digit) * 1
+            digits_sum += digit * 1
         else:
-            digits_sum += int(digit) * 3
-
+            digits_sum += digit * 3
     # Find the remainder of dividing the sum by 10, then subtract it from 10
-    calculated_check_digit = 10 - digits_sum % 10
-    # If the result is 10, use 0 instead
-    expected_check_digit = calculated_check_digit % 10
+    result = 10 - digits_sum % 10
 
-    return given_check_digit == expected_check_digit
+    # The calculation result can range from 1 to 10.
+    # If the result is 10, use 0.
+    # Use the value as it is for other numbers.
+    if result == 10:
+        expected_check_digit = '0'
+    else:
+        expected_check_digit = str(result)
+    return expected_check_digit
 
 
 def main():
@@ -168,10 +174,10 @@ def main():
 
     length = int(values[1])
 
-    if validate_isbn(isbn, length):
-    print('Valid ISBN Code.')
+    if length == 10 or length == 13:
+    validate_isbn(isbn, length)
     else:
-    print('Invalid ISBN Code.')
+    print('Length should be 10 or 13.')
 
 
 main()
--- fullstack-cert/python-projects/isbn-validator-lab/user-stories.md
+++ fullstack-cert/python-projects/isbn-validator-lab/user-stories.md
@@ -6,14 +6,15 @@ Camperbot has tried to build their own ISBN validator. However, they have made a
 
 In this lab, you will fix the existing code and make it function properly.
 
-The user should be able to enter the ISBN code they want to validate in `ISBN,length` format. The ISBN code should not contain hyphens, followed by its length (`10` or `13`), separated by a comma.
+Expected behavior:
+When the user runs the program, it will show the prompt `Enter ISBN and length: `. The user can enter the ISBN code they want to validate in `ISBN,length` format. The ISBN code should not contain hyphens, followed by its length (`10` or `13`), separated by a comma.
 
 Example inputs:
 `1530051126,10` for ISBN-10 codes.
 `9781530051120,13` for ISBN-13 codes.
 
 How to find the check digit:
-- You don't have to know the detailed calculation logic in this lab. The functions `calculate_check_digit_10` and `calculate_check_digit_13` will take care of it.
+- You don't have to know the detailed calculation logic in this lab. The functions `calculate_check_digit_10` and `calculate_check_digit_13` will take care of the calculation and return the expected check digit in string.
 - The check digit for ISBN-10 codes can be a number from `0` to `9` or an uppercase letter `X`. 
 - The check digit for ISBN-13 codes can be a number from `0` to `9`. 
 
@@ -27,9 +28,11 @@ Fulfill the user stories below and get all the tests to pass to complete the lab
 1. Even if the user enters a non-numeric value for the length, the program should handle the `ValueError` without crashing.
 1. When the user enters a non-numeric value for the length, they should see the message `Length must be a number.` in the console, and the program should terminate.
 1. You should fix the `TypeError` in the current code that occurs when the user enters a valid ISBN code.
-1. You should fix the `IndexError` in the current code when the user enters a valid ISBN-10 code.
+1. You should fix the `IndexError` in the current code when the user enters a valid ISBN code.
 1. Even if the user enters an incorrect ISBN code with characters other than numbers, the program should handle the `ValueError` that occurs without crashing.
 1. When the user enters an incorrect ISBN code with characters other than numbers, they should see a message `Invalid character was found.` in the console.
+1. When the user enters `1530051126,10`, they should see a message `Valid ISBN Code.`
+1. When the user enters `9781530051120,13`, they should see a message `Valid ISBN Code.`
 
 When you complete the project, the user should see the following messages depending on the values they enter.
 
