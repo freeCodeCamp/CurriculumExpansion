2afd80de6568d1ec727598892f671444d7ecc484
zaira <zairahira@gmail.com>
2025-03-25 13:23:47 +0500

    add keyboard accessibility

--- coding-challenges/faq-component/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const faqItems = document.querySelectorAll(".faq-item");
-
-    faqItems.forEach(item => {
-        item.addEventListener("click", function () {
-            this.classList.toggle("active");
-        });
-    });
-});
