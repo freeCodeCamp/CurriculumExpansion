265c83b090979cef7cd2aa1451ba6ce6a121b8e3
jdwilkin4 <jwilkin4@hotmail.com>
2025-04-21 22:33:21 -0700

    chore: adding user stories

--- fullstack-cert/python-projects/selection-sort/main.py
+++ fullstack-cert/python-projects/selection-sort/main.py
@@ -1,4 +1,4 @@
-def selectionSort(list):
+def selection_sort(list):
     for i in range(len(list)):
         min_index = i
 
@@ -14,14 +14,14 @@ def selectionSort(list):
 # Test cases shown to campers
 
 
-print(selectionSort([33, 1, 89, 2, 67, 245]))
-print(selectionSort([5, 16, 99, 12, 567, 23, 15, 72, 3]))
-print(selectionSort([1, 4, 2, 8, 345, 123, 43,
+print(selection_sort([33, 1, 89, 2, 67, 245]))
+print(selection_sort([5, 16, 99, 12, 567, 23, 15, 72, 3]))
+print(selection_sort([1, 4, 2, 8, 345, 123, 43,
       32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]))
 
 # Test cases not shown to campers to prevent attempts at hardcoded solutions
 
-print(selectionSort([42, 17, 93, 8, 61, 29]))
-print(selectionSort([11, 4, 78, 23, 55, 198, 65, 90, 2]))
-print(selectionSort([9, 27, 3, 7, 101, 66, 34, 52, 87, 42, 12, 29]))
-print(selectionSort([5, 14, 33, 77, 2, 18, 92, 1, 100, 45, 73, 64, 28, 56]))
+print(selection_sort([42, 17, 93, 8, 61, 29]))
+print(selection_sort([11, 4, 78, 23, 55, 198, 65, 90, 2]))
+print(selection_sort([9, 27, 3, 7, 101, 66, 34, 52, 87, 42, 12, 29]))
+print(selection_sort([5, 14, 33, 77, 2, 18, 92, 1, 100, 45, 73, 64, 28, 56]))
---
+++ fullstack-cert/python-projects/selection-sort/user-stories.md
@@ -0,0 +1,10 @@
+# User Stories
+
+1. You should define a function named `selection_sort`.
+1. Your `selection_sort` function should have one parameter that represents the list of items.
+1. Your `selection_sort` function should take a list and sort the items in place from smallest to largest.
+1. `selection_sort([33, 1, 89, 2, 67, 245])` should return a list that is unchanged except for the order.
+1. `selection_sort([33, 1, 89, 2, 67, 245])` should return `[1, 2, 33, 67, 89, 245]`.
+1. `selection_sort([5, 16, 99, 12, 567, 23, 15, 72, 3])` should return `[3, 5, 12, 15, 16, 23, 72, 99, 567]`.
+1. `selection_sort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])` should return `[1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]`.
+1. Your `selection_sort` function should not use the built-in `sort()` method.
