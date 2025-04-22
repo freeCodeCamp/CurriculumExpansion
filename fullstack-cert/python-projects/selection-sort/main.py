4862a221eddeebcc12400565ea1b8c31d0d0cea5
jdwilkin4 <jwilkin4@hotmail.com>
2025-04-21 22:19:33 -0700

    feat: adding selection sort solution

---
+++ fullstack-cert/python-projects/selection-sort/main.py
@@ -0,0 +1,27 @@
+def selectionSort(list):
+    for i in range(len(list)):
+        min_index = i
+
+        for j in range(i + 1, len(list)):
+            if list[j] < list[min_index]:
+                min_index = j
+
+        if min_index != i:
+            list[i], list[min_index] = list[min_index], list[i]
+
+    return list
+
+# Test cases shown to campers
+
+
+print(selectionSort([33, 1, 89, 2, 67, 245]))
+print(selectionSort([5, 16, 99, 12, 567, 23, 15, 72, 3]))
+print(selectionSort([1, 4, 2, 8, 345, 123, 43,
+      32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]))
+
+# Test cases not shown to campers to prevent attempts at hardcoded solutions
+
+print(selectionSort([42, 17, 93, 8, 61, 29]))
+print(selectionSort([11, 4, 78, 23, 55, 198, 65, 90, 2]))
+print(selectionSort([9, 27, 3, 7, 101, 66, 34, 52, 87, 42, 12, 29]))
+print(selectionSort([5, 14, 33, 77, 2, 18, 92, 1, 100, 45, 73, 64, 28, 56]))
