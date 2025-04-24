bd6ead8f3a8d90e1125bc511d22792e28350b05d
jdwilkin4 <jwilkin4@hotmail.com>
2025-04-24 11:49:01 -0700

    chore: code review changes

--- fullstack-cert/python-projects/binary-search-workshop/main.py
+++ fullstack-cert/python-projects/binary-search-workshop/main.py
@@ -1,22 +1,18 @@
-import math
-
-
 def binary_search(search_list, value):
     path_to_target = []
     low = 0
     high = len(search_list) - 1
 
     while low <= high:
-        mid = math.floor((low + high) / 2)
+        mid = (low + high) // 2
+
+        path_to_target.append(search_list[mid])
 
         if value == search_list[mid]:
-            path_to_target.append(search_list[mid])
             return path_to_target
         elif (value > search_list[mid]):
-            path_to_target.append(search_list[mid])
             low = mid + 1
         else:
-            path_to_target.append(search_list[mid])
             high = mid - 1
 
     return 'Value Not Found'
