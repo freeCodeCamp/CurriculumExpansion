b28aefbbde6f8c378ca782b8211f270ba54fa3d8
A Cottrill <a.rcottrill521@gmail.com>
2025-08-03 13:52:46 -0400

    add some fields

---
+++ fullstack-cert/typescript-projects/resume-builder/src/Field.tsx
@@ -0,0 +1,9 @@
+import { EventHandler, ChangeEvent } from "react";
+
+export function Field({ labelText,fieldType,onChange }: { labelText: string,fieldType:string, onChange: EventHandler<ChangeEvent> }) {
+  return (
+    <>
+      <label>{labelText}  <input type={fieldType} onChange={onChange}></input></label>
+    </>
+  );
+}
