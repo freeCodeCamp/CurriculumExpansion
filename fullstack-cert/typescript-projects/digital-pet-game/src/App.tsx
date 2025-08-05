a0335d84a9096434ba12c651813258840615a21c
Robert-Beckett <robert.beckett@onion.red>
2025-08-04 21:30:20 -0300

    - Pulled in a mimic sprite and tamagotchi style frame. Currently the CSS   is dependent on the frame in order to fit the game into the correct   location. - Completed implementation of the pet "Actions", though they need to be   tidied up and built to work with the enums for the purpose of teaching   typescript. - Implemented a slow decay of pet happiness. - Set up initial calls to a temporary D&D API while we wait for the FCC   API to come up. - Built out a small tamagotchi container with an animated mimic within   it. Currently "animation" is just handled by swapping between animated   webp's and images.

--- fullstack-cert/typescript-projects/digital-pet-game/src/App.tsx
+++ fullstack-cert/typescript-projects/digital-pet-game/src/App.tsx
@@ -1,6 +1,95 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const ToyShell = new URL('assets/DigitalPetShell.webp', import.meta.url);
+const Heart = {
+  FULL: new URL('assets/Heart/00_heart_animated_1.png', import.meta.url),
+  MORE: new URL('assets/Heart/01_heart_animated_1.png', import.meta.url),
+  HALF: new URL('assets/Heart/02_heart_animated_1.png', import.meta.url),
+  LESS: new URL('assets/Heart/03_heart_animated_1.png', import.meta.url),
+  EMPTY: new URL('assets/Heart/04_heart_animated_1.png', import.meta.url),
+};
+
+type Size = "Tiny"|"Small"|"Medium"|"Large"|"Huge"|"Gargantuan";
+type Type = any;
+
+enum PetAction {
+  NONE,
+  EAT,
+  PLAY,
+  SLEEP,
+};
+
+enum PetState {
+  IDLE = "Idle",
+  DEATH = "Death",
+  SLEEP = "Sleep",
+  WAKE = "Wake",
+  WALK = "Walk",
+  TURN = "Turn",
+  JUMP = "Jump",
+};
+
+interface MonsterImages {
+  Idle: URL;
+  Death: URL;
+  Sleep: URL;
+  Wake: URL;
+  Walk: URL;
+  Turn: URL;
+  Jump: URL;
+}
+
+interface MonsterDetail {
+  index: string;
+  name: string;
+  size: Size;
+  type: Type;
+  alignment: string;
+  armor_class: any[];
+  images: MonsterImages;
+}
+
+interface MonsterSummary {
+  index: string;
+  name: string;
+  url: string;
+}
+
+interface MonstersList {
+  count: number;
+  results: MonsterSummary[];
+}
+
+const monstersBaseUri = 'https://www.dnd5eapi.co/api/2014/monsters/';
+
+const handleFetchErrors = (response: Response) => {
+  if (!response.ok) {
+    throw new Error('Error retrieving data: ' + response.statusText);
+  }
+};
+
+const getMonsters = async () => {
+  const response = await fetch(monstersBaseUri);
+  handleFetchErrors(response);
+  return await response.json() as MonstersList;
+};
+
+const getMonsterDetail = async (monsterName: string) => {
+  const response = await fetch(`${monstersBaseUri}${monsterName}`);
+  handleFetchErrors(response);
+  return await response.json() as MonsterDetail;
+};
+
+const loadMonsterArt = (): MonsterImages => ({
+  Idle: new URL('assets/Idle.webp', import.meta.url),
+  Death: new URL('assets/Death.webp', import.meta.url),
+  Sleep: new URL('assets/Hide.webp', import.meta.url),
+  Wake: new URL('assets/Reveal.webp', import.meta.url),
+  Walk: new URL('assets/Walk.webp', import.meta.url),
+  Turn: new URL('assets/Turn.webp', import.meta.url),
+  Jump: new URL('assets/Jump.webp', import.meta.url),
+});
 
 
 interface Pet
@@ -10,78 +99,138 @@ interface Pet
   hunger: number;
   energy: number; 
   species: String; 
+  action: PetAction;
+  images: MonsterImages;
+  state: PetState;
 }
 
 const saveKey = "pet"
 
-
 export function App()  {
-  let myPet: Pet = {
+  let [pet, setPet] = useState<Pet>({
     name: "",
     happiness: 100,
     hunger: 0,
     energy: 100,
     species: "",
-  }; 
+    action: PetAction.NONE,
+    images: loadMonsterArt(),
+    state: PetState.IDLE, 
+  }); 
   const [fact, setFact] = useState("");
 
+  useEffect(() => {
+    getMonsterDetail("mimic")
+      .then((monster: MonsterDetail) => {
+        console.log(monster);
+        setPet({
+          ...pet,
+          species: monster.name,
+        });
+      });
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPet(pet => ({
+        ...pet,
+        happiness: Math.max(pet.happiness - 5, 0),
+      }));
+    }, 5 * 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    console.log(pet.happiness);
+  }, [pet]);
+
   function feedPet() {
-      // TODO: consider multiple food types if time permits
-      const myFoodFill = 10;
-      if (myPet.hunger - myFoodFill > 0) {
-        myPet.hunger -= myFoodFill;
-      } else {
-        myPet.hunger = 0;
-      }
-    }
+    // TODO: consider multiple food types if time permits
+    const myFoodFill = 10;
+    setPet({
+      ...pet,
+      hunger: Math.max(pet.hunger - myFoodFill, 0),
+    });
+    pet.hunger -= myFoodFill;
+  }
 
-    function playWithPet() {
-      const myHappinessIncrease = 5;
-      const energyDecrease = 5;
-      if (myPet.energy - energyDecrease > 0) {
-        myPet.happiness += myHappinessIncrease;
-        myPet.energy -= energyDecrease;
-        fetchPetFact(); 
-      } else {
-        // Give warning that low energy
-      }
-    }
+  function playWithPet() {
+    const myHappinessIncrease = 5;
+    const energyDecrease = 5;
+    setPet({
+      ...pet,
+      happiness: Math.min(pet.happiness + myHappinessIncrease, 100),
+      energy: Math.max(pet.energy - energyDecrease, 0),
+      state: PetState.JUMP,
+    });
+    fetchPetFact(); 
+  }
 
-    function restPet() {
-      const hungerDecrease = 5;
-      const energyIncrease = 5;
-      if (myPet.hunger - hungerDecrease > 0) {
-        myPet.hunger -= 5;
-        myPet.energy += energyIncrease;
-      } else {
-        // Give warning that low hunger
-      }
-    }
+  function restPet() {
+    const hungerDecrease = 5;
+    const energyIncrease = 5;
+    setPet({
+      ...pet,
+      hunger: Math.max(pet.hunger - hungerDecrease, 0),
+      energy: Math.min(pet.energy += energyIncrease, 100),
+      state: PetState.SLEEP,
+    });
+  }
 
-    function fetchPetFact() {
-      // TODO: Put actual fetch here
-      const petFact = JSON.parse(
-        '{"fact":"Mimics have a shape-changing ability and can polymorph into any object."}'
-      );
-      setFact(petFact.fact); 
-    }
+  function hungrierPet() {
+    const hungerIncrease = 5;
+    setPet({
+      ...pet,
+      hunger: Math.min(pet.hunger += hungerIncrease, 100),
+        state: PetState.WALK,
+    });
+  }
 
-    function savePetData() {
-      localStorage.setItem(saveKey, JSON.stringify(myPet));
-    }
+  function fetchPetFact() {
+    // TODO: Put actual fetch here
+    const petFact = JSON.parse(
+      '{"fact":"Mimics have a shape-changing ability and can polymorph into any object."}'
+    );
+    setFact(petFact.fact); 
+  }
 
-    function loadPetData() {
-      myPet = JSON.parse(localStorage.getItem(saveKey) || "");
-    }
+  function getHeartIcon() {
+    if (pet.happiness >= 90) return Heart.FULL;
+    if (pet.happiness >= 75) return Heart.MORE;
+    if (pet.happiness >= 50) return Heart.HALF;
+    if (pet.happiness >= 10) return Heart.LESS;
+    return Heart.EMPTY;
+  }
+
+  function savePetData() {
+    localStorage.setItem(saveKey, JSON.stringify(pet));
+  }
+
+  function loadPetData() {
+    pet = JSON.parse(localStorage.getItem(saveKey) || "");
+  }
 
   return (
     <>
+      <div className="pet-shell">
+        <img src={ToyShell} />
+        <div className="pet-hud">
+          <p id="pet-species">MIMIC</p>
+          <img id="happiness-meter" src={getHeartIcon()} />
+          <div id="hunger-meter" />
+          <div id="energy-meter" />
+        </div>
+        <div className="pet-sprite">
+          <img src={pet.images[pet.state]} />
+        </div>
+      </div>
       <h1>Digital Pet Game</h1>
-      <h2>Pet Name: {myPet.name}</h2>
-      <p id="pet-species">Species: {myPet.species}</p>
-      <p id="happiness-meter">Happiness: {myPet.happiness}</p>
-      <p id="hunger-meter">Hunger: {myPet.hunger}</p>
-      <p id="energy-meter">Energy: {myPet.energy}</p>
+      <h2>Pet Name: {pet.name}</h2>
+      <p id="pet-species">Species: {pet.species}</p>
+      <p id="happiness-meter">Happiness: {pet.happiness}</p>
+      <p id="hunger-meter">Hunger: {pet.hunger}</p>
+      <p id="energy-meter">Energy: {pet.energy}</p>
       <p id="pet-fact">Pet Fact: {fact}</p>
       <button id="save-game" onClick={savePetData}>
         Save
@@ -98,6 +247,10 @@ export function App()  {
       <button id="reset-pet" onClick={restPet}>
         Tell pet to take a nap (5 increase to energy, 5 decrease to hunger)
       </button>
+      <button id="increase-hunger" onClick={hungrierPet}>
+        TEST [INCREASE HUNGER 5]
+      </button>
     </>
   );
 }
+
