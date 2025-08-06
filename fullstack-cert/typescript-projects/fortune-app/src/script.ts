e31d8e4b79dc006019382c87a524fa30f003fd95
Teklu Abayneh <tekluabayneh@gmail.com>
2025-08-06 18:21:14 +0000

    feat: add click handler to card elements to update displayed card data

--- fullstack-cert/typescript-projects/fortune-app/src/script.ts
+++ fullstack-cert/typescript-projects/fortune-app/src/script.ts
@@ -1,21 +1,41 @@
-
 interface Card
 {
   name: string; 
   value: number; 
+  name_short:string,
+  value_int:number,
+   suit:string,
+   type:boolean,
+   img:string,
+   meaning_up:string,
+   meaning_rev:string,
+   desc:string,
 }
+
 
 interface Deck
 {
   cards: Card[]; 
 }
 
+
+
 const singleCardBtn = document.getElementById("btn-single-card") as HTMLButtonElement;
 const multipleCardsBtn = document.getElementById("btn-multiple-cards") as HTMLButtonElement;
 const fortuneContainer = document.getElementsByClassName("fortune_container")[0] as HTMLElement;
+const fortune_description = document.getElementsByClassName("fortune_description")[0] as HTMLElement;
 const multiple_card = document.getElementsByClassName("multiple_card")[0] as HTMLElement;
 const singleCard = document.getElementsByClassName("single_card")[0] as HTMLElement;
 const newReadingBtn = document.getElementsByClassName("btn_reveal")[0] as HTMLButtonElement;
+const title = document.querySelector(".title")! as HTMLElement 
+const header_title = document.querySelector(".header_title")! as HTMLElement 
+const sub_title = document.querySelector(".sub_title")! as HTMLElement 
+const Cardtitle =  document.querySelector(".desc_title")! as HTMLSpanElement
+const description = document.querySelector(".description")! as HTMLElement
+
+
+
+
 const apiURL = "https://cdn.freecodecamp.org/curriculum/typescript/tarot-app";
 
 const dataUrl = apiURL + "/card_data.json"; 
@@ -28,6 +48,8 @@ document.addEventListener("DOMContentLoaded", async function () {
   // This is the logic for where the card is fetched and parsed
   const response = await fetch(dataUrl); 
   cardData = await response.json(); 
+
+
 }); 
 
 const HideElement = (...element:HTMLElement[]) => {
@@ -36,11 +58,12 @@ element.forEach(el => {
 })
 }
 
-const cardTemplate = function (drawingType: string, cardName: string, value: number, isReversed : boolean) {
+
+const cardTemplate = function (drawingType: string, cardName: string, value:number,  isReversed : boolean, shortName:string) {
   return `
  <div>
     <h2>${drawingType}</h2>
-    <div class="card_container ">
+    <div class="card_container " data-id=${shortName}>
         <div class="card_front ${isReversed ? 'reversed-card' : ""}"> 
             <span>${value}</span>
             <span>${cardName}</span>
@@ -51,6 +74,7 @@ const cardTemplate = function (drawingType: string, cardName: string, value: num
   `;
 };
 
+
 const ShowElement = (...element:HTMLElement[]) =>{
     element.forEach(el => {
         el.classList.remove("hidden")
@@ -58,7 +82,7 @@ const ShowElement = (...element:HTMLElement[]) =>{
 }
 // reveal card for single card
 singleCardBtn.addEventListener("click", (e:Event) =>{ 
-  HideElement(singleCardBtn, multipleCardsBtn, multiple_card, text);
+  HideElement(singleCardBtn, multipleCardsBtn, multiple_card, text, header_title);
   const isReversed = Math.round(Math.random()) + 1 === 1; 
   let chosenCardIndex = Math.round(Math.random() * cardData.cards.length - 1);
   console.log(chosenCardIndex); 
@@ -69,7 +93,8 @@ singleCardBtn.addEventListener("click", (e:Event) =>{
       "Your card",
       chosenCard.name,
       chosenCard.value,
-      isReversed
+      isReversed ,
+      chosenCard.name_short
     );
     multiple_card.innerHTML = "";
 })
@@ -78,7 +103,7 @@ singleCardBtn.addEventListener("click", (e:Event) =>{
 // reveal card for multiple 
 multipleCardsBtn.addEventListener("click", (e: Event) => {
   ShowElement(multiple_card, fortuneContainer, text)
-  HideElement(singleCard, singleCardBtn, multipleCardsBtn)
+  HideElement(singleCard, singleCardBtn, multipleCardsBtn, header_title)
 
   /// i am just adding to test how to multiple card look like we will change this later 
   const cards = ["past", "future", "present"];
@@ -89,7 +114,7 @@ multipleCardsBtn.addEventListener("click", (e: Event) => {
     const isReversed = (Math.round(Math.random()) + 1) === 1;
     let chosenCardIndex = Math.round(Math.random() * cardData.cards.length - 1); 
     let chosenCard = cardData.cards[chosenCardIndex]; 
-    return cardTemplate(itm, chosenCard.name, chosenCard.value, isReversed);
+    return cardTemplate(itm, chosenCard.name, chosenCard.value, isReversed, chosenCard.name_short);
   }).join('')}`
 
   multiple_card.innerHTML = MultipleCards
@@ -99,9 +124,26 @@ multipleCardsBtn.addEventListener("click", (e: Event) => {
 
 // new Reading button to just clear the reveal card
 newReadingBtn.addEventListener("click", (e:Event) =>{ 
- ShowElement( singleCardBtn ,multipleCardsBtn)
-    HideElement(singleCard, multiple_card, fortuneContainer)
+ ShowElement( singleCardBtn ,multipleCardsBtn, header_title)
+    HideElement(singleCard, multiple_card, fortuneContainer,fortune_description)
 
 })
 
 
+document.addEventListener("click", (e:Event) => {
+const target = e.target as HTMLElement 
+const cardElement = target.closest(".card_container") as HTMLElement  
+
+if(cardElement){
+const cardId = cardElement.getAttribute("data-id") 
+const FindElement = cardData.cards.find(el => el.name_short == cardId)
+
+if(FindElement){
+ description.textContent = FindElement.desc
+ Cardtitle.textContent = FindElement.name 
+ sub_title.textContent = FindElement.meaning_up
+ title.textContent = FindElement.name
+}
+ShowElement(fortune_description)
+}
+})
