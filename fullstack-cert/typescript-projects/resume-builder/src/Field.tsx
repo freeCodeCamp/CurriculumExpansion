f0a449c992164de88005e7051125b6b590b1a4fb
moshe vilner <shootermv@gmail.com>
2025-08-07 12:11:27 +0300

    chore: better change inout to `self closing` tag

--- fullstack-cert/typescript-projects/resume-builder/src/Field.tsx
+++ fullstack-cert/typescript-projects/resume-builder/src/Field.tsx
@@ -3,7 +3,7 @@ import { EventHandler, ChangeEvent } from "react";
 export function Field({ labelText,fieldType,onChange }: { labelText: string,fieldType:string, onChange: EventHandler<ChangeEvent> }) {
   return (
     <>
-      <label>{labelText}  <input type={fieldType} onChange={onChange}></input></label>
+      <label>{labelText}  <input type={fieldType} onChange={onChange}/></label>
     </>
   );
 }
