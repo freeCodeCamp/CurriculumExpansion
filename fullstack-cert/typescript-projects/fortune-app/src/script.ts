df3c219757bbba6fda81d08204f870eaa1a1d08b
moshe vilner <shootermv@gmail.com>
2025-08-07 20:22:49 +0300

    refactor: attach logic to clss methods

--- fullstack-cert/typescript-projects/fortune-app/src/script.ts
+++ fullstack-cert/typescript-projects/fortune-app/src/script.ts
@@ -21,12 +21,15 @@ const getElement = <T>(selector: string): T => {
   return el as T;
 };
 
+const IMG_URL = "https://cdn.freecodecamp.org/curriculum/typescript/tarot-app";
+
+// SELECTORS
 const singleCardBtn = getElement<HTMLElement>("#btn-single-card");
+const singleCard = getElement<HTMLElement>(".single_card");
 const multipleCardsBtn = getElement<HTMLElement>("#btn-multiple-cards");
+const multiple_card = getElement<HTMLElement>(".multiple_card");
 const fortuneContainer = getElement<HTMLElement>(".fortune_container");
 const fortune_description = getElement<HTMLElement>(".fortune_description");
-const multiple_card = getElement<HTMLElement>(".multiple_card");
-const singleCard = getElement<HTMLElement>(".single_card");
 const newReadingBtn = getElement<HTMLElement>(".btn_reveal");
 const title = getElement<HTMLElement>(".title")!;
 const header_title = getElement<HTMLElement>(".header_title");
@@ -35,9 +38,18 @@ const Cardtitle = getElement<HTMLElement>(".desc_title");
 const description = getElement<HTMLElement>(".description");
 const text = getElement<HTMLElement>(".text");
 
-const apiURL = "https://cdn.freecodecamp.org/curriculum/typescript/tarot-app";
+// UTILS
+const HideElement = (...element: HTMLElement[]) => {
+  element.forEach((el) => {
+    el.classList.add("hidden");
+  });
+};
 
-const dataUrl = apiURL + "/card_data.json";
+const ShowElement = (...element: HTMLElement[]) => {
+  element.forEach((el) => {
+    el.classList.remove("hidden");
+  });
+};
 
 const getRandomItem = <T>(items: T[]): T => {
   const index = Math.floor(Math.random() * items.length - 1);
@@ -64,23 +76,22 @@ const isDeck = (data: any): data is Deck => {
   return Array.isArray(data.cards) && data.cards.every(isCard);
 };
 
-let cardData: Deck = { cards: [] };
+const renderCard = function (
+  drawingType: string,
+  isReversed: boolean,
+  shortName: string,
+  img: string,
+) {
+  return `
+  <div>
+    <h2>${drawingType}</h2>
+    <div class="card_container ${isReversed ? "reversed-card" : ""}" data-id=${shortName} style="background-image: url(${IMG_URL}/${img})">
+    </div>
+  </div>
+  `;
+};
 
-document.addEventListener("DOMContentLoaded", async function () {
-  try {
-    const response = await fetch(dataUrl);
-    const RowData: unknown = await response.json();
-
-    // check row data type
-    if (isDeck(RowData)) {
-      cardData = RowData;
-    } else {
-      throw new Error("Response data does not match expected Deck format");
-    }
-  } catch (err) {
-    console.error(" Failed to load data:", err);
-  }
-});
+// GAME CLASS
 class Game {
   cards: Card[];
 
@@ -101,132 +112,106 @@ class Game {
     this.fetchCardsData();
   }
 
-  showFortune(e) {
-    //add here show fortune logic
-  }
-
   singleCardSelected() {
-    //add here the single card logic
+    HideElement(
+      singleCardBtn,
+      multipleCardsBtn,
+      multiple_card,
+      text,
+      header_title,
+    );
+    const isReversed = Math.round(Math.random()) + 1 === 1;
+
+    let chosenCard = getRandomItem(this.cards);
+
+    ShowElement(singleCard, fortuneContainer);
+    singleCard.innerHTML = renderCard(
+      "Your card",
+      isReversed,
+      chosenCard.name_short,
+      chosenCard.img,
+    );
+    multiple_card.innerHTML = "";
   }
 
   multipleCardSelected() {
-    //add here the multiple card logic
-  }
-}
+    ShowElement(multiple_card, fortuneContainer, text);
+    HideElement(singleCard, singleCardBtn, multipleCardsBtn, header_title);
 
-const game = new Game();
-// UTILS
-const HideElement = (...element: HTMLElement[]) => {
-  element.forEach((el) => {
-    el.classList.add("hidden");
-  });
-};
+    enum DrawingType {
+      Past = "past",
+      Present = "present",
+      Future = "future",
+    }
+    const cardTypes: DrawingType[] = [
+      DrawingType.Past,
+      DrawingType.Present,
+      DrawingType.Future,
+    ];
 
-const renderCard = function (
-  drawingType: string,
-  isReversed: boolean,
-  shortName: string,
-  img: string,
-) {
-  return `
- <div>
-    <h2>${drawingType}</h2>
-    <div class="card_container ${isReversed ? "reversed-card" : ""}" data-id=${shortName} style="background-image: url(${apiURL}/${img})">
-        </div>
-    </div>
-  `;
-};
-
-const ShowElement = (...element: HTMLElement[]) => {
-  element.forEach((el) => {
-    el.classList.remove("hidden");
-  });
-};
-
-// CLICK HANDLERS:
-// reveal card for single card
-singleCardBtn.addEventListener("click", (e: Event) => {
-  HideElement(
-    singleCardBtn,
-    multipleCardsBtn,
-    multiple_card,
-    text,
-    header_title,
-  );
-  const isReversed = Math.round(Math.random()) + 1 === 1;
-
-  let chosenCard = getRandomItem(cardData.cards);
-
-  ShowElement(singleCard, fortuneContainer);
-  singleCard.innerHTML = renderCard(
-    "Your card",
-    isReversed,
-    chosenCard.name_short,
-    chosenCard.img,
-  );
-  multiple_card.innerHTML = "";
-});
-
-// reveal card for multiple
-multipleCardsBtn.addEventListener("click", (e: Event) => {
-  ShowElement(multiple_card, fortuneContainer, text);
-  HideElement(singleCard, singleCardBtn, multipleCardsBtn, header_title);
-
-  enum DrawingType {
-    Past = "past",
-    Present = "present",
-    Future = "future",
-  }
-  const cards: DrawingType[] = [
-    DrawingType.Past,
-    DrawingType.Present,
-    DrawingType.Future,
-  ];
-
-  const MultipleCards = `
-  ${cards
+    const MultipleCards = `
+  ${cardTypes
     .map((itm) => {
       // 1 for front, 2 for reversed
       const isReversed = Math.round(Math.random()) + 1 === 1;
 
-      let chosenCard = getRandomItem(cardData.cards);
-      return renderCard(
-        itm,
-        isReversed,
-        chosenCard.name_short,
-        chosenCard.img,
-      );
+      let chosenCard = getRandomItem(this.cards);
+      return renderCard(itm, isReversed, chosenCard.name_short, chosenCard.img);
     })
     .join("")}`;
 
-  multiple_card.innerHTML = MultipleCards;
-});
+    multiple_card.innerHTML = MultipleCards;
+  }
+
+  showFortune(e) {
+    const target = e.target;
+
+    if (!(target instanceof HTMLElement)) return;
+
+    const cardElement = target.closest(".card_container");
+
+    if (!(cardElement instanceof HTMLElement)) return;
+
+    if (cardElement) {
+      const cardId = cardElement.getAttribute("data-id");
+      const FindElement = this.cards.find((el) => el.name_short == cardId);
+
+      if (FindElement) {
+        description.textContent = FindElement.desc;
+        Cardtitle.textContent = FindElement.name;
+        sub_title.textContent = FindElement.meaning_up;
+        title.textContent = FindElement.name;
+      }
+      ShowElement(fortune_description);
+    }
+  }
+
+  newReading() {
+    ShowElement(singleCardBtn, multipleCardsBtn, header_title);
+    HideElement(
+      singleCard,
+      multiple_card,
+      fortuneContainer,
+      fortune_description,
+    );
+  }
+}
+
+const game = new Game();
+
+// CLICK HANDLERS:
+// reveal card for single card
+singleCardBtn.addEventListener("click", game.singleCardSelected.bind(game));
+
+// reveal card for multiple
+multipleCardsBtn.addEventListener(
+  "click",
+  game.multipleCardSelected.bind(game),
+);
 
 // new Reading button to just clear the reveal card
-newReadingBtn.addEventListener("click", (e: Event) => {
-  ShowElement(singleCardBtn, multipleCardsBtn, header_title);
-  HideElement(singleCard, multiple_card, fortuneContainer, fortune_description);
-});
+newReadingBtn.addEventListener("click", game.newReading);
 
-document.addEventListener("click", (e: Event) => {
-  const target = e.target;
-
-  if (!(target instanceof HTMLElement)) return;
-
-  const cardElement = target.closest(".card_container");
-
-  if (!(cardElement instanceof HTMLElement)) return;
-
-  if (cardElement) {
-    const cardId = cardElement.getAttribute("data-id");
-    const FindElement = cardData.cards.find((el) => el.name_short == cardId);
-
-    if (FindElement) {
-      description.textContent = FindElement.desc;
-      Cardtitle.textContent = FindElement.name;
-      sub_title.textContent = FindElement.meaning_up;
-      title.textContent = FindElement.name;
-    }
-    ShowElement(fortune_description);
-  }
-});
+document.addEventListener("click", (e: Event) =>
+  game.showFortune.bind(game)(e),
+);
