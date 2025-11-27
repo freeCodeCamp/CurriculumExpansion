885652b236231436ed61762f2f2177c71283e68b
aaronmccollum <mccollumaarons@gmail.com>
2025-11-27 10:28:54 +0900

    added boilerplate functions

---
+++ javascript-algorithms-and-data-structures/learn-javascript-objects-by-building-a-device-loan-ledger/script.js
@@ -0,0 +1,25 @@
+function checkoutDevice(ledger, assetTag, borrower) {
+    // code
+    return 0;
+}
+
+function checkinDevice(ledger, assetTag) {
+    // code
+    return 0;
+}
+
+function listOverdueDevices(ledger, today) {
+    // code
+    return 0;
+}
+
+// --- HELPER FUNCTIONS ---
+function serializeLedger(ledger) {
+    // code
+    return 0;
+}
+
+function loadLedger(json) {
+    // code
+    return 0;
+}
