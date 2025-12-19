e0bbed5ea3515bf001938fa3b40c98ef1c3f3fdf
Phyu Sin Shin Thant <phyusinshinthant02@gmail.com>
2025-12-20 01:44:37 +0800

    Created prototype script for Python data types workshop (#1083)

---
+++ fullstack-cert/python-projects/data-types-workshop/main.py
@@ -0,0 +1,16 @@
+# get used to print function
+name = 'Alice'
+print(name)
+print(type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+# difference between int and float
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, int))
+print(score, type(score))
+print(isinstance(score, float))
