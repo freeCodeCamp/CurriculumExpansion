f0339970dd7cf9e0636764cb400ccfea16cf27a7
. <17124333+Dafterfly@users.noreply.github.com>
2025-12-19 12:31:32 +0200

    fix: remove function in prototype, use global variables

--- fullstack-cert/python-projects/booleans-conditonals-lab/main.py
+++ fullstack-cert/python-projects/booleans-conditonals-lab/main.py
@@ -1,19 +1,17 @@
-def can_commute(distance_mi, is_raining, has_bike, has_car, has_ride_share_app):
-    if not distance_mi:
-        return False
+distance_mi = 12
+is_raining = False
+has_bike = False
+has_car = False
+has_ride_share_app = True
 
-    if distance_mi <= 1:
-        return not is_raining
+if not distance_mi:
+    print(False)
 
-    elif distance_mi <= 6:
-        return has_bike and not is_raining
+elif distance_mi <= 1:
+    print(not is_raining)
 
-    else:
-        return has_car or has_ride_share_app
+elif distance_mi <= 6:
+    print(has_bike and not is_raining)
 
-print(can_commute(1, False, False, False, False))
-print(can_commute(3, False, True, False, False))
-print(can_commute(3, True, True, False, False))
-print(can_commute(12, False, False, True, False))
-print(can_commute(12, False, False, False, True))
-print(can_commute(0, False, False, False, True))
+else:
+    print(has_car or has_ride_share_app)
