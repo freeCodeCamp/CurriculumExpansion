f759e8ad4d044b619d2e464c4766321cfd5c01d7
sebacodes <214313960+sebacodes@users.noreply.github.com>
2025-12-20 11:25:52 -0300

    create workshop and input data

---
+++ fullstack-cert/js-projects/sensor-burst-analyzer/scripts.js
@@ -0,0 +1,52 @@
+const sensorBurst = [
+[52,77], 
+[65,99], 
+[8,72,68], 
+[25,6], 
+[93,42,23], 
+[48,33,9], 
+[0,0], 
+[84,3,23], 
+[0,0,0], 
+[0,0], 
+[0,0,0], 
+[51,16], 
+[7,71,47], 
+[16], 
+[0,0,0], 
+[99,53], 
+[0,0], 
+[74,9], 
+[22,11], 
+[55,14,39], 
+[0,0,0]                                 
+]
+
+
+function analyzeBurst(burst, windowSize) {
+//You should create analyzeBurst(burst, windowSize) that returns averages, mins, and maxes using sliding windows.
+
+
+}
+
+
+function detectSpikes(burst, threshold) {
+//You should create detectSpikes(burst, threshold) that uses some/every to flag bursts exceeding thresholds.
+
+
+}
+
+
+function detectSilence(burst, windowSize) {
+//You should create detectSilence(burst, windowSize) that returns true if a full window contains zero readings.
+
+
+}
+
+
+function summarizeSensorData(bursts){
+//You should create summarizeSensorData(bursts) that maps through all bursts, aggregates stats, and returns an array of summary objects.
+
+
+}
+
