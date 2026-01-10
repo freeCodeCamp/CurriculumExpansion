9170028ea441687648a8e4f6fb5a1230c3ecbc60
Aaliyah1225 <aaliyah_haynes@Outlook.com>
2026-01-10 11:38:57 -0500

    updated script.js with route merger

--- fullstack-cert/js-projects/delivery-route-merger/script.js
+++ fullstack-cert/js-projects/delivery-route-merger/script.js
@@ -1,37 +1,60 @@
-const stop1 = [
-    {address: "924 Oak St", distance: 60, windowStart: "12:00", windowEnd: "14:36"},
-    {address: "145 River Rd", distance: 43, windowStart: "10:45", windowEnd: "13:28"},
-    {address: "701 Mountain View Ave", distance: 55, windowStart: "12:20", windowEnd: "14:48"},
-    {address: "33 S. Main St", distance: 72, windowStart: "09:00", windowEnd: "11:55"},
-    {address: "88 Sunset Blvd Apt 12", distance: 85, windowStart: "09:30", windowEnd: "12:27"},
-    {address: "45 Elmwood Dr", distance: 43, windowStart: "07:00", windowEnd: "09:55"},
-    {address: "102 Meadow Ln", distance: 67, windowStart: "09:20", windowEnd: "11:46"},
-    {address: "512 Industrial Way", distance: 45, windowStart: "08:30", windowEnd: "12:17"},
-    {address: "22 Ceder Ave", distance: 23, windowStart: "11:20", windowEnd: "13:32"},
-    {address: "606 Pinecrest Rd", distance: 56, windowStart: "10:10", windowEnd: "13:04"}
+
+const route1 = [
+    {address: "924 Oak St", distance: 60, windowStart: 1765904400, windowEnd: 1765913760},
+    {address: "145 River Rd", distance: 43, windowStart: 1765899900, windowEnd: 1765909680},
+    {address: "701 Mountain View Ave", distance: 55, windowStart: 1765905600, windowEnd: 1765914480},
+    {address: "33 S. Main St", distance: 72, windowStart: 1765893600, windowEnd: 1765904100},
+    {address: "88 Sunset Blvd Apt 12", distance: 85, windowStart: 1765895400, windowEnd: 1765906020},
+    {address: "45 Elmwood Dr", distance: 43, windowStart: 1765886400, windowEnd: 1765896900},
+    {address: "102 Meadow Ln", distance: 67, windowStart: 1765894800, windowEnd: 1765903560},
+    {address: "512 Industrial Way", distance: 45, windowStart: 1765891800, windowEnd: 1765905420},
+    {address: "22 Ceder Ave", distance: 23, windowStart: 1765902000, windowEnd: 1765909920},
+    {address: "606 Pinecrest Rd", distance: 56, windowStart: 1765897800, windowEnd: 1765908240}
 ]
-const stop2 = [
-    {address: "18 Market St Suite 200", distance: 98, windowStart: "10:50", windowEnd: "13:24"},
-    {address: "409 Lakeview Ct", distance: 56, windowStart: "12:40", windowEnd: "15:40"},
-    {address: "111 Park Pl", distance: 34, windowStart: "11:00", windowEnd: "12:54"},
-    {address: "77 Willow Creek Rd", distance: 68, windowStart: "09:30", windowEnd: "11:57"},
-    {address: "29 High St", distance: 78, windowStart: "12:30", windowEnd: "15:10"},
-    {address: "505 University Dr", distance: 45, windowStart: "07:50", windowEnd: "10:03"},
-    {address: "345 Grand Ave", distance: 98, windowStart: "09:30", windowEnd: "12:45"},
-    {address: "900 Bay St", distance: 54, windowStart: "10:40", windowEnd: "14:06"},
-    {address: "1200 Washington Blvd", distance: 78, windowStart: "11:20", windowEnd: "13:07"},
-    {address: "25 Harbor View Pkwy", distance: 48, windowStart: "09:00", windowEnd: "10:17"}
+const route2 = [
+    {address: "18 Market St Suite 200", distance: 98, windowStart: 1765900200, windowEnd: 1765909440},
+    {address: "409 Lakeview Ct", distance: 56, windowStart: 1765906800, windowEnd: 1765917600},
+    {address: "111 Park Pl", distance: 34, windowStart: 1765900800, windowEnd: 1765907640},
+    {address: "77 Willow Creek Rd", distance: 68, windowStart: 1765895400, windowEnd: 1765904220},
+    {address: "29 High St", distance: 78, windowStart: 1765906200, windowEnd: 1765915800},
+    {address: "505 University Dr", distance: 45, windowStart: 1765889400, windowEnd: 1765897380},
+    {address: "345 Grand Ave", distance: 98, windowStart: 1765895400, windowEnd: 1765907100},
+    {address: "900 Bay St", distance: 54, windowStart: 1765899600, windowEnd: 1765911960},
+    {address: "1200 Washington Blvd", distance: 78, windowStart: 1765902000, windowEnd: 1765908420},
+    {address: "25 Harbor View Pkwy", distance: 48, windowStart: 1765893600, windowEnd: 1765898220}
 ]
 
-const stop3 = [
-    {address: "678 Lighthouse Rd", distance: 37, windowStart: "08:30", windowEnd: "10:59"},
-    {address: "810 Canyon Dr", distance: 51, windowStart: "07:30", windowEnd: "09:52"},
-    {address: "44 North Pkwy", distance: 67, windowStart: "10:20", windowEnd: "13:07"},
-    {address: "155 South St Apt 3B", distance: 35, windowStart: "11:30", windowEnd: "14:15"},
-    {address: "732 Maplewood Ave", distance: 61, windowStart: "09:20", windowEnd: "12:48"},
-    {address: "13 Colonial Dr", distance: 30, windowStart: "12:20", windowEnd: "15:08"},
-    {address: "202 Capitol Way", distance: 50, windowStart: "09:50", windowEnd: "13:10"},
-    {address: "55 Eagle Blvd", distance: 42, windowStart: "08:50", windowEnd: "11:14"},
-    {address: "303 Liberty Ln", distance: 72, windowStart: "06:30", windowEnd: "09:25"},
-    {address: "999 Commerce St", distance: 81, windowStart: "08:20", windowEnd: "11:19"},
+const route3 = [
+    {address: "678 Lighthouse Rd", distance: 37, windowStart: 1765891800, windowEnd: 1765900740},
+    {address: "810 Canyon Dr", distance: 51, windowStart: 1765888200, windowEnd: 1765896720},
+    {address: "44 North Pkwy", distance: 67, windowStart: 1765898400, windowEnd: 1765908420},
+    {address: "155 South St Apt 3B", distance: 35, windowStart: 1765902600, windowEnd: 1765912500},
+    {address: "732 Maplewood Ave", distance: 61, windowStart: 1765894800, windowEnd: 1765907280},
+    {address: "13 Colonial Dr", distance: 30, windowStart: 1765905600, windowEnd: 1765915680},
+    {address: "202 Capitol Way", distance: 50, windowStart: 1765896600, windowEnd: 1765908600},
+    {address: "55 Eagle Blvd", distance: 42, windowStart: 1765893000, windowEnd: 1765901640},
+    {address: "303 Liberty Ln", distance: 72, windowStart: 1765884600, windowEnd: 1765895100},
+    {address: "999 Commerce St", distance: 81, windowStart: 1765891200, windowEnd: 1765901940},
 ]
+
+const mergeRoutes = (...routes) => {
+    const finalRoute = [];
+    for (const route of routes) {
+        for (const stop of route) {
+            for (const s of finalRoute)
+                if (finalRoute != s) {
+                s.address === stop.address;
+            finalRoute.push(stop)
+        }
+    }
+}
+return finalRoute;
+}
+
+const sortbyWindows = (stops) => {
+    const sortedStops = [];
+    for (const stop of stops) {
+
+    }
+    return sortedStops;
+}
