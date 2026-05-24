4f63860a51f0898a1c729fcede482ecad65845f6
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-05-25 01:06:45 +0200

    chore: update comments to reflect steps of the workshop
    This updates the overview of how "Guild Loot Tracker" workshop should be broken down into steps.

--- fullstack-cert/js-projects/guild-loot-tracker/script.js
+++ fullstack-cert/js-projects/guild-loot-tracker/script.js
@@ -1,6 +1,6 @@
-// This will be a workshop for the Loops section, probably after the "Build a Profile Lookup" lab.
+// This will be a workshop for the Loops section, probably after the "Build a Profile Lookup" lab. Probably around 32 steps.
 
-// Creating the "guild" object could be 2 or 3 Steps (creating "guild" object itself and adding the first "guild member" object as the first property - rest of the "guild members" can be filled out somewhere between the next steps to avoid too much repetition).
+// The "guild" object could be already provided, or created over 3 or so steps (creating "guild" object itself and adding the first "guild member" object as the first property - rest of the "guild members" can be filled out somewhere between the next steps to avoid too much repetition).
 let guild = {
   ethan: {
     gold: 31,
@@ -43,7 +43,10 @@ let guild = {
 /* NOTE:
 This method seemed like a good addition before I refactored `listMembers()` - it still might be a good(?), isolated example of `Object.keys()` use, but otherwise seems slightly redundant.
 */
-// 'listMemberNames()` introduces `Object.keys()` static method, and provides a list of current members of the guild. Could be 3 steps (1. create a function, 2. assign the array returned by the `Object.keys()` to a variable + explanation of the method, 3. for loop and `console.log()`s)
+// 'listMemberNames()` introduces `Object.keys()` static method, and provides a list of current members of the guild. Could be 3 steps: 
+//    1. create a function,
+//    2. assign the array returned by the `Object.keys()` to a variable + explanation of the method,
+//    3. for loop and `console.log()` methods for the name, 
 function listMemberNames(guildObject) {
   console.log("Current Guild Members:");
   const guildMembers = Object.keys(guildObject);
@@ -54,11 +57,14 @@ function listMemberNames(guildObject) {
 
 listMemberNames(guild);
 
-// `listMembers()` introduces `Object.values()`
+// `listMembers()` introduces `Object.values()`. Probably good for about 4 steps: 
+//    1. Create a new function and add a `console.log()` with the function's description,
+//    2. Create a `for()` loop, and log the `Object.values()`,
+//    3. Replace the `console.log()` for the `Object.values()` with destructuring,
+//    4. add the 3 `console.log()` for the member's name, value names and the destructured values
 function listMembers(guildObject) {
   console.log("Guild Member's Resources:");
   for (const member in guildObject) {
-    // Use Object.keys() and Object.resources() to get a list of resources of each guild member
     const [gold, silver, reputation, experience] = Object.values(
       guildObject[member],
     );
@@ -71,7 +77,11 @@ function listMembers(guildObject) {
 
 listMembers(guild);
 
-// 'getMemberTotals()` introduces `Object.entries() method
+// 'getMemberTotals()` introduces `Object.entries() method. 
+//    1. Create a function and assign `Object.entries()` to a variable,
+//    2. Use `console.log()` to see what has been assigned to the variable
+//    3. Remove the `console.log()` and create a `for()` loop, add `return false` statement,
+//    4. Destructure `guildMember` inside the `if()` conditional, add `return entry`
 function getMemberTotals(guildObject, member) {
   const guildMembers = Object.entries(guildObject);
 
@@ -83,23 +93,27 @@ function getMemberTotals(guildObject, member) {
   }
   return false;
 }
-// Checks if `getMemberTotals()` works correctly - can be one of the steps and removed later
+
+// Checks for validating `getMemberTotals()` is working correctly - can be one of the steps and removed later
 console.log(getMemberTotals(guild, "natalie"));
 console.log(getMemberTotals(guild, "francis"));
 
-// "cloneGuildData" can be broken into multiple (3?) steps to show that using the spread syntax creates a shallow copy of the object, for example #3 write the function and log it #4 assign the returned data to a variable, and change some of its values, then log both original and the copy to the console, #5 remove the variable and console.log() calls to continue
+// "cloneGuildData()" can be broken into multiple (4?) steps, showing that using the spread syntax creates a shallow copy of the object (and explaining how it prevents accidental mutations) for example:
+//    1. Write the function and assign the input parameter to one variable directly, and via the spread operator to a second variable, then call the function,
+//    2. Modify the content of the first variable, then use `console.log()` to compare the original input parameter, and both variables, showing that modifying the direct copy modified the original as well,
+//    3. Modify the shallow copy to show it didn't affect the original
+//    4. Remove the variables and the function call, write the  final version of the function.
 function cloneGuildData(guildObject) {
   return { ...guildObject };
 }
 
 function addLootEntry(guildObject, entry) {
-  // Use cloneGuildData() to prevent accidental mutations before committing the changes to the 'guild object'
+  //    1. Use cloneGuildData() to get a copy of the first input parameter,
   const clonedGuild = cloneGuildData(guildObject);
-  // Use `Object.keys() to get the guild member's
+  //    2. Use `Object.keys() to get a list of guild member's names, and use the spread operator to separate data from the second parameter 
   const guildMemberNames = Object.keys(guildObject);
-  // Use spread operator to get the member's name and resources out of the "entry" argument
   const { member: memberName, ...newMemberTotals } = entry;
-  // Check if the member's name exists, and is a string
+  //    3, 4. Write conditional checking to validate the member's name exists, and is a string
   if (!memberName) {
     console.log("New entry must include a name of the guild member!");
     return;
@@ -107,18 +121,19 @@ function addLootEntry(guildObject, entry) {
     console.log("Member name must be a string!");
     return;
   }
-  // Check if the guildObject includes a record of the provided member
+  //    5. Add a `for()` loop iterating over the guild member's names, add the `console.log()` and a return statement after it (I just realized that `return` is kind of pointless...), Check if the guildObject includes a record of the provided member,
   for (const guildMember in guildMemberNames) {
+    //    6. Write a `if()` statement for finding the name provided in the second input parameter, then get this guild member's data with `getMemberTotals()` and assign it to a variable for readability (we're not making a copy of the object! it could be worth pointing out the difference in the steps),
     if (guildMemberNames[guildMember] === memberName) {
-      // Now, we assign the member from the cloned data to a variable to make the loops more readable (we're not making a copy of the object! it could be worth pointing out the difference in the steps).
       const currentMemberTotals = getMemberTotals(clonedGuild, memberName);
-      // We could check if the entry includes all of the required fields (the same as the existing member)
-      // First we check if the new entry has all of the required resources:
+      //    7. Add a `for()` loop to iterate over the member's resources,
       for (const resource in currentMemberTotals) {
+        //    8. Add a `if()` statement to check if the new data is missing one of the values - if it does, end the function execution if it does,
         if (!Object.hasOwn(newMemberTotals, resource)) {
           console.log(`Updated data is missing an entry for "${resource}"!`);
           return;
         } else if (
+          //    9. Add the else if statement to check if the new value is of the same type as the current one, return early if it isn't
           typeof newMemberTotals[resource] !==
           typeof currentMemberTotals[resource]
         ) {
@@ -127,27 +142,27 @@ function addLootEntry(guildObject, entry) {
           );
           return;
         }
-        // Then, we check if the new entry has ONLY the required resources NOTE: (this one is probably unnecessary, I'm not sure if ALL of the resources should actually be required to make an update):
+        //    10., 11. Then, check if the new entry has ONLY the required resources (could be just one step, but it might be a bit too confusing),
         for (const resource in newMemberTotals) {
           if (!Object.hasOwn(currentMemberTotals, resource)) {
             console.log(`Updated data has an unknown entry "${resource}!`);
             return;
           }
         }
-        // Assigning new values could be done outside of the loop, but on the other hand, it might be how we can show how cloning the current data can prevent against accidental mutation on case where someone provides incorrect input data more clearly (maybe?).
+        //    12. Since at this point the new data passed all of the guard clauses, we can overwrite the old data
         currentMemberTotals[resource] = newMemberTotals[resource];
       }
-      // Finally, now we're sure our input was correct, we can overwrite the old data.
+      //    13. Finally, now we're sure our input was correct, we can overwrite the old data, inform the user the operation was successful, and exit the function.
       guild = clonedGuild;
       console.log("Guild roster updated.");
       return;
     }
   }
-  // In the case that the guildObject doesn't include a member with the given name, inform the user and return
   console.log(`${memberName} is not a member of this guild!`);
   return;
 }
 
+// Last step: use `AddLootEntry() to change the guild member's resources, and use `listMembers()` to see if it worked.
 addLootEntry(guild, {
   member: "natalie",
   gold: 1,
@@ -156,5 +171,4 @@ addLootEntry(guild, {
   experience: 4,
 });
 
-// Using `listMembers()` to check if the data has been updated
 listMembers(guild);
